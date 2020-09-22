d1f52cd7745db569d886d90210fdd6c9d9b0e7a3
Samir B <sbenfares@gmail.com>
2020-09-22 19:06:19 +0200

    Fix clear (#4)

--- server/src/jobs/clear/clear.js
+++ server/src/jobs/clear/clear.js
@@ -1,9 +1,9 @@
 const logger = require("../../common/logger");
-const { User, SampleEntity } = require("../../common/model");
+const { User, StatutCandidat } = require("../../common/model");
 
 module.exports = async () => {
-  await SampleEntity.deleteMany({});
+  await StatutCandidat.deleteMany({});
   await User.deleteMany({});
   logger.info(`Users deleted`);
-  logger.info(`SampleEntity deleted`);
+  logger.info(`StatutCandidat deleted`);
 };
