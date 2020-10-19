4a4a0854a106245c852e451d14a1047f48fb8176
Samir B <sbenfares@gmail.com>
2020-10-19 09:48:57 +0200

    Remove stdio - refacto clear Jobs #32 (#33)
    * Increase Wrapper Timeout
    
    * Remove stdio & ask in clear jobs
    
    * Update version number

--- server/src/jobs/clear/clearStatutsCandidats.js
+++ server/src/jobs/clear/clearStatutsCandidats.js
@@ -1,17 +1,9 @@
 const { runScript } = require("../scriptWrapper");
-const { ask } = require("stdio");
 const logger = require("../../common/logger");
 const { clearStatutsCandidats } = require("./utils/clearUtils");
 
 runScript(async () => {
-  try {
-    const color = await ask("-> Êtes vous sur de vouloir supprimer tous les statutsCandidats ?", {
-      options: ["oui", "non"],
-    });
-    if (color === "oui") {
-      await clearStatutsCandidats();
-    }
-  } catch (err) {
-    logger.info("Réponse non prise en charge...");
-  }
+  logger.info("Suppression de tous les statutsCandidats ....");
+  await clearStatutsCandidats();
+  logger.info("StatutsCandidats supprimés avec succès !");
 });
