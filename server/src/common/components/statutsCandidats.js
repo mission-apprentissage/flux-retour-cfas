7cd00979b81371eb5b9056702c1747e8386cf2fb
Stephane Ruhlmann <sj.ruhlmann@gmail.com>
2020-11-05 14:46:55 +0100

    Import statuts candidat from Gesti (#67)
    * Add 'source' field in StatutCandidat model
    
    * Update tests for statuts candidats
    
    * Create script to import, validate and insert statuts candidats from Gesti in db
    
    * Fix /statuts-candidat route tests
    
    * StatutCandidat source comes from username adding data + add gesti user in config
    
    * Clean users config

--- server/src/common/components/statutsCandidats.js
+++ server/src/common/components/statutsCandidats.js
@@ -2,96 +2,13 @@ const { StatutCandidat } = require("../model");
 const { codesMajStatutsInterdits, codesStatutsMajStatutCandidats } = require("../model/constants");
 const { asyncForEach } = require("../../common/utils/asyncUtils");
 
-module.exports = async () => {
-  return {
-    existsStatut,
-    existsWithStatutValue: async ({
-      ine_apprenant = null,
-      nom_apprenant = null,
-      prenom_apprenant = null,
-      prenom2_apprenant = null,
-      prenom3_apprenant = null,
-      email_contact = null,
-      id_formation,
-      uai_etablissement,
-      statut_apprenant,
-    }) => {
-      const query = getFindStatutWithValueQuery(
-        ine_apprenant,
-        nom_apprenant,
-        prenom_apprenant,
-        prenom2_apprenant,
-        prenom3_apprenant,
-        email_contact,
-        id_formation,
-        uai_etablissement,
-        statut_apprenant
-      );
-      const count = await StatutCandidat.countDocuments(query);
-      return count !== 0;
-    },
-    getStatut,
-    addOrUpdateStatuts: async (itemsToAddOrUpdate) => {
-      const added = [];
-      const updated = [];
-
-      await asyncForEach(itemsToAddOrUpdate, async (item) => {
-        // If status found update it
-        const foundItem = await getStatut({
-          ine_apprenant: item.ine_apprenant,
-          nom_apprenant: item.nom_apprenant,
-          prenom_apprenant: item.prenom_apprenant,
-          prenom2_apprenant: item.prenom2_apprenant,
-          prenom3_apprenant: item.prenom3_apprenant,
-          email_contact: item.email_contact,
-          id_formation: item.id_formation,
-          uai_etablissement: item.uai_etablissement,
-        });
-        if (foundItem) {
-          const updatedItem = await updateStatut(foundItem._id, item);
-          await updated.push(updatedItem);
-        } else {
-          const toAdd = new StatutCandidat({
-            ine_apprenant: item.ine_apprenant,
-            nom_apprenant: item.nom_apprenant,
-            prenom_apprenant: item.prenom_apprenant,
-            prenom2_apprenant: item.prenom2_apprenant,
-            prenom3_apprenant: item.prenom3_apprenant,
-            ne_pas_solliciter: item.ne_pas_solliciter,
-            email_contact: item.email_contact,
-            nom_representant_legal: item.nom_representant_legal,
-            tel_representant_legal: item.tel_representant_legal,
-            tel2_representant_legal: item.tel2_representant_legal,
-            id_formation: item.id_formation,
-            libelle_court_formation: item.libelle_court_formation,
-            libelle_long_formation: item.libelle_long_formation,
-            uai_etablissement: item.uai_etablissement,
-            nom_etablissement: item.nom_etablissement,
-            statut_apprenant: item.statut_apprenant,
-            historique_statut_apprenant: {
-              valeur_statut: item.statut_apprenant,
-              position_statut: 1,
-              date_statut: new Date(Date.now()),
-            },
-            date_entree_statut: item.date_entree_statut,
-            date_saisie_statut: item.date_saisie_statut,
-            date_mise_a_jour_statut: item.date_mise_a_jour_statut,
-            date_metier_mise_a_jour_statut: item.date_metier_mise_a_jour_statut,
-          });
-          const addedItem = await toAdd.save();
-          added.push(addedItem);
-        }
-      });
-
-      return {
-        added,
-        updated,
-      };
-    },
-    updateStatut,
-    getStatutHistory,
-  };
-};
+module.exports = () => ({
+  existsStatut,
+  getStatut,
+  addOrUpdateStatuts,
+  updateStatut,
+  getStatutHistory,
+});
 
 const existsStatut = async ({
   ine_apprenant = null,
@@ -139,6 +56,66 @@ const getStatut = async ({
   );
   const found = await StatutCandidat.findOne(query);
   return found;
+};
+
+const addOrUpdateStatuts = async (itemsToAddOrUpdate) => {
+  const added = [];
+  const updated = [];
+
+  await asyncForEach(itemsToAddOrUpdate, async (item) => {
+    // If status found update it
+    const foundItem = await getStatut({
+      ine_apprenant: item.ine_apprenant,
+      nom_apprenant: item.nom_apprenant,
+      prenom_apprenant: item.prenom_apprenant,
+      prenom2_apprenant: item.prenom2_apprenant,
+      prenom3_apprenant: item.prenom3_apprenant,
+      email_contact: item.email_contact,
+      id_formation: item.id_formation,
+      uai_etablissement: item.uai_etablissement,
+    });
+
+    if (foundItem) {
+      const updatedItem = await updateStatut(foundItem._id, item);
+      await updated.push(updatedItem);
+    } else {
+      const toAdd = new StatutCandidat({
+        ine_apprenant: item.ine_apprenant,
+        nom_apprenant: item.nom_apprenant,
+        prenom_apprenant: item.prenom_apprenant,
+        prenom2_apprenant: item.prenom2_apprenant,
+        prenom3_apprenant: item.prenom3_apprenant,
+        ne_pas_solliciter: item.ne_pas_solliciter,
+        email_contact: item.email_contact,
+        nom_representant_legal: item.nom_representant_legal,
+        tel_representant_legal: item.tel_representant_legal,
+        tel2_representant_legal: item.tel2_representant_legal,
+        id_formation: item.id_formation,
+        libelle_court_formation: item.libelle_court_formation,
+        libelle_long_formation: item.libelle_long_formation,
+        uai_etablissement: item.uai_etablissement,
+        nom_etablissement: item.nom_etablissement,
+        statut_apprenant: item.statut_apprenant,
+        historique_statut_apprenant: {
+          valeur_statut: item.statut_apprenant,
+          position_statut: 1,
+          date_statut: new Date(),
+        },
+        date_entree_statut: item.date_entree_statut,
+        date_saisie_statut: item.date_saisie_statut,
+        date_mise_a_jour_statut: item.date_mise_a_jour_statut,
+        date_metier_mise_a_jour_statut: item.date_metier_mise_a_jour_statut,
+        source: item.source,
+      });
+      const addedItem = await toAdd.save();
+      added.push(addedItem);
+    }
+  });
+
+  return {
+    added,
+    updated,
+  };
 };
 
 const getStatutHistory = async ({
@@ -204,35 +181,6 @@ const updateStatut = async (existingItemId, toUpdate) => {
   const updated = await StatutCandidat.findByIdAndUpdate(existingItemId, updateQuery, { new: true });
   return updated;
 };
-
-const getFindStatutWithValueQuery = (
-  ine_apprenant = null,
-  nom_apprenant = null,
-  prenom_apprenant = null,
-  prenom2_apprenant = null,
-  prenom3_apprenant = null,
-  email_contact = null,
-  id_formation,
-  uai_etablissement,
-  statut_apprenant
-) =>
-  ine_apprenant
-    ? {
-        ine_apprenant: ine_apprenant,
-        id_formation: id_formation,
-        uai_etablissement: uai_etablissement,
-        statut_apprenant: statut_apprenant,
-      }
-    : {
-        nom_apprenant: nom_apprenant,
-        prenom_apprenant: prenom_apprenant,
-        prenom2_apprenant: prenom2_apprenant,
-        prenom3_apprenant: prenom3_apprenant,
-        email_contact: email_contact,
-        id_formation: id_formation,
-        uai_etablissement: uai_etablissement,
-        statut_apprenant: statut_apprenant,
-      };
 
 const getFindStatutQuery = (
   ine_apprenant = null,
--- server/src/jobs/seed/utils/seedUtils.js
+++ server/src/jobs/seed/utils/seedUtils.js
@@ -4,6 +4,7 @@ const { apiStatutsSeeder, administrator } = require("../../../common/roles");
 const { fullSampleWithUpdates } = require("../../../../tests/data/sample");
 const { createRandomStatutsCandidatsList } = require("../../../../tests/data/randomizedSample");
 const { User } = require("../../../common/model/index");
+const { asyncForEach } = require("../../../common/utils/asyncUtils");
 
 const seedUsers = async (users) => {
   if ((await User.countDocuments({ username: config.users.defaultAdmin.name })) !== 0) {
@@ -15,15 +16,21 @@ const seedUsers = async (users) => {
     });
   }
 
-  if ((await User.countDocuments({ username: config.users.ymag })) !== 0) {
-    logger.info(`User ${config.users.ymag} already existing - no creation needed`);
-  } else {
-    logger.info(`Creating user ${config.users.ymag}`);
-    await users.createUser(config.users.ymag, config.apiKeys.ymag, {
-      permissions: [apiStatutsSeeder],
-      apiKey: config.apiKeys.ymag,
-    });
-  }
+  // data providers (ymag, gesti)
+  const usersExcludingDefaultAdmin = Object.keys(config.users)
+    .filter((name) => name !== "defaultAdmin")
+    .map((userKey) => config.users[userKey]);
+  await asyncForEach(usersExcludingDefaultAdmin, async (user) => {
+    if ((await User.countDocuments({ username: user.name })) !== 0) {
+      logger.info(`User ${user.name} already existing - no creation needed`);
+    } else {
+      logger.info(`Creating user ${user.name}`);
+      await users.createUser(user.name, user.password, {
+        permissions: [apiStatutsSeeder],
+        apiKey: user.apiKey,
+      });
+    }
+  });
 };
 
 const seedSample = async (statutsCandidats) => {
