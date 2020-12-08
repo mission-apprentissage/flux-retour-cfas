bb4338a11acb2514e618cdf0d5f2cb96c43b7e60
Stephane Ruhlmann <sj.ruhlmann@gmail.com>
2020-12-08 17:58:41 +0100

    Remove tabler and useless files

--- ui/src/common/components/CenteredCol.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Grid } from "tabler-react";
-
-export default styled(Grid.Col)`
-  max-width: 24rem;
-  margin-left: auto !important;
-  margin-right: auto !important;
-`;
--- ui/src/common/components/FormError.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-  width: 100%;
-  margin-top: 0.25rem;
-  font-size: 87.5%;
-  color: #cd201f;
-`;
