a735b7be4ec9cf3b8cd1b80979806de8f256668a
Stephane Ruhlmann <sj.ruhlmann@gmail.com>
2020-12-16 17:58:25 +0100

    Add docker HEALTHCHECK for server

---
+++ server/scripts/healthcheck.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-process-exit */
+const axios = require("axios");
+
+const isTrue = (v) => v === true;
+
+axios
+  .get("http://localhost:5000/api/healthcheck")
+  .then((response) => {
+    if (response.status === 200 && Object.values(response.data.healthcheck).every(isTrue)) {
+      console.log("healthcheck passed!");
+      process.exit(0);
+    }
+    console.error("Status code was", response.status);
+    console.error("Response data", response.data);
+    process.exit(1);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
---
+++ server/src/http/routes/healthcheck.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const logger = require("../../common/logger");
+const config = require("../../../config");
+const tryCatch = require("../middlewares/tryCatchMiddleware");
+const packageJson = require("../../../package.json");
+
+module.exports = ({ db }) => {
+  const router = express.Router();
+
+  router.get(
+    "/",
+    tryCatch(async (req, res) => {
+      let mongodbStatus;
+      await db
+        .collection("statutsCandidats")
+        .stats()
+        .then(() => {
+          mongodbStatus = true;
+        })
+        .catch((e) => {
+          mongodbStatus = false;
+          logger.error("Healthcheck failed", e);
+        });
+
+      return res.json({
+        name: `Serveur MNA - ${config.appName}`,
+        version: packageJson.version,
+        env: config.env,
+        healthcheck: {
+          mongodb: mongodbStatus,
+        },
+      });
+    })
+  );
+
+  return router;
+};
