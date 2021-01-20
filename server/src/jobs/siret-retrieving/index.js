538c70177067a6260c6cd455cb966f6de202857b
Samir <sbenfares@gmail.com>
2021-01-20 11:42:49 +0100

    WIP

--- server/src/jobs/siret-retrieving/index.js
+++ server/src/jobs/siret-retrieving/index.js
@@ -3,6 +3,9 @@ const logger = require("../../common/logger");
 const ovhStorageManager = require("../../common/utils/ovhStorageManager");
 const path = require("path");
 const fs = require("fs-extra");
+const { readJsonFromCsvFile } = require("../../common/utils/fileUtils");
+const { asyncForEach } = require("../../common/utils/asyncUtils");
+const { StatutCandidat } = require("../../common/model");
 
 const siretGestiReferenceFilePath = path.join(__dirname, `./assets/sirets-gesti.csv`);
 const siretYmagReferenceFilePath = path.join(__dirname, `./assets/sirets-ymag.csv`);
@@ -19,7 +22,20 @@ runScript(async () => {
 
 const retrieveSiretForGesti = async () => {
   logger.info("Retrieving sirets for Gesti");
+
+  // Gets the referentiel file
   await downloadIfNeeded(`siret-erps/sirets-gesti.csv`, siretGestiReferenceFilePath);
+
+  // Parse all data for gesti with siret_etablissement null & uai not null
+  const statutsWithoutSiretsWithUais = await StatutCandidat.find({
+    source: "gesti",
+    $and: [{ siret_etablissement: null }, { uai_etablissement: { $ne: null } }],
+  });
+
+  await asyncForEach(statutsWithoutSiretsWithUais, async (currentStatutWithoutSiret) => {
+    const siretFound = findSiretForUai(currentStatutWithoutSiret.uai_etablissement);
+    logger.info(siretFound);
+  });
 };
 
 const retrieveSiretForYmag = async () => {
@@ -39,4 +55,10 @@ const downloadIfNeeded = async (downloadDestinationFilePath, referenceFilePath) 
   } else {
     logger.info(`File ${referenceFilePath} already present.`);
   }
+};
+
+const findSiretForUai = (uai, filepath) => {
+  const jsonData = readJsonFromCsvFile(__dirname + filepath);
+  logger.info(jsonData);
+  return "123";
 };
