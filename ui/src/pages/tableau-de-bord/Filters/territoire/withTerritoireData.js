4dec8dc242b389da068d1bb7534d0257bd5f589f
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-03-18 16:38:35 +0100

    Refacto withTerritoireData HOC so the wrapping component is named

--- ui/src/pages/tableau-de-bord/Filters/territoire/withTerritoireData.js
+++ ui/src/pages/tableau-de-bord/Filters/territoire/withTerritoireData.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/display-name */
 import React from "react";
 
 import { useFetch } from "../../../../common/hooks/useFetch";
@@ -10,19 +9,23 @@ export const TERRITOIRE_TYPES = {
   departement: "departement",
 };
 
-const withTerritoiresData = (Component) => (props) => {
-  const [departements, departementsLoading] = useFetch(`${GEO_API_URL}/departements`);
-  const [regions, regionsLoading] = useFetch(`${GEO_API_URL}/regions`);
+const withTerritoiresData = (Component) => {
+  const WithTerritoiresData = (props) => {
+    const [departements, departementsLoading] = useFetch(`${GEO_API_URL}/departements`);
+    const [regions, regionsLoading] = useFetch(`${GEO_API_URL}/regions`);
 
-  const departementsOptions =
-    departements?.map((departement) => ({
-      ...departement,
-      type: TERRITOIRE_TYPES.departement,
-    })) || [];
-  const regionsOptions = regions?.map((region) => ({ ...region, type: TERRITOIRE_TYPES.region })) || [];
-  const isLoading = departementsLoading || regionsLoading;
+    const departementsOptions =
+      departements?.map((departement) => ({
+        ...departement,
+        type: TERRITOIRE_TYPES.departement,
+      })) || [];
+    const regionsOptions = regions?.map((region) => ({ ...region, type: TERRITOIRE_TYPES.region })) || [];
+    const isLoading = departementsLoading || regionsLoading;
 
-  return <Component {...props} departements={departementsOptions} regions={regionsOptions} loading={isLoading} />;
+    return <Component {...props} departements={departementsOptions} regions={regionsOptions} loading={isLoading} />;
+  };
+
+  return WithTerritoiresData;
 };
 
 export default withTerritoiresData;
