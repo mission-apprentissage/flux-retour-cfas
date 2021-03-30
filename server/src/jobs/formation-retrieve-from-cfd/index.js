c16b4931ea33d6ef10e1d3e23d51de9a95a2dda9
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-03-30 15:50:52 +0200

    Improve formation-retrieve-from-cfd job logging

--- server/src/jobs/formation-retrieve-from-cfd/index.js
+++ server/src/jobs/formation-retrieve-from-cfd/index.js
@@ -10,14 +10,12 @@ runScript(async ({ db, formations }) => {
   logger.info("Run Retrieve Formations from CFD");
 
   let createdFormationsCount = 0;
-  let formationsNotFoundInTablesCorrespondances = 0;
-  let formationsError = 0;
+  let formationsNotCreated = [];
 
   const collection = db.collection("statutsCandidats");
 
   // get all valid CFDs
   const allCfds = await collection.distinct("id_formation", { id_formation_valid: true });
-
   logger.info(`${allCfds.length} distinct CFDs found in collection StatutsCandidats`);
 
   // for each CFD, if no formation exists in DB, create and store it
@@ -26,16 +24,16 @@ runScript(async ({ db, formations }) => {
     if (!formationExistsInDb) {
       try {
         const created = await formations.createFormation(cfd);
-        created ? createdFormationsCount++ : formationsNotFoundInTablesCorrespondances++;
+        created ? createdFormationsCount++ : formationsNotCreated.push(cfd);
       } catch (err) {
+        formationsNotCreated.push(cfd);
+        logger.error("error while creating formation for CFD", cfd);
         logger.error(err);
-        formationsError++;
       }
     }
   });
 
   logger.info(`${createdFormationsCount} formations created in DB`);
-  logger.warn(`${formationsNotFoundInTablesCorrespondances} formations not found in Tables de Correspondances API`);
-  logger.warn(`${formationsError} CFD with error while fetching Tables de Correspondances API`);
+  logger.warn(`${formationsNotCreated.length} formations could not be created. CFD list: ${formationsNotCreated}`);
   logger.info("End Retrieve Formations from CFD");
 }, jobNames.formationRetrieveFromCfd);
