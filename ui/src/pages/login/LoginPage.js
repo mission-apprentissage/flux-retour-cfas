f6a7043c2c9652347b7ee61e8dabc8cdf96a6f0e
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-04-22 16:36:57 +0200

    Split and refactor LoginPage

--- ui/src/pages/login/LoginPage.js
+++ ui/src/pages/login/LoginPage.js
@@ -1,25 +1,11 @@
-import {
-  Box,
-  Button,
-  Center,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  HStack,
-  Input,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
+import { Box, Center, Flex, Heading, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import queryString from "query-string";
 import React from "react";
-import * as Yup from "yup";
 
 import useAuth from "../../common/hooks/useAuth";
 import { _post } from "../../common/httpClient";
+import LoginForm from "./LoginForm";
 
 const LoginPage = ({ history }) => {
   const [, setAuth] = useAuth();
@@ -37,69 +23,17 @@ const LoginPage = ({ history }) => {
   };
 
   return (
-    <Center background="bluefrance" height="100vh" verticalAlign="center">
-      <Stack align="center">
-        <Flex align="center" mb="5">
-          <Text fontSize="beta" fontWeight="700" color="white" mr="5">
-            <i className="ri-shield-star-fill"></i>
-          </Text>
-          <Text fontSize="beta" fontWeight="700" color="white">
-            Tableau de bord - Flux Cfas
-          </Text>
-        </Flex>
-        <Box background="white" borderRadius="2%" width="28rem">
-          <Center>
-            <Heading mt={4} fontFamily="Marianne" fontWeight="700" marginBottom="2w">
-              Connexion
-            </Heading>
-          </Center>
-          <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
-            <Formik
-              initialValues={{ username: "", password: "" }}
-              validationSchema={Yup.object().shape({
-                username: Yup.string().required("Requis"),
-                password: Yup.string().required("Requis"),
-              })}
-              onSubmit={login}
-            >
-              {({ status = {} }) => {
-                return (
-                  <Form>
-                    <Box marginBottom="2w">
-                      <Field name="username">
-                        {({ field, meta }) => (
-                          <FormControl isRequired isInvalid={meta.error && meta.touched} marginBottom="2w">
-                            <FormLabel>Identifiant</FormLabel>
-                            <Input {...field} id={field.name} placeholder="Votre identifiant..." />
-                            <FormErrorMessage>{meta.error}</FormErrorMessage>
-                          </FormControl>
-                        )}
-                      </Field>
-                      <Field name="password">
-                        {({ field, meta }) => {
-                          return (
-                            <FormControl isRequired isInvalid={meta.error && meta.touched} marginBottom="2w">
-                              <FormLabel>Mot de passe</FormLabel>
-                              <Input {...field} id={field.name} type="password" placeholder="Votre mot de passe..." />
-                              <FormErrorMessage>{meta.error}</FormErrorMessage>
-                            </FormControl>
-                          );
-                        }}
-                      </Field>
-                    </Box>
-                    <HStack spacing="4w">
-                      <Button color="white" type="submit">
-                        Connexion
-                      </Button>
-                    </HStack>
-                    {status.error && <Text color="error">{status.error}</Text>}
-                  </Form>
-                );
-              }}
-            </Formik>
-          </Box>
-        </Box>
-      </Stack>
+    <Center background="bluefrance" height="100vh" flexDirection="column">
+      <Flex fontSize="beta" fontWeight="700" color="white">
+        <Box as="i" className="ri-shield-star-fill" mr="5" />
+        <Text>Tableau de bord - Flux Cfas</Text>
+      </Flex>
+      <Box background="white" borderRadius="2%" width="28rem" mt="4">
+        <Heading mt={4} fontFamily="Marianne" fontWeight="700" marginBottom="2w" textAlign="center">
+          Connexion
+        </Heading>
+        <LoginForm onSubmit={login} />
+      </Box>
     </Center>
   );
 };
