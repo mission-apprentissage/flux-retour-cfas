a7835640a436242e3661e04df7001d02f93c9c36
Samir B <sbenfares@gmail.com>
2021-06-17 16:47:18 +0200

    Add encoding to constants & add Agri file (#317)

--- server/src/common/model/constants/index.js
+++ server/src/common/model/constants/index.js
@@ -52,14 +52,17 @@ const reseauxCfas = {
   CMA: {
     nomReseau: "CMA",
     nomFichier: "cfas-cma",
+    encoding: "latin1",
   },
-  // AGRI: {
-  //   nomReseau: "AGRI",
-  //   nomFichier: "cfas-agri",
-  // },
+  AGRI: {
+    nomReseau: "AGRI",
+    nomFichier: "cfas-agri",
+    encoding: "utf8",
+  },
   ANASUP: {
     nomReseau: "ANASUP",
     nomFichier: "cfas-anasup",
+    encoding: "utf8",
   },
   // PROMOTRANS: {
   //   nomReseau: "PROMOTRANS",
@@ -72,14 +75,17 @@ const reseauxCfas = {
   UIMM: {
     nomReseau: "UIMM",
     nomFichier: "cfas-uimm",
+    encoding: "utf8",
   },
   BTP_CFA: {
     nomReseau: "BTP CFA",
     nomFichier: "cfas-btp-cfa",
+    encoding: "utf8",
   },
   MFR: {
     nomReseau: "MFR",
     nomFichier: "cfas-mfr",
+    encoding: "utf8",
   },
 };
 
