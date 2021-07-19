0ec36a87466ee44d93045ea9e4e2454184a7165d
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-07-19 12:04:25 +0200

    Remove SIRET column in RepartitionEffectifsCfa and use ProgressCell

--- ui/src/common/components/tables/RepartitionEffectifsParCfa.js
+++ ui/src/common/components/tables/RepartitionEffectifsParCfa.js
@@ -1,8 +1,9 @@
-import { HStack, Progress, Tbody, Td, Text, Tr } from "@chakra-ui/react";
+import { Box, Tbody, Td, Tr } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import React from "react";
 
 import { getPercentage } from "../../../common/utils/calculUtils";
+import ProgressCell from "./ProgressCell";
 import Table from "./Table";
 
 const RepartitionEffectifsParCfa = ({ repartitionEffectifsParCfa, loading, error }) => {
@@ -12,55 +13,17 @@ const RepartitionEffectifsParCfa = ({ repartitionEffectifsParCfa, loading, error
     content = (
       <Tbody>
         {repartitionEffectifsParCfa.map((item, index) => {
-          const { siret_etablissement, uai_etablissement, nom_etablissement, effectifs } = item;
+          const { uai_etablissement, nom_etablissement, effectifs } = item;
           const total = effectifs.abandons + effectifs.apprentis + effectifs.inscrits;
           return (
             <Tr key={"headerRow_" + index}>
-              <Td color="grey.800">{uai_etablissement}</Td>
-              <Td color="grey.800">{siret_etablissement}</Td>
-              <Td color="grey.800">{nom_etablissement}</Td>
-              <Td>
-                <HStack>
-                  <Progress
-                    width="3rem"
-                    size="sm"
-                    colorScheme="main"
-                    value={getPercentage(effectifs.apprentis, total)}
-                    borderRadius="8px"
-                  />{" "}
-                  <Text color="grey.800" fontSize="delta" fontWeight="700">
-                    {effectifs.apprentis}
-                  </Text>
-                </HStack>
+              <Td color="grey.800">
+                <Box>{nom_etablissement}</Box>
+                <Box fontSize="omega">UAI : {uai_etablissement}</Box>
               </Td>
-              <Td>
-                <HStack>
-                  <Progress
-                    width="3rem"
-                    size="sm"
-                    colorScheme="main"
-                    borderRadius="8px"
-                    value={getPercentage(effectifs.inscrits, total)}
-                  />{" "}
-                  <Text color="grey.800" fontSize="delta" fontWeight="700">
-                    {effectifs.inscrits}
-                  </Text>
-                </HStack>
-              </Td>
-              <Td>
-                <HStack>
-                  <Progress
-                    width="3rem"
-                    size="sm"
-                    colorScheme="main"
-                    borderRadius="8px"
-                    value={getPercentage(effectifs.abandons, total)}
-                  />{" "}
-                  <Text color="grey.800" fontSize="delta" fontWeight="700">
-                    {effectifs.abandons}
-                  </Text>
-                </HStack>
-              </Td>
+              <ProgressCell label={effectifs.apprentis} value={getPercentage(effectifs.apprentis, total)} />
+              <ProgressCell label={effectifs.inscrits} value={getPercentage(effectifs.inscrits, total)} />
+              <ProgressCell label={effectifs.abandons} value={getPercentage(effectifs.abandons, total)} />
             </Tr>
           );
         })}
@@ -70,7 +33,7 @@ const RepartitionEffectifsParCfa = ({ repartitionEffectifsParCfa, loading, error
 
   return (
     <Table
-      headers={["UAI", "SIRET", "Nom de l'organisme", "apprentis", "apprenants sans contrat", "abandons"]}
+      headers={["Nom de l'organisme", "apprentis", "apprenants sans contrat", "abandons"]}
       loading={loading}
       error={error}
     >
@@ -82,7 +45,6 @@ const RepartitionEffectifsParCfa = ({ repartitionEffectifsParCfa, loading, error
 RepartitionEffectifsParCfa.propTypes = {
   repartitionEffectifsParCfa: PropTypes.arrayOf(
     PropTypes.shape({
-      siret_etablissement: PropTypes.string.isRequired,
       uai_etablissement: PropTypes.string,
       nom_etablissement: PropTypes.string.isRequired,
       effectifs: PropTypes.shape({
