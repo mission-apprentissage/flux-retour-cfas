a152caf910d044c60d98927c77a04a34d804637d
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-07-21 17:03:05 +0200

    Extract NiveauFormationRow to its own file

---
+++ ui/src/common/components/tables/RepartitionEffectifsParFormation/NiveauFormationRow.js
@@ -0,0 +1,39 @@
+import { Box, Td, Tr } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+
+import { getPercentage } from "../../../utils/calculUtils";
+import ProgressCell from "../ProgressCell";
+import FormationRows from "./FormationRows";
+
+const NiveauFormationRow = ({ niveauFormation, effectifs }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const total = effectifs.apprentis + effectifs.inscrits + effectifs.abandons;
+  return (
+    <>
+      <Tr background="galt">
+        <Td color="bluefrance" onClick={() => setIsOpen(!isOpen)} cursor="pointer">
+          <Box as="i" className={isOpen ? "ri-subtract-line" : "ri-add-line"} verticalAlign="middle" fontSize="beta" />
+          <Box as="span" fontWeight="700" verticalAlign="middle" marginLeft="1w">
+            Niveau {niveauFormation}
+          </Box>
+        </Td>
+        <ProgressCell label={effectifs.apprentis} value={getPercentage(effectifs.apprentis, total)} />
+        <ProgressCell label={effectifs.inscrits} value={getPercentage(effectifs.inscrits, total)} />
+        <ProgressCell label={effectifs.abandons} value={getPercentage(effectifs.abandons, total)} />
+      </Tr>
+      {isOpen && <FormationRows niveauFormation={niveauFormation} />}
+    </>
+  );
+};
+
+NiveauFormationRow.propTypes = {
+  niveauFormation: PropTypes.string.isRequired,
+  effectifs: PropTypes.shape({
+    apprentis: PropTypes.number.isRequired,
+    inscrits: PropTypes.number.isRequired,
+    abandons: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default NiveauFormationRow;
--- ui/src/common/components/tables/RepartitionEffectifsParFormation/index.js
+++ ui/src/common/components/tables/RepartitionEffectifsParFormation/index.js
@@ -1,11 +1,9 @@
-import { Box, Tbody, Td, Tr } from "@chakra-ui/react";
+import { Tbody } from "@chakra-ui/react";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 
-import { getPercentage } from "../../../utils/calculUtils";
-import ProgressCell from "../ProgressCell";
 import Table from "../Table";
-import FormationRows from "./FormationRows";
+import NiveauFormationRow from "./NiveauFormationRow";
 
 const RepartitionEffectifsParFormation = ({ repartitionEffectifs, loading, error }) => {
   return (
@@ -14,7 +12,7 @@ const RepartitionEffectifsParFormation = ({ repartitionEffectifs, loading, error
         {repartitionEffectifs
           ? repartitionEffectifs.map((data) => {
               return (
-                <RepartitionParNiveauRow
+                <NiveauFormationRow
                   key={data.niveau_formation}
                   niveauFormation={data.niveau_formation}
                   effectifs={data.effectifs}
@@ -25,36 +23,6 @@ const RepartitionEffectifsParFormation = ({ repartitionEffectifs, loading, error
       </Tbody>
     </Table>
   );
-};
-
-const RepartitionParNiveauRow = ({ niveauFormation, effectifs }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const total = effectifs.apprentis + effectifs.inscrits + effectifs.abandons;
-  return (
-    <>
-      <Tr background="galt">
-        <Td color="bluefrance" onClick={() => setIsOpen(!isOpen)} cursor="pointer">
-          <Box as="i" className={isOpen ? "ri-subtract-line" : "ri-add-line"} verticalAlign="middle" fontSize="beta" />
-          <Box as="span" fontWeight="700" verticalAlign="middle" marginLeft="1w">
-            Niveau {niveauFormation}
-          </Box>
-        </Td>
-        <ProgressCell label={effectifs.apprentis} value={getPercentage(effectifs.apprentis, total)} />
-        <ProgressCell label={effectifs.inscrits} value={getPercentage(effectifs.inscrits, total)} />
-        <ProgressCell label={effectifs.abandons} value={getPercentage(effectifs.abandons, total)} />
-      </Tr>
-      {isOpen && <FormationRows niveauFormation={niveauFormation} />}
-    </>
-  );
-};
-
-RepartitionParNiveauRow.propTypes = {
-  niveauFormation: PropTypes.string.isRequired,
-  effectifs: PropTypes.shape({
-    apprentis: PropTypes.number.isRequired,
-    inscrits: PropTypes.number.isRequired,
-    abandons: PropTypes.number.isRequired,
-  }).isRequired,
 };
 
 RepartitionEffectifsParFormation.propTypes = {
