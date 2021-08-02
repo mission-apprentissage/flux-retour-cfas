742320bb2c7fb00581ebee1741e3f62698b63d53
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-08-02 20:18:08 +0200

    Clean useFetch and usePostFetch hooks

--- ui/src/common/hooks/useFetch.js
+++ ui/src/common/hooks/useFetch.js
@@ -1,29 +1,26 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { _get, _post } from "../httpClient";
 
-export function useFetch(url, initialState = null) {
-  const [response, setResponse] = useState(initialState);
+export function useFetch(url) {
+  const [response, setResponse] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [refetchTrigger, setRefetchTrigger] = useState(0);
 
-  const fetchData = useCallback(async () => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await _get(url);
-      setResponse(response);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }, [url]);
-
   useEffect(() => {
-    fetchData();
+    async function fetch() {
+      try {
+        setLoading(true);
+        const response = await _get(url);
+        setResponse(response);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetch();
   }, [url, refetchTrigger]);
 
   const refetch = () => setRefetchTrigger(refetchTrigger + 1);
@@ -31,31 +28,28 @@ export function useFetch(url, initialState = null) {
   return [response, loading, error, refetch];
 }
 
-export function usePostFetch(url, body, initialState = null) {
-  const [response, setResponse] = useState(initialState);
+export function usePostFetch(url, body) {
+  const [response, setResponse] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  const _fetch = useCallback(async () => {
-    setLoading(true);
-    setError(null);
-
-    try {
-      const response = await _post(url, body);
-      setResponse(response);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }, [url, JSON.stringify(body)]);
+  const [refetchTrigger, setRefetchTrigger] = useState(0);
 
   useEffect(() => {
-    async function fetchData() {
-      return _fetch();
+    async function fetch() {
+      try {
+        setLoading(true);
+        const response = await _post(url, body);
+        setResponse(response);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
     }
-    fetchData();
-  }, [_fetch]);
+    fetch();
+  }, [url, JSON.stringify(body), refetchTrigger]);
 
-  return [response, loading, error];
+  const refetch = () => setRefetchTrigger(refetchTrigger + 1);
+
+  return [response, loading, error, refetch];
 }
