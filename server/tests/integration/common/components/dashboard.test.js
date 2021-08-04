9ec004397e581bd81f504efb8c9847fc950554b7
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-08-04 16:29:39 +0200

    Improve rupturants computation using index in historique_statut_apprenant instead of unsafe position_statut

--- server/tests/integration/common/components/dashboard.test.js
+++ server/tests/integration/common/components/dashboard.test.js
@@ -523,62 +523,58 @@ integrationTests(__filename, () => {
 
     beforeEach(async () => {
       const statuts = [
-        // rupturant
+        // following statuts are potential rupturants (depends on date)
         createRandomStatutCandidat({
           etablissement_num_region: "199",
           historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 3, date_statut: new Date("2020-09-13T00:00:00") },
-            { position_statut: 2, valeur_statut: 2, date_statut: new Date("2020-10-01T00:00:00") },
+            { valeur_statut: 3, date_statut: new Date("2020-09-13T00:00:00") },
+            { valeur_statut: 2, date_statut: new Date("2020-10-01T00:00:00") },
           ],
         }),
         createRandomStatutCandidat({
           historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 3, date_statut: new Date("2020-09-13T00:00:00") },
-            { position_statut: 2, valeur_statut: 2, date_statut: new Date("2020-10-01T00:00:00") },
-            { position_statut: 3, valeur_statut: 3, date_statut: new Date("2020-11-01T00:00:00") },
-          ],
-        }),
-        // not a rupturant
-        createRandomStatutCandidat({
-          historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 1, date_statut: new Date("2020-03-22T00:00:00") },
-          ],
-        }),
-        // not a rupturant
-        createRandomStatutCandidat({
-          historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 1, date_statut: new Date("2020-04-21T00:00:00") },
-            { position_statut: 2, valeur_statut: 2, date_statut: new Date("2020-04-24T00:00:00") },
-            { position_statut: 3, valeur_statut: 3, date_statut: new Date("2020-04-30T00:00:00") },
+            { valeur_statut: 3, date_statut: new Date("2020-09-13T00:00:00") },
+            { valeur_statut: 2, date_statut: new Date("2020-10-01T00:00:00") },
+            { valeur_statut: 3, date_statut: new Date("2020-11-01T00:00:00") },
           ],
         }),
         createRandomStatutCandidat({
           historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 3, date_statut: new Date("2020-07-29T00:00:00") },
-            { position_statut: 2, valeur_statut: 2, date_statut: new Date("2020-09-20T00:00:00") },
-            { position_statut: 3, valeur_statut: 0, date_statut: new Date("2020-12-07T00:00:00") },
+            { valeur_statut: 1, date_statut: new Date("2020-03-01T00:00:00") },
+            { valeur_statut: 3, date_statut: new Date("2020-07-29T00:00:00") },
+            { valeur_statut: 2, date_statut: new Date("2020-09-20T00:00:00") },
+            { valeur_statut: 0, date_statut: new Date("2020-12-07T00:00:00") },
           ],
         }),
         createRandomStatutCandidat({
           historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 1, date_statut: new Date("2020-07-29T00:00:00") },
-            { position_statut: 2, valeur_statut: 2, date_statut: new Date("2020-09-20T00:00:00") },
-            { position_statut: 3, valeur_statut: 3, date_statut: new Date("2020-12-07T00:00:00") },
-            { position_statut: 4, valeur_statut: 2, date_statut: new Date("2020-12-21T00:00:00") },
+            { valeur_statut: 1, date_statut: new Date("2020-07-29T00:00:00") },
+            { valeur_statut: 2, date_statut: new Date("2020-09-20T00:00:00") },
+            { valeur_statut: 3, date_statut: new Date("2020-12-07T00:00:00") },
+            { valeur_statut: 2, date_statut: new Date("2020-12-21T00:00:00") },
           ],
         }),
-        // not a rupturant
+        // following statuts cannot be rupturants
+        createRandomStatutCandidat({
+          historique_statut_apprenant: [{ valeur_statut: 2, date_statut: new Date("2020-03-22T00:00:00") }],
+        }),
+        createRandomStatutCandidat({
+          historique_statut_apprenant: [{ valeur_statut: 3, date_statut: new Date("2020-11-01T00:00:00") }],
+        }),
         createRandomStatutCandidat({
           historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 2, date_statut: new Date("2020-02-01T00:00:00") },
+            { valeur_statut: 1, date_statut: new Date("2020-04-21T00:00:00") },
+            { valeur_statut: 2, date_statut: new Date("2020-04-24T00:00:00") },
+            { valeur_statut: 3, date_statut: new Date("2020-04-30T00:00:00") },
           ],
         }),
-        // not a rupturant
         createRandomStatutCandidat({
           etablissement_num_region: "199",
-          historique_statut_apprenant: [
-            { position_statut: 1, valeur_statut: 3, date_statut: new Date("2020-05-15T00:00:00") },
-          ],
+          historique_statut_apprenant: [{ valeur_statut: 2, date_statut: new Date("2020-02-01T00:00:00") }],
+        }),
+        createRandomStatutCandidat({
+          etablissement_num_region: "199",
+          historique_statut_apprenant: [{ valeur_statut: 3, date_statut: new Date("2020-05-15T00:00:00") }],
         }),
       ];
       for (let index = 0; index < statuts.length; index++) {
@@ -599,7 +595,7 @@ integrationTests(__filename, () => {
       assert.equal(count, 2);
     });
 
-    it("gets count of rupturants at date with additional filter", async () => {
+    it("gets count of rupturants now with additional filter", async () => {
       const date = new Date();
       const count = await getRupturantsCountAtDate(date, { etablissement_num_region: "199" });
       assert.equal(count, 1);
