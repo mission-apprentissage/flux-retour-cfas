5b77eea7ec8594954b3b0aaa355cb09c5bff4302
Samir B <sbenfares@gmail.com>
2021-08-23 09:29:20 +0200

    Move alert in effectif component (#442)

--- ui/src/pages/tableau-de-bord/TableauDeBordPage.js
+++ ui/src/pages/tableau-de-bord/TableauDeBordPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Alert, Page, Section } from "../../common/components";
+import { Page } from "../../common/components";
 import { FiltersProvider } from "./FiltersContext";
 import IndicesHeader from "./IndicesHeader";
 import useEffectifs from "./useEffectifs";
@@ -12,13 +12,7 @@ const TableauDeBordPage = () => {
   return (
     <Page>
       <IndicesHeader />
-      <Section paddingY="2w">
-        <Alert>
-          La collecte des effectifs 2021-2022 est en cours ce qui peut expliquer pour certains organismes des
-          informations incomplètes. Les chiffres seront disponibles progressivement d’ici début septembre. Nous vous
-          remercions de votre compréhension.
-        </Alert>
-      </Section>
+
       <View effectifs={effectifs} loading={loading} error={error} />
     </Page>
   );
