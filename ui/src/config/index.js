ed0f4616ca245adac736102a37ddfbb96f4dd7a5
Samir B <sbenfares@gmail.com>
2021-09-17 23:46:10 +0200

    Fix react_app env var (#481) (#485)
    * Refacto react_app env var
    
    * Update docker.override local
    
    * Bump version

--- ui/src/config/index.js
+++ ui/src/config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  env: process.env.REACT_APP_FLUX_RETOUR_CFAS_ENV,
-  crispWebsiteId: process.env.REACT_APP_FLUX_RETOUR_CFAS_CRISP_WEBSITE_ID,
+  env: process.env.REACT_APP_ENV,
+  crispWebsiteId: process.env.REACT_APP_CRISP_WEBSITE_ID,
 };
