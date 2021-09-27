2a71c6e7fbc7dc54a991834633a4ce7b775a8d36
Pablo <pablohanry9@gmail.com>
2021-09-27 15:48:44 +0200

    feat: display domainesMetiers

--- ui/src/pages/tableau-de-bord/views/CfaView/InfosCfa/DomainesMetiers.js
+++ ui/src/pages/tableau-de-bord/views/CfaView/InfosCfa/DomainesMetiers.js
@@ -1,30 +1,39 @@
-import { HStack, Tag } from "@chakra-ui/react";
+import { Button, HStack, Tag, useBoolean } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import React from "react";
 
 import { MAX_DISPLAYED_DOMAINE_METIERS } from "../../../../../common/constants/domainesMetiers";
 
 const DomainesMetiers = ({ domainesMetiers }) => {
-  const domainesMetierToDisplay =
-    domainesMetiers.length > MAX_DISPLAYED_DOMAINE_METIERS
-      ? [...domainesMetiers.slice(0, MAX_DISPLAYED_DOMAINE_METIERS), "..."]
-      : domainesMetiers;
+  // const [show, setShow] = useBoolean();
+  const [displayDomaines, setDisplayDomaines] = useBoolean();
+
+  const domainesMetierToDisplay = displayDomaines
+    ? [...domainesMetiers.slice(0, MAX_DISPLAYED_DOMAINE_METIERS)]
+    : domainesMetiers;
 
   return (
-    <HStack marginTop="1w">
+    <HStack marginTop="1w" flexWrap="wrap">
       {domainesMetierToDisplay.map((item, i) => (
-        <Tag
-          key={i}
-          fontSize="omega"
-          paddingX="2w"
-          paddingY="1w"
-          borderRadius="20px"
-          color="white"
-          background="rgba(255, 255, 255, 0.24)"
-        >
-          {item}
-        </Tag>
+        <>
+          <br />
+          <br />
+          <Tag
+            key={i}
+            fontSize="omega"
+            paddingX="2w"
+            paddingY="1w"
+            borderRadius="20px"
+            color="white"
+            background="rgba(255, 255, 255, 0.24)"
+          >
+            {item}
+          </Tag>
+        </>
       ))}
+      <Button size="sm" onClick={setDisplayDomaines.toggle} mt="1rem" color="white">
+        {displayDomaines ? "afficher les domaines" : "masquer les domaines"}
+      </Button>
     </HStack>
   );
 };
