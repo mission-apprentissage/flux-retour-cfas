7a2dd2a97cc39529cb29bbbcec3b45f96a8eec76
Samir B <sbenfares@gmail.com>
2021-10-19 13:49:28 +0200

    Affelnet export - Add CFD & UAI fields (#527)
    * Add CFD & UAI fields
    Add CFD & UAI Fields to export
    
    * Move to support / Add filter on ine & siret / annee_formation to export
    
    * Add periode_formation
    
    * Add annee_scolaire

--- server/src/common/model/schema/croisementVoeuxAffelnet.js
+++ server/src/common/model/schema/croisementVoeuxAffelnet.js
@@ -12,6 +12,35 @@ const croisementVoeuxAffelnet = new Schema({
     default: null,
     description: "Statut apprenant",
   },
+  uai_etablissement: {
+    type: String,
+    default: null,
+    description: "UAI de l'établissement",
+  },
+  siret_etablissement: {
+    type: String,
+    default: null,
+    description: "Siret de l'établissement",
+  },
+  formation_cfd: {
+    type: String,
+    default: null,
+    description: "Code CFD de la formation",
+  },
+  annee_formation: {
+    type: Number,
+    default: null,
+    description: "Numéro de l'année dans la formation (promo)",
+  },
+  periode_formation: {
+    type: [Number],
+    default: [],
+    description: "Date debut & date de fin de la formation",
+  },
+  annee_scolaire: {
+    type: String,
+    description: 'Année scolaire sur laquelle le statut candidat est enregistré (ex: "2020-2021")',
+  },
 });
 
 module.exports = croisementVoeuxAffelnet;
