cee6466be717db35a68ae9d5b8b4ce74164d7814
Samir B <sbenfares@gmail.com>
2021-10-19 14:26:43 +0200

    Add clearFile arg & rename seed function (#528)

--- server/src/jobs/seed/seedUsers.js
+++ server/src/jobs/seed/seedUsers.js
@@ -7,16 +7,20 @@ const { jobNames } = require("../../common/model/constants");
 const { asyncForEach } = require("../../common/utils/asyncUtils");
 const { readJsonFromCsvFile } = require("../../common/utils/fileUtils");
 const { User } = require("../../common/model");
+const arg = require("arg");
 
 const usersFileName = config.env === "production" ? "users" : "users-recette";
 const usersFromCsvFile = path.join(__dirname, `./assets/${usersFileName}.csv`);
+let args = [];
 
 runScript(async ({ users }) => {
+  args = arg({ "--clearCsvFile": Boolean }, { argv: process.argv.slice(2) });
+
   logger.info("-> Seed API Users from config...");
   await seedApiUsers(users);
 
   logger.info("-> Seed Tdb Users from csv...");
-  await seedusers(users);
+  await seedTdbUsers(users, args["--clearCsvFile"]);
 
   logger.info("-> All users are successfully created !");
 }, jobNames.seedUsers);
@@ -58,8 +62,8 @@ const seedApiUsers = async (usersModule) => {
  * Seeding Tdb Users
  * @param {*} users
  */
-const seedusers = async (usersModule) => {
-  await downloadIfNeeded(`users/${usersFileName}.csv`, usersFromCsvFile);
+const seedTdbUsers = async (usersModule, clearCsvFile = false) => {
+  await downloadIfNeeded(`users/${usersFileName}.csv`, usersFromCsvFile, clearCsvFile);
   const usersFromCsv = readJsonFromCsvFile(usersFromCsvFile, "utf8");
 
   const usersToCreate = usersFromCsv.map((item) => ({
