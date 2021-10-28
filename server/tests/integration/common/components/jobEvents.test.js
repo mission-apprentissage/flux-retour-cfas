1d0064000c5f9c12b827d051a0309a49cceb1e4b
Samir B <sbenfares@gmail.com>
2021-10-28 08:56:17 +0200

    Refacto JobEvents (#547)
    * Update scriptWrapper for started / executed / finished action
    
    * Add jobEvents component & isJobInAction method + tests

---
+++ server/tests/integration/common/components/jobEvents.test.js
@@ -0,0 +1,53 @@
+const assert = require("assert");
+const integrationTests = require("../../../utils/integrationTests");
+const jobEvents = require("../../../../src/common/components/jobEvents");
+const { JobEvent } = require("../../../../src/common/model");
+const { jobEventStatuts } = require("../../../../src/common/model/constants");
+
+integrationTests(__filename, () => {
+  it("Permet de vérifier si le job courant est dans l'action terminée", async () => {
+    const { isJobInAction } = await jobEvents();
+
+    const testJobName = "TEST-JOB";
+    // Add started event
+    await new JobEvent({
+      jobname: testJobName,
+      action: jobEventStatuts.started,
+    }).save();
+
+    // Add executed event
+    await new JobEvent({
+      jobname: testJobName,
+      action: jobEventStatuts.executed,
+    }).save();
+
+    // Add ended event
+    await new JobEvent({
+      jobname: testJobName,
+      action: jobEventStatuts.ended,
+    }).save();
+
+    const isEnded = await isJobInAction(testJobName, jobEventStatuts.ended);
+    assert.strictEqual(isEnded, true);
+  });
+
+  it("Permet de vérifier si le job courant n'est pas dans l'action terminée", async () => {
+    const { isJobInAction } = await jobEvents();
+
+    const testJobName = "TEST-JOB";
+    // Add started event
+    await new JobEvent({
+      jobname: testJobName,
+      action: jobEventStatuts.started,
+    }).save();
+
+    // Add executed event
+    await new JobEvent({
+      jobname: testJobName,
+      action: jobEventStatuts.executed,
+    }).save();
+
+    const isEnded = await isJobInAction(testJobName, jobEventStatuts.ended);
+    assert.strictEqual(isEnded, false);
+  });
+});
