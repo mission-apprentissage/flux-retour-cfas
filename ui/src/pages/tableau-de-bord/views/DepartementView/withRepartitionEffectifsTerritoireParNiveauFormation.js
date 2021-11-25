0c40a1d8f3da3df0c76350f0eac4721955851592
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-11-25 11:54:06 +0100

    ESlint fixes

--- ui/src/pages/tableau-de-bord/views/DepartementView/withRepartitionEffectifsTerritoireParNiveauFormation.js
+++ ui/src/pages/tableau-de-bord/views/DepartementView/withRepartitionEffectifsTerritoireParNiveauFormation.js
@@ -1,4 +1,3 @@
-import qs from "query-string";
 import React from "react";
 import { useQuery } from "react-query";
 
--- ui/src/pages/tableau-de-bord/views/RegionView/withRepartitionEffectifsRegionParDepartement.js
+++ ui/src/pages/tableau-de-bord/views/RegionView/withRepartitionEffectifsRegionParDepartement.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { fetchEffectifsParDepartement } from "../../../../common/api/tableauDeBord";
 
+import { fetchEffectifsParDepartement } from "../../../../common/api/tableauDeBord";
 import { mapFiltersToApiFormat } from "../../../../common/utils/mapFiltersToApiFormat";
 import { pick } from "../../../../common/utils/pick";
 import { filtersPropTypes } from "../../FiltersContext";
@@ -9,11 +9,11 @@ import { filtersPropTypes } from "../../FiltersContext";
 const withRepartitionEffectifsTerritoireParDepartement = (Component) => {
   const WithRepartitionEffectifsTerritoireParDepartement = ({ filters, ...props }) => {
     const requestFilters = pick(mapFiltersToApiFormat(filters), ["date", "etablissement_num_region"]);
-    const { data } = useQuery(["effectifs-par-departement", requestFilters], () =>
+    const { data, isLoading, error } = useQuery(["effectifs-par-departement", requestFilters], () =>
       fetchEffectifsParDepartement(requestFilters)
     );
 
-    return <Component {...props} repartitionEffectifsParDepartement={data} loading={loading} error={error} />;
+    return <Component {...props} repartitionEffectifsParDepartement={data} loading={isLoading} error={error} />;
   };
 
   WithRepartitionEffectifsTerritoireParDepartement.propTypes = {
