28c78ffee92adb75713bc046008ed06d35116ae5
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2021-12-07 11:42:53 +0100

    Allow users to see effectifs on national level

--- ui/src/pages/tableau-de-bord/views/NationalView/NationalView.js
+++ ui/src/pages/tableau-de-bord/views/NationalView/NationalView.js
@@ -2,26 +2,22 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import { Page } from "../../../../common/components";
-import { filtersPropTypes } from "../../FiltersContext";
 import { effectifsPropType } from "../../propTypes";
 import { EffectifsSection, IndicesHeaderSection, ProvenanceIndicesSection } from "../../sections";
-import RepartitionEffectifsRegion from "./RepartitionEffectifsRegion";
 
-const RegionView = ({ effectifs, loading, filters }) => {
+const NationalView = ({ effectifs, loading }) => {
   return (
     <Page>
       <IndicesHeaderSection />
       <ProvenanceIndicesSection />
       <EffectifsSection effectifs={effectifs} loading={loading} />
-      <RepartitionEffectifsRegion filters={filters} />
     </Page>
   );
 };
 
-RegionView.propTypes = {
+NationalView.propTypes = {
   effectifs: effectifsPropType,
-  filters: filtersPropTypes.state,
   loading: PropTypes.bool.isRequired,
 };
 
-export default RegionView;
+export default NationalView;
