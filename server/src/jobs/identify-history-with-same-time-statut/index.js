6da773424b089ac20a6c55c6ded5a1f0e7bc8aee
Samir <sbenfares@gmail.com>
2022-01-06 13:28:48 +0100

    Update script & add command

--- server/src/jobs/identify-history-with-same-time-statut/index.js
+++ server/src/jobs/identify-history-with-same-time-statut/index.js
@@ -1,21 +1,29 @@
 const omit = require("lodash.omit");
 const { runScript } = require("../scriptWrapper");
 const logger = require("../../common/logger");
-const { jobNames } = require("../../common/model/constants");
+const { jobNames, codesStatutsCandidats } = require("../../common/model/constants");
 
-const hasStatutsWithSameDateInHistorique = (historique) => {
+const hasStatutsInscritsOrApprentisWithSameDateInHistorique = (historique) => {
   const dates = [];
   for (let index = 0; index < historique.length; index++) {
-    const date = historique[index].date_statut.getTime();
-    if (dates.includes(date)) {
-      return true;
+    // check only for statuts inscrits or apprentis
+    if (
+      historique[index].valeur_statut === codesStatutsCandidats.inscrit ||
+      historique[index].valeur_statut === codesStatutsCandidats.apprenti
+    ) {
+      const date = historique[index].date_statut.getTime();
+      if (dates.includes(date)) {
+        return true;
+      }
+      dates.push(date);
     }
-    dates.push(date);
   }
   return false;
 };
 
 runScript(async ({ db }) => {
+  logger.info(`Identification for statuts inscrits / apprentis with equal dates in historique_statut_apprenant`);
+
   const resultCollection = db.collection("statutsAvecDateIdentiqueDansHistorique");
   // create a cursor over all the statuts with an historique of size > 1
   const statutsCandidatsCollection = db.collection("statutsCandidats");
@@ -24,7 +32,7 @@ runScript(async ({ db }) => {
   let count = 0;
   while (await cursor.hasNext()) {
     const document = await cursor.next();
-    if (hasStatutsWithSameDateInHistorique(document.historique_statut_apprenant)) {
+    if (hasStatutsInscritsOrApprentisWithSameDateInHistorique(document.historique_statut_apprenant)) {
       resultCollection.insertOne({ ...omit(document, "_id"), original_id: document._id });
       count++;
     }
