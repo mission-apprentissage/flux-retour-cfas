8520a00983205c6d8e336f06ed04206ca0281969
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-01-06 15:10:01 +0100

    Sort reseaux alphabetically in reseaux filter

--- ui/src/pages/tableau-de-bord/sections/IndicesHeaderSection/CfasFilter/withReseauxData.js
+++ ui/src/pages/tableau-de-bord/sections/IndicesHeaderSection/CfasFilter/withReseauxData.js
@@ -2,21 +2,16 @@ import React from "react";
 import { useQuery } from "react-query";
 
 import { fetchReseaux } from "../../../../../common/api/tableauDeBord";
+import { sortAlphabeticallyBy } from "../../../../../common/utils/sortAlphabetically";
 
 const withReseauxData = (Component) => {
   const WithReseauxData = (props) => {
     // reseaux are very unlikely during the user's session, thus the infinite staleTime
-    const { data: reseaux } = useQuery("reseaux", () => fetchReseaux(), { staleTime: Infinity });
+    const { data } = useQuery("reseaux", () => fetchReseaux(), { staleTime: Infinity });
 
-    const sortedReseaux = reseaux
-      ? reseaux.slice().sort((a, b) => {
-          if (a.nom < b.nom) return -1;
-          if (a.nom > b.nom) return 1;
-          return 0;
-        })
-      : [];
+    const reseaux = sortAlphabeticallyBy("nom", data || []);
 
-    return <Component {...props} reseaux={sortedReseaux} />;
+    return <Component {...props} reseaux={reseaux} />;
   };
 
   return WithReseauxData;
