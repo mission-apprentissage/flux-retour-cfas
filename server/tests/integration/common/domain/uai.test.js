f93c240a4ab3083b596d1abe29f826fbe9c731a2
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-01-25 12:08:31 +0100

    Create util function to etract departement code from uai

--- server/tests/integration/common/domain/uai.test.js
+++ server/tests/integration/common/domain/uai.test.js
@@ -1,5 +1,5 @@
 const assert = require("assert").strict;
-const { validateUai } = require("../../../../src/common/domain/uai");
+const { validateUai, getDepartementCodeFromUai } = require("../../../../src/common/domain/uai");
 
 describe("Domain UAI", () => {
   describe("validateUai", () => {
@@ -37,6 +37,31 @@ describe("Domain UAI", () => {
       const input = "0000001S";
       const expectedOutput = true;
       assert.deepEqual(validateUai(input), expectedOutput);
+    });
+  });
+
+  describe("getDepartementCodeFromUai", () => {
+    it("Vérifie qu'on récupère le département 01 pour 0011074M", () => {
+      const input = "0011074M";
+      const expectedOutput = "01";
+      assert.equal(getDepartementCodeFromUai(input), expectedOutput);
+    });
+
+    it("Vérifie qu'on récupère le département 77 pour 0772242U", () => {
+      const input = "0772242U";
+      const expectedOutput = "77";
+      assert.equal(getDepartementCodeFromUai(input), expectedOutput);
+    });
+
+    it("Vérifie qu'on récupère le département 974 pour 9741681J", () => {
+      const input = "9741681J";
+      const expectedOutput = "974";
+      assert.equal(getDepartementCodeFromUai(input), expectedOutput);
+    });
+
+    it("Vérifie que la fonction throw une erreur lorsqu'un uai invalide est passé", () => {
+      const input = "abc";
+      assert.throws(() => getDepartementCodeFromUai(input), new Error("invalid uai passed"));
     });
   });
 });
