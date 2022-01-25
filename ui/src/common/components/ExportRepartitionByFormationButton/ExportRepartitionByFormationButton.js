365883eed10c8f67d9d37ee42710b33bd0144538
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-01-25 17:30:00 +0100

    Contextualize csv export file name

--- ui/src/common/components/ExportRepartitionByFormationButton/ExportRepartitionByFormationButton.js
+++ ui/src/common/components/ExportRepartitionByFormationButton/ExportRepartitionByFormationButton.js
@@ -3,11 +3,12 @@ import React from "react";
 import { fetchRepartitionByFormationCsvExport } from "../../../common/api/tableauDeBord";
 import { useFiltersContext } from "../../../pages/tableau-de-bord/FiltersContext";
 import DownloadButton from "../../components/DownloadButton/DownloadButton";
+import { buildFileName } from "../../utils/buildFileNameFromFilters";
 import { mapFiltersToApiFormat } from "../../utils/mapFiltersToApiFormat";
 
 const ExportRepartitionByFormationButton = () => {
   const { state: filters } = useFiltersContext();
-  const fileName = `export-repartition-effectifs-par-formation-${filters.date.toISOString()}.csv`;
+  const fileName = buildFileName("liste_formations", filters);
 
   return (
     <DownloadButton
--- ui/src/common/components/ExportRepartitionByOrganismeButton/ExportRepartitionByOrganismeButton.js
+++ ui/src/common/components/ExportRepartitionByOrganismeButton/ExportRepartitionByOrganismeButton.js
@@ -3,11 +3,12 @@ import React from "react";
 import { fetchRepartitionByOrganismeCsvExport } from "../../../common/api/tableauDeBord";
 import { useFiltersContext } from "../../../pages/tableau-de-bord/FiltersContext";
 import DownloadButton from "../../components/DownloadButton/DownloadButton";
+import { buildFileName } from "../../utils/buildFileNameFromFilters";
 import { mapFiltersToApiFormat } from "../../utils/mapFiltersToApiFormat";
 
 const ExportRepartitionByOrganismeButton = () => {
   const { state: filters } = useFiltersContext();
-  const fileName = `export-repartition-effectifs-par-organisme-${filters.date.toISOString()}.csv`;
+  const fileName = buildFileName("liste_organismes", filters);
 
   return (
     <DownloadButton
