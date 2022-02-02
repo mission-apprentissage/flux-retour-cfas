42eb9cad05bca94a6a4b33e9ef0e42346b3e3eae
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-02-02 16:34:08 +0100

    Delete unused /last-date userEvents route and related code

--- server/src/common/components/userEvents.js
+++ server/src/common/components/userEvents.js
@@ -2,15 +2,7 @@ const { UserEventModel } = require("../model");
 
 module.exports = () => ({
   create,
-  getLastUserEventDate,
 });
-
-const getLastUserEventDate = async ({ username, type, action }) => {
-  const lastUserEventDate = await UserEventModel.findOne({ username: username, type: type, action: action }).sort({
-    date: "desc",
-  });
-  return lastUserEventDate?.date.toLocaleString("fr-FR");
-};
 
 const create = async ({ username, type, action, data }) => {
   const event = new UserEventModel({
--- server/tests/integration/common/components/userEvents.test.js
+++ server/tests/integration/common/components/userEvents.test.js
@@ -4,40 +4,11 @@ const userEvents = require("../../../../src/common/components/userEvents");
 const { UserEventModel } = require("../../../../src/common/model");
 
 integrationTests(__filename, () => {
-  describe("getLastUserEventDate", () => {
-    it("Permet de retrouver la date du dernier userEvent", async () => {
-      const { getLastUserEventDate } = await userEvents();
-
-      // Add first user event
-      const firstEvent = new UserEventModel({
-        username: "TEST",
-        type: "TYPE",
-        action: "ACTION",
-        data: null,
-      });
-      await firstEvent.save();
-      const firstDateCreated = await UserEventModel.findOne({ username: "TEST", type: "TYPE", action: "ACTION" });
-
-      // Add second user event
-      const secondEvent = new UserEventModel({
-        username: "TEST",
-        type: "TYPE",
-        action: "ACTION",
-        data: null,
-      });
-      await secondEvent.save();
-
-      const found = await getLastUserEventDate({ username: "TEST", type: "TYPE", action: "ACTION" });
-      assert.notEqual(found, null);
-      assert.notEqual(found, firstDateCreated);
-    });
-  });
-
   describe("createUserEvent", () => {
     it("Permet de créer un userEvent et de le sauver en base", async () => {
       const { create } = userEvents();
 
-      await create({ username: "admin", type: "any", action: "test", data: { hello: "world" } });
+      await create({ username: "admin", type: "any", action: "test", data: { hello: "world" }, date: new Date() });
       const foundInDb = await UserEventModel.findOne({ username: "admin" });
       assert.ok(foundInDb);
     });
