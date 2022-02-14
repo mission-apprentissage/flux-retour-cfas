71f00b6a0d4b08dc1f2563a0a003554ab6f9727c
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-02-14 15:30:41 +0100

    Use mongodb-in-memory for tests

--- server/tests/utils/mochaSetup.js
+++ server/tests/utils/mochaSetup.js
@@ -1,18 +1,19 @@
 const nock = require("nock"); // eslint-disable-line node/no-unpublished-require
-const { connectToMongo, closeMongoConnection } = require("../../src/common/mongodb");
-const { clearAllCollections } = require("./mongoUtils");
+const { clearAllCollections, startAndConnectMongodb, stopMongodb } = require("./mongoUtils");
 const { nockExternalApis } = require("./nockApis");
 const redisFakeClient = require("./redisClientMock");
+const { createIndexes } = require("../../src/common/indexes");
 
-// disable HTTP requests on the network for tests, except to reach local server
+// disable HTTP requests on the network for tests, except to reach local server and mondodb-inmemory-server
 nock.disableNetConnect();
 nock.enableNetConnect((host) => {
-  return host.startsWith("127.0.0.1");
+  return host.startsWith("127.0.0.1") || host.includes("fastdl.mongodb.org");
 });
 
-// connect to mongodb before running tests
+// connect to mongodb and create indexes before running tests
 exports.mochaGlobalSetup = async () => {
-  await connectToMongo();
+  const db = await startAndConnectMongodb();
+  await createIndexes(db);
 };
 
 // hooks that will be used in every test suite
@@ -29,5 +30,5 @@ exports.mochaHooks = {
 
 // close mongo connection when all tests have been run
 exports.mochaGlobalTeardown = async () => {
-  await closeMongoConnection();
+  await stopMongodb();
 };
--- server/tests/utils/mongoUtils.js
+++ server/tests/utils/mongoUtils.js
@@ -1,4 +1,24 @@
-const { mongooseInstance } = require("../../src/common/mongodb");
+// eslint-disable-next-line node/no-unpublished-require
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { mongooseInstance, closeMongoConnection, connectToMongo } = require("../../src/common/mongodb");
+
+let mongoInMemory;
+
+const startAndConnectMongodb = async () => {
+  mongoInMemory = await MongoMemoryServer.create({
+    binary: {
+      version: "5.0.2",
+    },
+  });
+  const uri = mongoInMemory.getUri();
+  const { db } = await connectToMongo(uri);
+  return db;
+};
+
+const stopMongodb = async () => {
+  await closeMongoConnection();
+  await mongoInMemory.stop();
+};
 
 const clearAllCollections = async () => {
   const collections = mongooseInstance.connection.collections;
@@ -11,5 +31,7 @@ const clearAllCollections = async () => {
 };
 
 module.exports = {
+  startAndConnectMongodb,
+  stopMongodb,
   clearAllCollections,
 };
