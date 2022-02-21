5826ffb61885539807cd1160c0c393b0e9a53119
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-02-21 10:43:25 +0100

    Set authorized http hosts as constants in tests

--- server/tests/utils/mochaSetup.js
+++ server/tests/utils/mochaSetup.js
@@ -4,10 +4,13 @@ const { nockExternalApis } = require("./nockApis");
 const redisFakeClient = require("./redisClientMock");
 const { createIndexes } = require("../../src/common/indexes");
 
+const LOCAL_HOST = "127.0.0.1";
+const MONGODB_MEMORY_SERVER_DL_HOST = "fastdl.mongodb.org";
+
 // disable HTTP requests on the network for tests, except to reach local server and mondodb-inmemory-server
 nock.disableNetConnect();
 nock.enableNetConnect((host) => {
-  return host.startsWith("127.0.0.1") || host.includes("fastdl.mongodb.org");
+  return host.includes(LOCAL_HOST) || host.includes(MONGODB_MEMORY_SERVER_DL_HOST);
 });
 
 // connect to mongodb and create indexes before running tests
