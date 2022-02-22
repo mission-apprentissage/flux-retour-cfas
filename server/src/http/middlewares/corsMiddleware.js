491d6720fb64be920cb8ccf5af14d8646328bd20
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-02-22 14:14:49 +0100

    Remove useless CORS middleware

--- server/src/http/middlewares/corsMiddleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => {
-  // eslint-disable-next-line no-unused-vars
-  return (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-api-key");
-    next();
-  };
-};
