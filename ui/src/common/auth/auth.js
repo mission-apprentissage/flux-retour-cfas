fadc2a6ece66e1f07c4ad3cb40e3d17009cca196
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-02-22 10:33:16 +0100

    Reset auth state when HTTP requests fail with a 401

--- ui/src/common/auth/auth.js
+++ ui/src/common/auth/auth.js
@@ -6,9 +6,13 @@ const access_token = localStorage.getItem("flux-retour-cfas:access_token");
 export const getAuthUserRole = () => localStorage.getItem("flux-retour-cfas:userPermissions");
 export const getAuthUserNetwork = () => localStorage.getItem("flux-retour-cfas:userNetwork");
 
-const { useGlobalState, getGlobalState } = createGlobalState({
+const { useGlobalState, getGlobalState, setGlobalState } = createGlobalState({
   auth: access_token ? decodeJWT(access_token) : null,
 });
 
 export const getAuth = () => getGlobalState("auth");
 export const useAuthState = () => useGlobalState("auth");
+export const resetAuth = () => {
+  localStorage.removeItem("flux-retour-cfas:access_token");
+  setGlobalState("auth", null);
+};
