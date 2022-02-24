10ee6a0ced49092157cde669f4dee9a274082aab
Samir B <sbenfares@gmail.com>
2022-02-24 09:08:41 +0100

    Display CopyCfaPrivateLink only for admin (#804)

--- ui/src/pages/tableau-de-bord/views/CfaView/sections/actions/ActionsSection.js
+++ ui/src/pages/tableau-de-bord/views/CfaView/sections/actions/ActionsSection.js
@@ -1,13 +1,18 @@
 import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
 
+import { hasUserRoles, roles } from "../../../../../../common/auth/roles";
 import { Section } from "../../../../../../common/components";
+import useAuth from "../../../../../../common/hooks/useAuth";
 import { infosCfaPropType } from "../../propTypes";
 import CopyCfaPrivateLinkButton from "./CopyCfaPrivateLinkButton";
 import DataFeedbackSection from "./DataFeedbackSection/DataFeedbackSection";
 import SousEtablissementSelection from "./SousEtablissementSelection";
 
 const ActionsSection = ({ infosCfa }) => {
+  const [auth] = useAuth();
+  const isAdmin = hasUserRoles(auth, roles.administrator);
+
   return (
     <Section>
       <Flex justifyContent="space-between">
@@ -18,7 +23,7 @@ const ActionsSection = ({ infosCfa }) => {
         </div>
         <Box justifySelf="flex-end">
           <DataFeedbackSection uai={infosCfa?.uai} />
-          {infosCfa?.url_tdb && <CopyCfaPrivateLinkButton link={infosCfa?.url_tdb} />}
+          {infosCfa?.url_tdb && isAdmin && <CopyCfaPrivateLinkButton link={infosCfa?.url_tdb} />}
         </Box>
       </Flex>
     </Section>
