3253575ed4033804c278c27d8ea49516e0e0c8de
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-03-02 10:11:44 +0100

    Delete statuts with historique_statut_apprenant empty

---
+++ server/src/jobs/remove-data/statuts-avec-historique-vide/index.js
@@ -0,0 +1,13 @@
+const { runScript } = require("../../scriptWrapper");
+const logger = require("../../../common/logger");
+const { StatutCandidatModel } = require("../../../common/model");
+
+runScript(async () => {
+  logger.info("Suppression des statuts avec historique vide");
+
+  const query = { historique_statut_apprenant: { $size: 0 } };
+  const count = await StatutCandidatModel.countDocuments(query);
+  logger.info(count, "statuts found");
+  const result = await StatutCandidatModel.deleteMany(query);
+  logger.info(result.deletedCount, "statuts candidats supprimés avec succès");
+}, "suppression-statuts-candidats-historique-vide");
