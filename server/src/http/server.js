c838f43e6c7e3c368ad401aced057edb6fcffeb9
Samir B <sbenfares@gmail.com>
2022-03-28 10:39:48 +0200

    Rename .route & router (#872)

--- server/src/http/server.js
+++ server/src/http/server.js
@@ -8,22 +8,22 @@ const errorMiddleware = require("./middlewares/errorMiddleware");
 const requireJwtAuthenticationMiddleware = require("./middlewares/requireJwtAuthentication");
 const permissionsMiddleware = require("./middlewares/permissionsMiddleware");
 
-const rcoRoute = require("./routes/rco");
-const dossierApprenantRoute = require("./routes/dossiers-apprenants.route");
-const lienPriveCfaRoute = require("./routes/lien-prive-cfa");
-const loginRoute = require("./routes/login");
-const loginCfaRoute = require("./routes/login-cfa.route");
-const configRoute = require("./routes/config");
-const referentielRoute = require("./routes/referentiel");
-const effectifsRoute = require("./routes/effectifs");
-const cfasRoute = require("./routes/cfas");
-const formationRoutes = require("./routes/formations");
-const healthcheckRoute = require("./routes/healthcheck");
-const demandeIdentifiants = require("./routes/demande-identifiants");
-const demandeLienPriveRoute = require("./routes/demande-lien-prive");
-const demandeBranchementErpRoute = require("./routes/demande-branchement-erp");
-const cacheRouter = require("./routes/cache");
-const updatePasswordRouter = require("./routes/update-password");
+const rcoRouter = require("./routes/rco.route");
+const dossierApprenantRouter = require("./routes/dossiers-apprenants.route");
+const lienPriveCfaRouter = require("./routes/lien-prive-cfa.route");
+const loginRouter = require("./routes/login.route");
+const loginCfaRouter = require("./routes/login-cfa.route");
+const configRouter = require("./routes/config.route");
+const referentielRouter = require("./routes/referentiel.route");
+const effectifsRouter = require("./routes/effectifs.route");
+const cfasRouter = require("./routes/cfas.route");
+const formationRouter = require("./routes/formations.route");
+const healthcheckRouter = require("./routes/healthcheck.route");
+const demandeIdentifiantsRouter = require("./routes/demande-identifiants.route");
+const demandeLienPriveRouter = require("./routes/demande-lien-prive.route");
+const demandeBranchementErpRouter = require("./routes/demande-branchement-erp.route");
+const cacheRouter = require("./routes/cache.route");
+const updatePasswordRouter = require("./routes/update-password.route");
 
 module.exports = async (components) => {
   const app = express();
@@ -35,15 +35,15 @@ module.exports = async (components) => {
   app.use(logMiddleware());
 
   // open routes
-  app.use("/api/login", loginRoute(components));
-  app.use("/api/login-cfa", loginCfaRoute(components));
-  app.use("/api/formations", formationRoutes(components));
-  app.use("/api/cfas", cfasRoute(components));
-  app.use("/api/referentiel", referentielRoute());
-  app.use("/api/healthcheck", healthcheckRoute(components));
-  app.use("/api/demande-identifiants", demandeIdentifiants(components));
-  app.use("/api/demande-lien-prive", demandeLienPriveRoute(components));
-  app.use("/api/demande-branchement-erp", demandeBranchementErpRoute(components));
+  app.use("/api/login", loginRouter(components));
+  app.use("/api/login-cfa", loginCfaRouter(components));
+  app.use("/api/formations", formationRouter(components));
+  app.use("/api/cfas", cfasRouter(components));
+  app.use("/api/referentiel", referentielRouter());
+  app.use("/api/healthcheck", healthcheckRouter(components));
+  app.use("/api/demande-identifiants", demandeIdentifiantsRouter(components));
+  app.use("/api/demande-lien-prive", demandeLienPriveRouter(components));
+  app.use("/api/demande-branchement-erp", demandeBranchementErpRouter(components));
   app.use("/api/update-password", updatePasswordRouter(components));
 
   // requires JWT auth
@@ -52,31 +52,31 @@ module.exports = async (components) => {
     "/api/statut-candidats",
     requireJwtAuthentication,
     permissionsMiddleware([apiRoles.apiStatutsSeeder]),
-    dossierApprenantRoute(components)
+    dossierApprenantRouter(components)
   );
   app.use(
     "/api/dossiers-apprenants",
     requireJwtAuthentication,
     permissionsMiddleware([apiRoles.apiStatutsSeeder]),
-    dossierApprenantRoute(components)
+    dossierApprenantRouter(components)
   );
   app.use(
     "/api/liens-prives-cfas",
     requireJwtAuthentication,
     permissionsMiddleware([apiRoles.apiStatutsSeeder]),
-    lienPriveCfaRoute(components)
+    lienPriveCfaRouter(components)
   );
   app.use(
     "/api/rco",
     requireJwtAuthentication,
     permissionsMiddleware([apiRoles.apiStatutsConsumer.anonymousDataConsumer]),
-    rcoRoute(components)
+    rcoRouter(components)
   );
-  app.use("/api/effectifs", requireJwtAuthentication, effectifsRoute(components));
+  app.use("/api/effectifs", requireJwtAuthentication, effectifsRouter(components));
 
   // admin routes
   app.use("/api/cache", requireJwtAuthentication, adminOnly, cacheRouter(components));
-  app.use("/api/config", requireJwtAuthentication, adminOnly, configRoute());
+  app.use("/api/config", requireJwtAuthentication, adminOnly, configRouter());
 
   app.use(errorMiddleware());
 
