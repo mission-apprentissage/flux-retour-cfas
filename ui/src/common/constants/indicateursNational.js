1b45e11d8bcf9d3db1b46d9ec2dde41011070903
Samir B <sbenfares@gmail.com>
2022-04-21 20:49:06 +0200

    refacto : indicateur static data (#956) (#957)

---
+++ ui/src/common/constants/indicateursNational.js
@@ -0,0 +1,18 @@
+export const indicateursNational = {
+  dateCalcul: "21 avril 2022",
+  nbOrganismeFormation: {
+    count: 2787,
+  },
+  nbApprentis: {
+    count: 329427,
+  },
+  nbInscritsSansContrats: {
+    count: 5863,
+  },
+  nbRupturants: {
+    count: 1189,
+  },
+  nbAbandons: {
+    count: 36605,
+  },
+};
