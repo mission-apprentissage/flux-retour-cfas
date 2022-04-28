e0bfe06817054fc3a51d1e0b468cd849c59e1612
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-04-28 14:57:12 +0200

    Add test to /referentiel/departements route

--- server/tests/integration/http/referentiel.route.test.js
+++ server/tests/integration/http/referentiel.route.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert").strict;
 const { startServer } = require("../../utils/testUtils");
 const { RESEAUX_CFAS } = require("../../../src/common/constants/networksConstants");
-const { REGIONS } = require("../../../src/common/constants/territoiresConstants");
+const { REGIONS, DEPARTEMENTS } = require("../../../src/common/constants/territoiresConstants");
 
 describe(__filename, () => {
   it("Vérifie qu'on peut récupérer les réseaux référentiels via API", async () => {
@@ -22,5 +22,15 @@ describe(__filename, () => {
     assert.deepEqual(response.data.length, REGIONS.length);
     assert.deepEqual(response.data[0].code, REGIONS[0].code);
     assert.deepEqual(response.data[0].nom, REGIONS[0].nom);
+  });
+
+  it("Vérifie qu'on peut récupérer les départements via API", async () => {
+    const { httpClient } = await startServer();
+    const response = await httpClient.get("/api/referentiel/departements");
+
+    assert.deepStrictEqual(response.status, 200);
+    assert.deepEqual(response.data.length, DEPARTEMENTS.length);
+    assert.deepEqual(response.data[0].code, DEPARTEMENTS[0].code);
+    assert.deepEqual(response.data[0].nom, DEPARTEMENTS[0].nom);
   });
 });
