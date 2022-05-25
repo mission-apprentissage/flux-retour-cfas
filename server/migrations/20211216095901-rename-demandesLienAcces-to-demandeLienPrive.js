10a86e28b0f1839b98a1c2b8f32f1f094bbef180
Pablo <pablohanry9@gmail.com>
2022-05-25 15:40:33 +0200

    fix(ui): delete SubmitPrivateLinkDemand

--- server/migrations/20211216095901-rename-demandesLienAcces-to-demandeLienPrive.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async up(db) {
-    await db.collection("demandeLienAcces").rename("demandesLienPrive", (err) => console.log(err));
-  },
-
-  async down(db) {
-    await db.collection("demandesLienPrive").rename("demandeLienAcces", (err) => console.log(err));
-  },
-};
