84736f7ff8ca9d741c2c260c135f4a73c48681e8
Pablo <pablohanry9@gmail.com>
2022-06-08 10:22:19 +0200

    feat(ui): add auto complete

--- ui/src/pages/admin/gestion-reseaux-cfas/CreateReseauCfaForm.js
+++ ui/src/pages/admin/gestion-reseaux-cfas/CreateReseauCfaForm.js
@@ -1,89 +1,84 @@
-import {
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-  ModalBody,
-  ModalFooter,
-  Select,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
-import { Field, Form, Formik } from "formik";
+import { Box, Button, FormControl, FormLabel, Input, ModalBody, ModalFooter, Select, Stack } from "@chakra-ui/react";
+import { useFormik } from "formik";
 import PropTypes from "prop-types";
 import React from "react";
+import { useQuery } from "react-query";
 import * as Yup from "yup";
 
-import { uaiRegex } from "../../../common/domain/uai";
-
-const formValidationSchema = Yup.object().shape({
-  nom_reseau: Yup.string().required("Requis"),
-  nom_etablissement: Yup.string().required("Requis"),
-  uai: Yup.string().matches(uaiRegex, "UAI invalide").required("Requis"),
-});
-
-const initialValues = { nom_reseau: "", nom_etablissement: "", uai: "" };
+import { fetchCfa } from "../../../common/api/tableauDeBord";
+import { uaiRegex, validateUai } from "../../../common/domain/uai";
 
 const CreateReseauCfaForm = ({ onSubmit, networkList }) => {
+  const { values, handleChange, setFieldValue, errors, handleSubmit } = useFormik({
+    initialValues: {
+      nom_reseau: "",
+      nom_etablissement: "",
+      uai: "",
+    },
+    validationSchema: Yup.object().shape({
+      nom_reseau: Yup.string().required("Requis"),
+      nom_etablissement: Yup.string().required("Requis"),
+      uai: Yup.string().matches(uaiRegex, "UAI invalide").required("Requis"),
+    }),
+    onSubmit: ({ nom_reseau, nom_etablissement, uai }) => {
+      onSubmit({ nom_reseau: nom_reseau, nom_etablissement: nom_etablissement, uai: uai });
+    },
+  });
+
+  const searchEnabled = validateUai(values.uai);
+
+  useQuery(["cfa", values.uai], () => fetchCfa(values.uai), {
+    enabled: searchEnabled,
+    onSuccess: (data) => {
+      setFieldValue("nom_etablissement", data?.libelleLong);
+    },
+    onError: () => {
+      setFieldValue("nom_etablissement", "");
+    },
+  });
+
   return (
-    <Formik initialValues={initialValues} validationSchema={formValidationSchema} onSubmit={onSubmit}>
-      {({ status = {}, isSubmitting }) => {
-        return (
-          <Form>
-            <ModalBody paddingX="8w">
-              <Stack spacing="2w" paddingY="5w" paddingX="4w" borderColor="bluefrance" border="1px solid">
-                <Field name="nom_reseau">
-                  {({ field, meta }) => (
-                    <FormControl isRequired isInvalid={meta.error && meta.touched}>
-                      <FormLabel color="grey.800">Nom du reseau</FormLabel>
-                      <Select marginTop="1w" {...field} id={field.name} placeholder="Sélectionnez un réseau">
-                        {networkList.map(({ nom }) => (
-                          <option key={nom} value={nom}>
-                            {nom}
-                          </option>
-                        ))}
-                      </Select>
-                      <FormErrorMessage>{meta.error}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
-                <Field name="nom_etablissement">
-                  {({ field, meta }) => (
-                    <FormControl isRequired isInvalid={meta.error && meta.touched}>
-                      <FormLabel color="grey.800">Nom du CFA</FormLabel>
-                      <Input {...field} id={field.name} />
-                      <FormErrorMessage>{meta.error}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
-                <Field name="uai">
-                  {({ field, meta }) => (
-                    <FormControl isRequired isInvalid={meta.error && meta.touched}>
-                      <FormLabel color="grey.800">UAI</FormLabel>
-                      <Input {...field} id={field.name} />
-                      <FormErrorMessage>{meta.error}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
-              </Stack>
-            </ModalBody>
-            <ModalFooter paddingY="3w" marginTop="5w" boxShadow="0px -4px 16px 0px rgba(0, 0, 0, 0.08)">
-              <Button type="submit" variant="primary" isLoading={isSubmitting}>
-                Envoyer
-              </Button>
-            </ModalFooter>
-            {status.error && <Text color="error">{status.error}</Text>}
-          </Form>
-        );
-      }}
-    </Formik>
+    <Box>
+      <ModalBody paddingX="8w">
+        <Stack spacing="2w" paddingY="5w" paddingX="4w" borderColor="bluefrance" border="1px solid">
+          <FormControl isRequired isInvalid={errors.nom_reseau}>
+            <FormLabel color="grey.800">Nom du reseau</FormLabel>
+            <Select
+              name="nom_reseau"
+              value={values.nom_reseau}
+              onChange={handleChange}
+              marginTop="1w"
+              placeholder="Sélectionnez un réseau"
+            >
+              {networkList.map(({ nom }) => (
+                <option key={nom} value={nom}>
+                  {nom}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl isRequired isInvalid={errors.nom_etablissement}>
+            <FormLabel color="grey.800">Nom du CFA</FormLabel>
+            <Input name="nom_etablissement" value={values.nom_etablissement} onChange={handleChange} />
+          </FormControl>
+          <FormControl isRequired isInvalid={errors.uai}>
+            <FormLabel color="grey.800">UAI</FormLabel>
+            <Input name="uai" value={values.uai} onChange={handleChange} />
+          </FormControl>
+        </Stack>
+      </ModalBody>
+      <ModalFooter paddingY="3w" marginTop="5w" boxShadow="0px -4px 16px 0px rgba(0, 0, 0, 0.08)">
+        <Button onClick={handleSubmit} type="submit" variant="primary">
+          Envoyer
+        </Button>
+      </ModalFooter>
+    </Box>
   );
 };
 
 CreateReseauCfaForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
   networkList: PropTypes.array,
+  onSubmit: PropTypes.func.isRequired,
 };
 
 export default CreateReseauCfaForm;
