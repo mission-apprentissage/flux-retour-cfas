21c44a5ee00413e3ea9904b6d55ff3e6374a06b1
Pablo <pablohanry9@gmail.com>
2022-06-22 10:57:44 +0200

    feat: add migration

---
+++ server/migrations/20220622085640-remove-useless-dossiersApprenants-fields.js
@@ -0,0 +1,18 @@
+module.exports = {
+  async up(db) {
+    const collection = db.collection("dossiersApprenants");
+    await collection.updateMany(
+      {},
+      {
+        $unset: {
+          etablissement_formateur_code_commune_insee: "",
+          etablissement_formateur_geo_coordonnees: "",
+          date_entrée_formation: "",
+          libelle_court_formation: "",
+        },
+      }
+    );
+  },
+
+  async down() {},
+};
