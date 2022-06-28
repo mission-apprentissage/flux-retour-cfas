cfcf7cd5bdecf8bf5057d0f380c22ee5efd94ebe
Pablo <pablohanry9@gmail.com>
2022-06-28 14:49:32 +0200

    feat(server): add code region corse

--- server/src/common/constants/territoiresConstants.js
+++ server/src/common/constants/territoiresConstants.js
@@ -88,6 +88,7 @@ const DEPARTEMENTS = [
   {
     nom: "Ain",
     code: "01",
+    uaiCode: "01",
     codeRegion: "84",
     region: {
       code: "84",
@@ -97,6 +98,7 @@ const DEPARTEMENTS = [
   {
     nom: "Aisne",
     code: "02",
+    uaiCode: "02",
     codeRegion: "32",
     region: {
       code: "32",
@@ -106,6 +108,7 @@ const DEPARTEMENTS = [
   {
     nom: "Allier",
     code: "03",
+    uaiCode: "03",
     codeRegion: "84",
     region: {
       code: "84",
@@ -115,6 +118,7 @@ const DEPARTEMENTS = [
   {
     nom: "Alpes-de-Haute-Provence",
     code: "04",
+    uaiCode: "04",
     codeRegion: "93",
     region: {
       code: "93",
@@ -124,6 +128,7 @@ const DEPARTEMENTS = [
   {
     nom: "Hautes-Alpes",
     code: "05",
+    uaiCode: "05",
     codeRegion: "93",
     region: {
       code: "93",
@@ -133,6 +138,7 @@ const DEPARTEMENTS = [
   {
     nom: "Alpes-Maritimes",
     code: "06",
+    uaiCode: "06",
     codeRegion: "93",
     region: {
       code: "93",
@@ -142,6 +148,7 @@ const DEPARTEMENTS = [
   {
     nom: "Ardèche",
     code: "07",
+    uaiCode: "07",
     codeRegion: "84",
     region: {
       code: "84",
@@ -151,6 +158,7 @@ const DEPARTEMENTS = [
   {
     nom: "Ardennes",
     code: "08",
+    uaiCode: "08",
     codeRegion: "44",
     region: {
       code: "44",
@@ -160,6 +168,7 @@ const DEPARTEMENTS = [
   {
     nom: "Ariège",
     code: "09",
+    uaiCode: "09",
     codeRegion: "76",
     region: {
       code: "76",
@@ -169,6 +178,7 @@ const DEPARTEMENTS = [
   {
     nom: "Aube",
     code: "10",
+    uaiCode: "10",
     codeRegion: "44",
     region: {
       code: "44",
@@ -178,6 +188,7 @@ const DEPARTEMENTS = [
   {
     nom: "Aude",
     code: "11",
+    uaiCode: "11",
     codeRegion: "76",
     region: {
       code: "76",
@@ -187,6 +198,7 @@ const DEPARTEMENTS = [
   {
     nom: "Aveyron",
     code: "12",
+    uaiCode: "12",
     codeRegion: "76",
     region: {
       code: "76",
@@ -196,6 +208,7 @@ const DEPARTEMENTS = [
   {
     nom: "Bouches-du-Rhône",
     code: "13",
+    uaiCode: "13",
     codeRegion: "93",
     region: {
       code: "93",
@@ -205,6 +218,7 @@ const DEPARTEMENTS = [
   {
     nom: "Calvados",
     code: "14",
+    uaiCode: "14",
     codeRegion: "28",
     region: {
       code: "28",
@@ -214,6 +228,7 @@ const DEPARTEMENTS = [
   {
     nom: "Cantal",
     code: "15",
+    uaiCode: "15",
     codeRegion: "84",
     region: {
       code: "84",
@@ -223,6 +238,7 @@ const DEPARTEMENTS = [
   {
     nom: "Charente",
     code: "16",
+    uaiCode: "16",
     codeRegion: "75",
     region: {
       code: "75",
@@ -232,6 +248,7 @@ const DEPARTEMENTS = [
   {
     nom: "Charente-Maritime",
     code: "17",
+    uaiCode: "17",
     codeRegion: "75",
     region: {
       code: "75",
@@ -241,6 +258,7 @@ const DEPARTEMENTS = [
   {
     nom: "Cher",
     code: "18",
+    uaiCode: "18",
     codeRegion: "24",
     region: {
       code: "24",
@@ -250,6 +268,7 @@ const DEPARTEMENTS = [
   {
     nom: "Corrèze",
     code: "19",
+    uaiCode: "19",
     codeRegion: "75",
     region: {
       code: "75",
@@ -259,6 +278,7 @@ const DEPARTEMENTS = [
   {
     nom: "Côte-d'Or",
     code: "21",
+    uaiCode: "21",
     codeRegion: "27",
     region: {
       code: "27",
@@ -268,6 +288,7 @@ const DEPARTEMENTS = [
   {
     nom: "Côtes-d'Armor",
     code: "22",
+    uaiCode: "22",
     codeRegion: "53",
     region: {
       code: "53",
@@ -277,6 +298,7 @@ const DEPARTEMENTS = [
   {
     nom: "Creuse",
     code: "23",
+    uaiCode: "23",
     codeRegion: "75",
     region: {
       code: "75",
@@ -286,6 +308,7 @@ const DEPARTEMENTS = [
   {
     nom: "Dordogne",
     code: "24",
+    uaiCode: "24",
     codeRegion: "75",
     region: {
       code: "75",
@@ -295,6 +318,7 @@ const DEPARTEMENTS = [
   {
     nom: "Doubs",
     code: "25",
+    uaiCode: "25",
     codeRegion: "27",
     region: {
       code: "27",
@@ -304,6 +328,7 @@ const DEPARTEMENTS = [
   {
     nom: "Drôme",
     code: "26",
+    uaiCode: "26",
     codeRegion: "84",
     region: {
       code: "84",
@@ -313,6 +338,7 @@ const DEPARTEMENTS = [
   {
     nom: "Eure",
     code: "27",
+    uaiCode: "27",
     codeRegion: "28",
     region: {
       code: "28",
@@ -322,6 +348,7 @@ const DEPARTEMENTS = [
   {
     nom: "Eure-et-Loir",
     code: "28",
+    uaiCode: "28",
     codeRegion: "24",
     region: {
       code: "24",
@@ -331,6 +358,7 @@ const DEPARTEMENTS = [
   {
     nom: "Finistère",
     code: "29",
+    uaiCode: "29",
     codeRegion: "53",
     region: {
       code: "53",
@@ -340,6 +368,7 @@ const DEPARTEMENTS = [
   {
     nom: "Corse-du-Sud",
     code: "2A",
+    uaiCode: "2A",
     codeRegion: "94",
     region: {
       code: "94",
@@ -349,6 +378,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Corse",
     code: "2B",
+    uaiCode: "2B",
     codeRegion: "94",
     region: {
       code: "94",
@@ -358,6 +388,7 @@ const DEPARTEMENTS = [
   {
     nom: "Gard",
     code: "30",
+    uaiCode: "30",
     codeRegion: "76",
     region: {
       code: "76",
@@ -367,6 +398,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Garonne",
     code: "31",
+    uaiCode: "31",
     codeRegion: "76",
     region: {
       code: "76",
@@ -376,6 +408,7 @@ const DEPARTEMENTS = [
   {
     nom: "Gers",
     code: "32",
+    uaiCode: "32",
     codeRegion: "76",
     region: {
       code: "76",
@@ -385,6 +418,7 @@ const DEPARTEMENTS = [
   {
     nom: "Gironde",
     code: "33",
+    uaiCode: "33",
     codeRegion: "75",
     region: {
       code: "75",
@@ -394,6 +428,7 @@ const DEPARTEMENTS = [
   {
     nom: "Hérault",
     code: "34",
+    uaiCode: "34",
     codeRegion: "76",
     region: {
       code: "76",
@@ -403,6 +438,7 @@ const DEPARTEMENTS = [
   {
     nom: "Ille-et-Vilaine",
     code: "35",
+    uaiCode: "35",
     codeRegion: "53",
     region: {
       code: "53",
@@ -412,6 +448,7 @@ const DEPARTEMENTS = [
   {
     nom: "Indre",
     code: "36",
+    uaiCode: "36",
     codeRegion: "24",
     region: {
       code: "24",
@@ -421,6 +458,7 @@ const DEPARTEMENTS = [
   {
     nom: "Indre-et-Loire",
     code: "37",
+    uaiCode: "37",
     codeRegion: "24",
     region: {
       code: "24",
@@ -430,6 +468,7 @@ const DEPARTEMENTS = [
   {
     nom: "Isère",
     code: "38",
+    uaiCode: "38",
     codeRegion: "84",
     region: {
       code: "84",
@@ -439,6 +478,7 @@ const DEPARTEMENTS = [
   {
     nom: "Jura",
     code: "39",
+    uaiCode: "39",
     codeRegion: "27",
     region: {
       code: "27",
@@ -448,6 +488,7 @@ const DEPARTEMENTS = [
   {
     nom: "Landes",
     code: "40",
+    uaiCode: "40",
     codeRegion: "75",
     region: {
       code: "75",
@@ -457,6 +498,7 @@ const DEPARTEMENTS = [
   {
     nom: "Loir-et-Cher",
     code: "41",
+    uaiCode: "41",
     codeRegion: "24",
     region: {
       code: "24",
@@ -466,6 +508,7 @@ const DEPARTEMENTS = [
   {
     nom: "Loire",
     code: "42",
+    uaiCode: "42",
     codeRegion: "84",
     region: {
       code: "84",
@@ -475,6 +518,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Loire",
     code: "43",
+    uaiCode: "43",
     codeRegion: "84",
     region: {
       code: "84",
@@ -484,6 +528,7 @@ const DEPARTEMENTS = [
   {
     nom: "Loire-Atlantique",
     code: "44",
+    uaiCode: "44",
     codeRegion: "52",
     region: {
       code: "52",
@@ -493,6 +538,7 @@ const DEPARTEMENTS = [
   {
     nom: "Loiret",
     code: "45",
+    uaiCode: "45",
     codeRegion: "24",
     region: {
       code: "24",
@@ -502,6 +548,7 @@ const DEPARTEMENTS = [
   {
     nom: "Lot",
     code: "46",
+    uaiCode: "46",
     codeRegion: "76",
     region: {
       code: "76",
@@ -511,6 +558,7 @@ const DEPARTEMENTS = [
   {
     nom: "Lot-et-Garonne",
     code: "47",
+    uaiCode: "47",
     codeRegion: "75",
     region: {
       code: "75",
@@ -520,6 +568,7 @@ const DEPARTEMENTS = [
   {
     nom: "Lozère",
     code: "48",
+    uaiCode: "48",
     codeRegion: "76",
     region: {
       code: "76",
@@ -529,6 +578,7 @@ const DEPARTEMENTS = [
   {
     nom: "Maine-et-Loire",
     code: "49",
+    uaiCode: "49",
     codeRegion: "52",
     region: {
       code: "52",
@@ -538,6 +588,7 @@ const DEPARTEMENTS = [
   {
     nom: "Manche",
     code: "50",
+    uaiCode: "50",
     codeRegion: "28",
     region: {
       code: "28",
@@ -547,6 +598,7 @@ const DEPARTEMENTS = [
   {
     nom: "Marne",
     code: "51",
+    uaiCode: "51",
     codeRegion: "44",
     region: {
       code: "44",
@@ -556,6 +608,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Marne",
     code: "52",
+    uaiCode: "52",
     codeRegion: "44",
     region: {
       code: "44",
@@ -565,6 +618,7 @@ const DEPARTEMENTS = [
   {
     nom: "Mayenne",
     code: "53",
+    uaiCode: "53",
     codeRegion: "52",
     region: {
       code: "52",
@@ -574,6 +628,7 @@ const DEPARTEMENTS = [
   {
     nom: "Meurthe-et-Moselle",
     code: "54",
+    uaiCode: "54",
     codeRegion: "44",
     region: {
       code: "44",
@@ -583,6 +638,7 @@ const DEPARTEMENTS = [
   {
     nom: "Meuse",
     code: "55",
+    uaiCode: "55",
     codeRegion: "44",
     region: {
       code: "44",
@@ -592,6 +648,7 @@ const DEPARTEMENTS = [
   {
     nom: "Morbihan",
     code: "56",
+    uaiCode: "56",
     codeRegion: "53",
     region: {
       code: "53",
@@ -601,6 +658,7 @@ const DEPARTEMENTS = [
   {
     nom: "Moselle",
     code: "57",
+    uaiCode: "57",
     codeRegion: "44",
     region: {
       code: "44",
@@ -610,6 +668,7 @@ const DEPARTEMENTS = [
   {
     nom: "Nièvre",
     code: "58",
+    uaiCode: "58",
     codeRegion: "27",
     region: {
       code: "27",
@@ -619,6 +678,7 @@ const DEPARTEMENTS = [
   {
     nom: "Nord",
     code: "59",
+    uaiCode: "59",
     codeRegion: "32",
     region: {
       code: "32",
@@ -628,6 +688,7 @@ const DEPARTEMENTS = [
   {
     nom: "Oise",
     code: "60",
+    uaiCode: "60",
     codeRegion: "32",
     region: {
       code: "32",
@@ -637,6 +698,7 @@ const DEPARTEMENTS = [
   {
     nom: "Orne",
     code: "61",
+    uaiCode: "61",
     codeRegion: "28",
     region: {
       code: "28",
@@ -646,6 +708,7 @@ const DEPARTEMENTS = [
   {
     nom: "Pas-de-Calais",
     code: "62",
+    uaiCode: "62",
     codeRegion: "32",
     region: {
       code: "32",
@@ -655,6 +718,7 @@ const DEPARTEMENTS = [
   {
     nom: "Puy-de-Dôme",
     code: "63",
+    uaiCode: "63",
     codeRegion: "84",
     region: {
       code: "84",
@@ -664,6 +728,7 @@ const DEPARTEMENTS = [
   {
     nom: "Pyrénées-Atlantiques",
     code: "64",
+    uaiCode: "64",
     codeRegion: "75",
     region: {
       code: "75",
@@ -673,6 +738,7 @@ const DEPARTEMENTS = [
   {
     nom: "Hautes-Pyrénées",
     code: "65",
+    uaiCode: "65",
     codeRegion: "76",
     region: {
       code: "76",
@@ -682,6 +748,7 @@ const DEPARTEMENTS = [
   {
     nom: "Pyrénées-Orientales",
     code: "66",
+    uaiCode: "66",
     codeRegion: "76",
     region: {
       code: "76",
@@ -691,6 +758,7 @@ const DEPARTEMENTS = [
   {
     nom: "Bas-Rhin",
     code: "67",
+    uaiCode: "67",
     codeRegion: "44",
     region: {
       code: "44",
@@ -700,6 +768,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haut-Rhin",
     code: "68",
+    uaiCode: "68",
     codeRegion: "44",
     region: {
       code: "44",
@@ -709,6 +778,7 @@ const DEPARTEMENTS = [
   {
     nom: "Rhône",
     code: "69",
+    uaiCode: "69",
     codeRegion: "84",
     region: {
       code: "84",
@@ -718,6 +788,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Saône",
     code: "70",
+    uaiCode: "70",
     codeRegion: "27",
     region: {
       code: "27",
@@ -727,6 +798,7 @@ const DEPARTEMENTS = [
   {
     nom: "Saône-et-Loire",
     code: "71",
+    uaiCode: "71",
     codeRegion: "27",
     region: {
       code: "27",
@@ -736,6 +808,7 @@ const DEPARTEMENTS = [
   {
     nom: "Sarthe",
     code: "72",
+    uaiCode: "72",
     codeRegion: "52",
     region: {
       code: "52",
@@ -745,6 +818,7 @@ const DEPARTEMENTS = [
   {
     nom: "Savoie",
     code: "73",
+    uaiCode: "73",
     codeRegion: "84",
     region: {
       code: "84",
@@ -754,6 +828,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Savoie",
     code: "74",
+    uaiCode: "74",
     codeRegion: "84",
     region: {
       code: "84",
@@ -763,6 +838,7 @@ const DEPARTEMENTS = [
   {
     nom: "Paris",
     code: "75",
+    uaiCode: "75",
     codeRegion: "11",
     region: {
       code: "11",
@@ -772,6 +848,7 @@ const DEPARTEMENTS = [
   {
     nom: "Seine-Maritime",
     code: "76",
+    uaiCode: "76",
     codeRegion: "28",
     region: {
       code: "28",
@@ -781,6 +858,7 @@ const DEPARTEMENTS = [
   {
     nom: "Seine-et-Marne",
     code: "77",
+    uaiCode: "77",
     codeRegion: "11",
     region: {
       code: "11",
@@ -790,6 +868,7 @@ const DEPARTEMENTS = [
   {
     nom: "Yvelines",
     code: "78",
+    uaiCode: "78",
     codeRegion: "11",
     region: {
       code: "11",
@@ -799,6 +878,7 @@ const DEPARTEMENTS = [
   {
     nom: "Deux-Sèvres",
     code: "79",
+    uaiCode: "79",
     codeRegion: "75",
     region: {
       code: "75",
@@ -808,6 +888,7 @@ const DEPARTEMENTS = [
   {
     nom: "Somme",
     code: "80",
+    uaiCode: "80",
     codeRegion: "32",
     region: {
       code: "32",
@@ -817,6 +898,7 @@ const DEPARTEMENTS = [
   {
     nom: "Tarn",
     code: "81",
+    uaiCode: "81",
     codeRegion: "76",
     region: {
       code: "76",
@@ -826,6 +908,7 @@ const DEPARTEMENTS = [
   {
     nom: "Tarn-et-Garonne",
     code: "82",
+    uaiCode: "82",
     codeRegion: "76",
     region: {
       code: "76",
@@ -835,6 +918,7 @@ const DEPARTEMENTS = [
   {
     nom: "Var",
     code: "83",
+    uaiCode: "83",
     codeRegion: "93",
     region: {
       code: "93",
@@ -844,6 +928,7 @@ const DEPARTEMENTS = [
   {
     nom: "Vaucluse",
     code: "84",
+    uaiCode: "84",
     codeRegion: "93",
     region: {
       code: "93",
@@ -853,6 +938,7 @@ const DEPARTEMENTS = [
   {
     nom: "Vendée",
     code: "85",
+    uaiCode: "85",
     codeRegion: "52",
     region: {
       code: "52",
@@ -862,6 +948,7 @@ const DEPARTEMENTS = [
   {
     nom: "Vienne",
     code: "86",
+    uaiCode: "86",
     codeRegion: "75",
     region: {
       code: "75",
@@ -871,6 +958,7 @@ const DEPARTEMENTS = [
   {
     nom: "Haute-Vienne",
     code: "87",
+    uaiCode: "87",
     codeRegion: "75",
     region: {
       code: "75",
@@ -880,6 +968,7 @@ const DEPARTEMENTS = [
   {
     nom: "Vosges",
     code: "88",
+    uaiCode: "88",
     codeRegion: "44",
     region: {
       code: "44",
@@ -889,6 +978,7 @@ const DEPARTEMENTS = [
   {
     nom: "Yonne",
     code: "89",
+    uaiCode: "89",
     codeRegion: "27",
     region: {
       code: "27",
@@ -898,6 +988,7 @@ const DEPARTEMENTS = [
   {
     nom: "Territoire de Belfort",
     code: "90",
+    uaiCode: "90",
     codeRegion: "27",
     region: {
       code: "27",
@@ -907,6 +998,7 @@ const DEPARTEMENTS = [
   {
     nom: "Essonne",
     code: "91",
+    uaiCode: "91",
     codeRegion: "11",
     region: {
       code: "11",
@@ -916,6 +1008,7 @@ const DEPARTEMENTS = [
   {
     nom: "Hauts-de-Seine",
     code: "92",
+    uaiCode: "92",
     codeRegion: "11",
     region: {
       code: "11",
@@ -925,6 +1018,7 @@ const DEPARTEMENTS = [
   {
     nom: "Seine-Saint-Denis",
     code: "93",
+    uaiCode: "93",
     codeRegion: "11",
     region: {
       code: "11",
@@ -934,6 +1028,7 @@ const DEPARTEMENTS = [
   {
     nom: "Val-de-Marne",
     code: "94",
+    uaiCode: "94",
     codeRegion: "11",
     region: {
       code: "11",
@@ -943,6 +1038,7 @@ const DEPARTEMENTS = [
   {
     nom: "Val-d'Oise",
     code: "95",
+    uaiCode: "95",
     codeRegion: "11",
     region: {
       code: "11",
@@ -952,6 +1048,7 @@ const DEPARTEMENTS = [
   {
     nom: "Guadeloupe",
     code: "971",
+    uaiCode: "971",
     codeRegion: "01",
     region: {
       code: "01",
@@ -961,6 +1058,7 @@ const DEPARTEMENTS = [
   {
     nom: "Martinique",
     code: "972",
+    uaiCode: "972",
     codeRegion: "02",
     region: {
       code: "02",
@@ -970,6 +1068,7 @@ const DEPARTEMENTS = [
   {
     nom: "Guyane",
     code: "973",
+    uaiCode: "973",
     codeRegion: "03",
     region: {
       code: "03",
@@ -979,6 +1078,7 @@ const DEPARTEMENTS = [
   {
     nom: "La Réunion",
     code: "974",
+    uaiCode: "974",
     codeRegion: "04",
     region: {
       code: "04",
@@ -988,10 +1088,31 @@ const DEPARTEMENTS = [
   {
     nom: "Mayotte",
     code: "976",
+    uaiCode: "976",
     codeRegion: "06",
     region: {
       code: "06",
       nom: "Mayotte",
+    },
+  },
+  {
+    nom: "Corse-du-Sud",
+    code: "620",
+    uaiCode: "620",
+    codeRegion: "94",
+    region: {
+      code: "94",
+      nom: "Corse",
+    },
+  },
+  {
+    nom: "Haute-Corse",
+    code: "720",
+    uaiCode: "720",
+    codeRegion: "94",
+    region: {
+      code: "94",
+      nom: "Corse",
     },
   },
 ];
