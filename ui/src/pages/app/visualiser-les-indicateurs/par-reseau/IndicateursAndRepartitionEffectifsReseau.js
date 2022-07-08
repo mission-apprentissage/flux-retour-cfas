798f910e62a3181d231366b9e4e3d07be4ca9690
Samir <sbenfares@gmail.com>
2022-07-08 15:25:01 +0200

    feat(ui): export csv for réseau

--- ui/src/pages/app/visualiser-les-indicateurs/par-reseau/IndicateursAndRepartitionEffectifsReseau.js
+++ ui/src/pages/app/visualiser-les-indicateurs/par-reseau/IndicateursAndRepartitionEffectifsReseau.js
@@ -1,12 +1,16 @@
+import { Stack } from "@chakra-ui/react";
 import { TabPanel } from "@chakra-ui/tabs";
 import PropTypes from "prop-types";
 import React from "react";
 
+import { fetchEffectifsAnonymizedDataListCsvExport } from "../../../../common/api/tableauDeBord";
 import { RepartitionEffectifsTabs, Section } from "../../../../common/components";
+import DownloadBlock from "../../../../common/components/DownloadBlock/DownloadBlock";
 import RepartitionEffectifsParCfa from "../../../../common/components/tables/RepartitionEffectifsParCfa";
 import RepartitionEffectifsParFormation from "../../../../common/components/tables/RepartitionEffectifsParFormation";
 import useFetchEffectifsParCfa from "../../../../common/hooks/useFetchEffectifsParCfa";
 import useFetchEffectifsParNiveauFormation from "../../../../common/hooks/useFetchEffectifsParNiveauFormation";
+import { mapFiltersToApiFormat } from "../../../../common/utils/mapFiltersToApiFormat";
 import DateWithTooltipSelector from "../DateWithTooltipSelector";
 import { filtersPropTypes } from "../FiltersContext";
 import IndicateursGridStack from "../IndicateursGridStack";
@@ -24,11 +28,21 @@ const IndicateursAndRepartitionEffectifsReseau = ({ filters, effectifs, loading,
     error: effectifsParNiveauFormationError,
   } = useFetchEffectifsParNiveauFormation(filters);
 
+  const exportFilename = `tdb-données-réseau-${filters.reseau?.nom}-${new Date().toLocaleDateString()}.csv`;
+
   return (
     <Section paddingY="4w">
       <RepartitionEffectifsTabs>
         <TabPanel paddingTop="4w">
-          <IndicateursGridStack effectifs={effectifs} loading={loading} showOrganismesCount={showOrganismesCount} />
+          <Stack spacing="4w">
+            <IndicateursGridStack effectifs={effectifs} loading={loading} showOrganismesCount={showOrganismesCount} />
+            <DownloadBlock
+              title="Télécharger les données du réseau sélectionné"
+              description="Le fichier est généré à date du jour, en fonction du réseau sélectionné et comprend la liste anonymisée des apprenants par organisme et formation."
+              fileName={exportFilename}
+              getFile={() => fetchEffectifsAnonymizedDataListCsvExport(mapFiltersToApiFormat(filters))}
+            />
+          </Stack>
         </TabPanel>
         <TabPanel paddingTop="4w">
           <DateWithTooltipSelector marginBottom="1w" />
