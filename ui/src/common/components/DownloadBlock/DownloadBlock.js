480d964949dd2ef7164e3921c6fa0925a9e5c8c9
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-07-13 18:23:44 +0200

    Improve DownloadBlock component layout and code

--- ui/src/common/components/DownloadBlock/DownloadBlock.js
+++ ui/src/common/components/DownloadBlock/DownloadBlock.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Link, Spacer, Spinner, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Spacer, Spinner, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -9,42 +9,37 @@ const DownloadBlock = ({ title, description, fileName, getFile }) => {
   const fileFormatOrName = fileName?.split(".").pop().toUpperCase();
 
   return (
-    <Link style={{ textDecoration: "none" }} onClick={onClick}>
-      <Box
-        background="white"
-        fontSize="gamma"
-        width="100%"
-        paddingY="4w"
-        paddingX="4w"
-        border="1px solid"
-        borderColor="#E5E5E5"
-        fontWeight="bold"
-        flex="1"
-      >
-        {isLoading && (
-          <Box>
-            <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
-          </Box>
-        )}
-        {!isLoading && (
-          <Box>
-            <Text color="black" marginBottom="2w">
-              {title}
+    <Box
+      background="white"
+      fontSize="gamma"
+      width="100%"
+      padding="4w"
+      border="1px solid"
+      borderColor="#E5E5E5"
+      fontWeight="bold"
+    >
+      {isLoading ? (
+        <Center>
+          <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+        </Center>
+      ) : (
+        <Box onClick={onClick} cursor="pointer">
+          <Text color="black" marginBottom="2w">
+            {title}
+          </Text>
+          <Text color="#3A3A3A" fontWeight="400" marginBottom="2w" fontSize="zeta">
+            {description}
+          </Text>
+          <Flex>
+            <Text color="#666666" fontWeight="400" marginBottom="2w" fontSize="omega">
+              {fileFormatOrName}
             </Text>
-            <Text color="#3A3A3A" fontWeight="400" marginBottom="2w" fontSize="zeta">
-              {description}
-            </Text>
-            <Flex>
-              <Text color="#666666" fontWeight="400" marginBottom="2w" fontSize="omega">
-                {fileFormatOrName}
-              </Text>
-              <Spacer />
-              <Box as="i" className="ri-download-line" fontWeight="400" />
-            </Flex>
-          </Box>
-        )}
-      </Box>
-    </Link>
+            <Spacer />
+            <Box as="i" className="ri-download-line" fontWeight="400" />
+          </Flex>
+        </Box>
+      )}
+    </Box>
   );
 };
 
