91d3a6ff162ff9302d56c016fb08301ba3b02e16
Samir B <sbenfares@gmail.com>
2022-07-26 10:37:06 +0200

    feat(ui): add pagination to usersTable (#1561)
    wip

--- ui/src/pages/admin/gestion-utilisateurs/UsersTable.js
+++ ui/src/pages/admin/gestion-utilisateurs/UsersTable.js
@@ -1,9 +1,9 @@
-import { Tbody, Td, Tr } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Table, TableCaption, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import Pagination from "@choc-ui/paginator";
+import React, { forwardRef } from "react";
 import { useQuery } from "react-query";
 
 import { fetchUsers } from "../../../common/api/tableauDeBord";
-import { Table } from "../../../common/components";
 import { QUERY_KEYS } from "../../../common/constants/queryKeys";
 import { formatDate } from "../../../common/utils/dateUtils";
 import GetUpdatePasswordUrlButton from "./GetUpdatePasswordUrlButton";
@@ -20,17 +20,72 @@ const getUsersListSortedChronologically = (users) => {
   return [...usersWithCreationDateSorted, ...usersWithoutCreationDate];
 };
 
+const Prev = forwardRef((props, ref) => (
+  <Button ref={ref} {...props}>
+    <Box as="i" className="ri-arrow-left-s-line" />
+  </Button>
+));
+const Next = forwardRef((props, ref) => (
+  <Button ref={ref} {...props}>
+    <Box as="i" className="ri-arrow-right-s-line" />
+  </Button>
+));
+
+const itemRender = (_, type) => {
+  if (type === "prev") {
+    return Prev;
+  }
+  if (type === "next") {
+    return Next;
+  }
+};
+
+Prev.displayName = "prev";
+Next.displayName = "next";
+
 const UsersTable = () => {
-  const { data, isLoading } = useQuery([QUERY_KEYS.USERS], () => fetchUsers());
+  const { data } = useQuery([QUERY_KEYS.USERS], () => fetchUsers());
   const usersList = data && getUsersListSortedChronologically(data);
+  const [current, setCurrent] = React.useState(1);
+
+  const pageSize = 10;
+  const offset = (current - 1) * pageSize;
+  const dataSliced = usersList?.slice(offset, offset + pageSize);
 
   return (
-    <Table
-      headers={["Nom d'utilisateur", "Roles", "Email", "Réseau", "Date de création", "Modifier mot de passe"]}
-      loading={isLoading}
-    >
+    <Table variant="secondary">
+      <TableCaption>
+        <Pagination
+          current={current}
+          onChange={(page) => {
+            setCurrent(page);
+          }}
+          pageSize={pageSize}
+          total={usersList?.length}
+          itemRender={itemRender}
+          paginationProps={{
+            display: "flex",
+            pos: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+          baseStyles={{ bg: "white" }}
+          activeStyles={{ bg: "bluefrance", color: "white", pointerEvents: "none" }}
+          hoverStyles={{ bg: "galt", color: "grey.800" }}
+        />
+      </TableCaption>
+      <Thead>
+        <Tr background="galt">
+          <Th>Nom d&apos;utilisateur</Th>
+          <Th>Roles</Th>
+          <Th>Email</Th>
+          <Th>Réseau</Th>
+          <Th>Date de création</Th>
+          <Th>Modifier mot de passe</Th>
+        </Tr>
+      </Thead>
       <Tbody>
-        {usersList?.map((user) => {
+        {dataSliced?.map((user) => {
           return (
             <Tr key={user.username}>
               <Td color="bluefrance">{user.username}</Td>
