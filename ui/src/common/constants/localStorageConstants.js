b11e41392792873f20ffb93bac1580c3d52b2422
Samir B <sbenfares@gmail.com>
2022-08-17 10:27:30 +0200

    refacto(ui): create constants for localStorage (#1656)

---
+++ ui/src/common/constants/localStorageConstants.js
@@ -0,0 +1,3 @@
+export const LOCAL_STORAGE_ACCESS_TOKEN = "flux-retour-cfas:access_token";
+export const LOCAL_STORAGE_USER_PERMISSIONS = "flux-retour-cfas:userPermissions";
+export const LOCAL_STORAGE_USER_NETWORK = "flux-retour-cfas:userNetwork";
