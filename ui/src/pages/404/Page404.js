2bd08efc8874ec61e3671a78bb463db6cc30560f
Samir B <sbenfares@gmail.com>
2022-09-27 10:10:28 +0200

    feat(ui): page 404 (#1788)
    * feat(ui): page 404
    
    * feat(ui): replace svg file by component (thx to Pablo)
    
    * feat(ui): rename bg to background

--- ui/src/pages/404/Page404.js
+++ ui/src/pages/404/Page404.js
@@ -1,25 +1,38 @@
-import { Heading, Link, Text } from "@chakra-ui/react";
+import { Box, Center, Heading, Link, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
-import { BreadcrumbNav, Page, Section } from "../../common/components";
+import { Page, Section } from "../../common/components";
 import { NAVIGATION_PAGES } from "../../common/constants/navigationPages";
+import { NotFound } from "../../theme/components/icons";
 
 const Page404 = () => {
   return (
     <Page>
-      <Section withShadow backgroundColor="galt" paddingY="2w" color="grey.800">
-        <BreadcrumbNav links={[NAVIGATION_PAGES.Accueil, NAVIGATION_PAGES.NotFound404]} />
-        <Heading as="h1" variant="h1" marginTop="1w">
-          {NAVIGATION_PAGES.NotFound404.title}
-        </Heading>
-      </Section>
-      <Section paddingY="4w">
-        <Text color="grey.800" marginBottom="2w">
-          La page que vous recherchez n&apos;a pas été trouvée.
-        </Text>
-        <Link href={NAVIGATION_PAGES.Accueil.path} color="bluefrance">
-          Retourner à l&apos;accueil
-        </Link>
+      <Section withShadow color="grey.800" paddingY="4w">
+        <Center>
+          <Box width="769px" border="1px solid" borderColor="#E3E3FD" padding="4w" marginTop="6w">
+            <Stack alignItems="center" spacing="4w">
+              <NotFound />
+              <Heading fontSize="28px" fontWeight="bold">
+                Page non trouvée
+              </Heading>
+              <Text color="grey.800" fontSize="zeta" marginBottom="2w">
+                La page que vous recherchez n’existe pas ou a été déplacée
+              </Text>
+
+              <Link
+                href={NAVIGATION_PAGES.Accueil.path}
+                _hover={{ textDecoration: "none", color: "grey.800", background: "galt" }}
+                color="bluefrance"
+              >
+                <Box as="i" className="ri-arrow-left-line" marginRight="1w" verticalAlign="middle" />
+                <Box as="span" verticalAlign="middle">
+                  Retourner à la page d&apos;accueil
+                </Box>
+              </Link>
+            </Stack>
+          </Box>
+        </Center>
       </Section>
     </Page>
   );
