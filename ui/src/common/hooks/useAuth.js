c4f708931a6ca8986d79004a13a764bb757c78de
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-10-06 18:04:57 +0200

    refactor: supprime les permissions et le reseau du user connecté du local storage

--- ui/src/common/hooks/useAuth.js
+++ ui/src/common/hooks/useAuth.js
@@ -1,11 +1,7 @@
 import { useCallback } from "react";
 
 import { useAuthState } from "../auth/auth";
-import {
-  LOCAL_STORAGE_ACCESS_TOKEN,
-  LOCAL_STORAGE_USER_NETWORK,
-  LOCAL_STORAGE_USER_PERMISSIONS,
-} from "../constants/localStorageConstants";
+import { LOCAL_STORAGE_ACCESS_TOKEN } from "../constants/localStorageConstants";
 import decodeJWT from "../utils/decodeJWT";
 
 export default function useAuth() {
@@ -14,15 +10,11 @@ export default function useAuth() {
   const setAuthFromToken = (access_token) => {
     const decodedAccessToken = decodeJWT(access_token);
     localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN, access_token);
-    localStorage.setItem(LOCAL_STORAGE_USER_PERMISSIONS, decodedAccessToken.permissions);
-    localStorage.setItem(LOCAL_STORAGE_USER_NETWORK, decodedAccessToken.network);
     setAuth(decodedAccessToken);
   };
 
   const resetAuthState = useCallback(() => {
     localStorage.removeItem(LOCAL_STORAGE_ACCESS_TOKEN);
-    localStorage.removeItem(LOCAL_STORAGE_USER_PERMISSIONS);
-    localStorage.removeItem(LOCAL_STORAGE_USER_NETWORK);
     setAuth(null);
   }, [setAuth]);
 
