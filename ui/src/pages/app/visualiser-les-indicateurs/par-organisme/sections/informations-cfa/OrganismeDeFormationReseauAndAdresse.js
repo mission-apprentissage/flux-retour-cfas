6554662c4f72ca5d7199692d4c4e351c5a1aae05
Samir BENFARES <sbenfares@gmail.com>
2022-10-28 14:43:32 +0200

    feat(ui): refacto OrganismesDeFormationsReseauxAdresse

--- ui/src/pages/app/visualiser-les-indicateurs/par-organisme/sections/informations-cfa/OrganismeDeFormationReseauAndAdresse.js
+++ ui/src/pages/app/visualiser-les-indicateurs/par-organisme/sections/informations-cfa/OrganismeDeFormationReseauAndAdresse.js
@@ -1,35 +1,32 @@
-import { Text } from "@chakra-ui/react";
+import { Box, HStack, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import React from "react";
 
 const OrganismeDeFormationReseauAndAdresse = ({ reseaux, adresse, multipleSirets, nbEtablissements }) => {
-  const hasReseaux = reseaux?.length > 0;
-  const getOrganismeReseauxAndAdresseText = () => {
-    if (hasReseaux) {
-      return (
-        <>
-          Cet organisme fait partie du réseau <strong>{reseaux[0]}</strong>{" "}
-          {reseaux.slice(1, reseaux.length)?.map((item) => (
-            <>
-              et du réseau <strong>{item}</strong>
-            </>
-          ))}
-          . {adresse ? `Sa domiciliation est ${adresse}.` : ""}
-        </>
-      );
-    } else {
-      if (adresse) {
-        return <>La domiciliation de cet organisme est {adresse}</>;
-      }
-      return null;
-    }
-  };
+  const hasReseaux = reseaux.length > 1;
+  const reseauxSliced = reseaux.slice(1, reseaux.length);
+  const getOrganismeReseaux = () => (
+    <HStack>
+      <Text>
+        Cet organisme fait partie du réseau <strong>{reseaux[0]}</strong>{" "}
+      </Text>
+      {reseauxSliced?.map((item, index) => {
+        return (
+          <Text key={index}>
+            et du réseau <strong>{item}</strong>
+          </Text>
+        );
+      })}
+    </HStack>
+  );
 
   return (
-    <Text fontSize="epsilon" textColor="grey.800" marginTop="3w">
-      {getOrganismeReseauxAndAdresseText()}
+    <Box fontSize="epsilon" textColor="grey.800" marginTop="3w">
+      {hasReseaux && getOrganismeReseaux()}
+      {hasReseaux && adresse ? `Sa domiciliation est ${adresse}.` : ""}
+      {!hasReseaux && adresse && <Text>La domiciliation de cet organisme est {adresse}</Text>}
       {multipleSirets && <strong> Il est identifié par une UAI qui utilise {nbEtablissements} numéros SIRET.</strong>}
-    </Text>
+    </Box>
   );
 };
 
