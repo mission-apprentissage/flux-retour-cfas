7f0949d9b098b4f56e50642ce2bf53597e73307e
Pablo <69269013+pabloHry@users.noreply.github.com>
2022-11-02 09:24:19 +0100

    Ajout de l'adresse mail au texte du footer et suppression de ce texte sur le lien (#2064)

--- ui/src/common/components/ContactSection/ContactSection.js
+++ ui/src/common/components/ContactSection/ContactSection.js
@@ -16,9 +16,13 @@ const ContactSection = () => {
         <Stack>
           <Text color="grey.800">
             Le service {PRODUCT_NAME} est porté par la Mission interministérielle pour l’apprentissage. Vous avez besoin
-            d’en savoir plus sur les données collectées, les différents types d’accès aux données, etc...
+            d’en savoir plus sur les données collectées, les différents types d’accès aux données, etc. Contacter
+            l&apos;équipe :{" "}
+            <Link href={`mailto:${CONTACT_ADDRESS}`} color="bluefrance" whiteSpace="nowrap">
+              <Text as="span">{CONTACT_ADDRESS}</Text>
+            </Link>
           </Text>
-          <Link as={NavLink} to={NAVIGATION_PAGES.QuestionsReponses.path} color="bluefrance" whiteSpace="nowrap">
+          <Link as={NavLink} to={NAVIGATION_PAGES.QuestionsReponses.path} color="bluefrance">
             <HStack>
               <Text>Consulter la page d&apos;aide</Text>
               <Box marginTop="2w" className="ri-arrow-right-line" />
@@ -26,7 +30,7 @@ const ContactSection = () => {
           </Link>
           <Link href={`mailto:${CONTACT_ADDRESS}`} color="bluefrance" whiteSpace="nowrap">
             <HStack>
-              <Text>Contacter l’équipe {CONTACT_ADDRESS}</Text>
+              <Text>Contacter l’équipe</Text>
               <Box marginTop="2w" className="ri-arrow-right-line" />
             </HStack>
           </Link>
