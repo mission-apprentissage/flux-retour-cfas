5140906813ea08822c667e6792c9370f6af4a94b
Samir BENFARES <sbenfares@gmail.com>
2022-11-11 09:25:31 +0100

    refacto(server): add test & named index for cfasModel

---
+++ server/tests/integration/db/indexes/cfasIndexes.test.js
@@ -0,0 +1,41 @@
+const assert = require("assert").strict;
+const { cfasDb } = require("../../../../src/common/model/collections");
+const { createIndexes, dropIndexes } = require("../../../../src/common/model/indexes/index");
+const { getDbCollectionIndexes } = require("../../../../src/common/mongodb");
+const cfasModelDescriptor = require("./../../../../src/common/model/cfas.model");
+
+describe("Cfas Indexes", () => {
+  let cfasIndexes = [];
+
+  beforeEach(async () => {
+    // Crée une entrée en base
+    const uai = "0802004U";
+    await cfasDb().insertOne({ uai });
+
+    // Re-créé les indexs après l'ajout d'une entrée en base & récupère les indexes
+    await dropIndexes();
+    await createIndexes();
+    cfasIndexes = await getDbCollectionIndexes(cfasModelDescriptor.collectionName);
+  });
+
+  it("Vérifie l'existence d'un index sur le champ nom", async () => {
+    assert.equal(
+      cfasIndexes.some((item) => item.name === "nom_text_nom_tokenized_text"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ uai", async () => {
+    assert.equal(
+      cfasIndexes.some((item) => item.name === "uai"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ sirets", async () => {
+    assert.equal(
+      cfasIndexes.some((item) => item.name === "sirets"),
+      true
+    );
+  });
+});
