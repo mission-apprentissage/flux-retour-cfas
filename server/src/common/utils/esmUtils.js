8d521db51e264a5fa79a27ad9edcca4222520f40
Samir BENFARES <sbenfares@gmail.com>
2022-11-12 20:55:01 +0100

    chore: update ESM libs & default exports

--- server/src/common/utils/esmUtils.js
+++ server/src/common/utils/esmUtils.js
@@ -1,4 +1,9 @@
+/* eslint-disable import/no-commonjs */
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+export const packageJson = require("../../../package.json");
 
 export const getDirname = (base) => dirname(fileURLToPath(base));
