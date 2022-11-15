0b64d2c99da7909dfb0c70bc6ca49e32269ef11e
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-11-15 16:13:57 +0100

    fix(server): fix le schema de validation du modele dossiersApprenants et supprime champs inutiles (migration)

---
+++ server/migrations/20221115150328-supprime-donnees-etablissement-inutiles-modele-dossiersApprenants.js
@@ -0,0 +1,22 @@
+module.exports = {
+  async up(db) {
+    db.collection("dossiersApprenants").updateMany(
+      {},
+      {
+        $unset: {
+          etablissement_formateur_code_postal: "",
+          etablissement_geo_coordonnees: "",
+          etablissement_formateur_uai: "",
+          etablissement_gestionnaire_uai: "",
+          etablissement_num_academie: "",
+          etablissement_nom_academie: "",
+          etablissement_localite: "",
+          etablissement_formateur_ville: "",
+          siret_catalogue: "",
+        },
+      }
+    );
+  },
+
+  async down() {},
+};
