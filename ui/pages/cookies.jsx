75d4d4b07458e940824a28f6642963810abd5b28
Antoine Bigard <bigard.antoine@gmail.com>
2022-11-15 14:49:23 +0100

    chore: cookie as ex

--- ui/pages/cookies.jsx
+++ ui/pages/cookies.jsx
@@ -16,7 +16,7 @@ const CookiePage = () => {
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box w="100%" pt={[4, 8]} px={[1, 1, 12, 24]}>
+      <Box w="100%" pt={[4, 8]} px={[1, 1, 6, 8]}>
         <Container maxW="xl">
           <Breadcrumb pages={[{ title: "Accueil", to: "/" }, { title: title }]} />
           <Heading textStyle="h2" color="grey.800" mt={5}>
