976d64a8b8bb2c5bb9666580a66087de39f4e740
Pablo <pablohanry9@gmail.com>
2022-11-16 14:52:34 +0100

    feat(ui): responsive tag header

--- ui/components/Page/components/Header.jsx
+++ ui/components/Page/components/Header.jsx
@@ -8,20 +8,21 @@ const Header = () => {
   return (
     <Container maxW={"full"} borderBottom={"1px solid"} borderColor={"grey.400"} px={[0, 4]} as="header">
       <Container maxW="xl" py={[0, 2]} px={[0, 4]}>
-        <Flex alignItems="center" color="grey.800">
+        <Flex flexDirection={["column", "column", "column", "row"]} alignItems="center" color="grey.800">
           {/* Logo */}
-          <Link href="/" p={[4, 0]}>
-            <Logo />
-          </Link>
-
-          <Box marginLeft="5w">
-            <Heading as="h6" variant="h1" fontSize="gamma">
-              Le {PRODUCT_NAME}{" "}
-            </Heading>
-            <Text fontFamily="Marianne" color="grey.700" fontSize="zeta">
-              Mettre à disposition des différents acteurs les données clés de l&apos;apprentissage en temps réel
-            </Text>
-          </Box>
+          <Flex alignItems="center">
+            <Link href="/" p={[4, 0]}>
+              <Logo />
+            </Link>
+            <Box marginLeft="5w">
+              <Heading as="h6" variant="h1" fontSize="gamma">
+                Le {PRODUCT_NAME}{" "}
+              </Heading>
+              <Text fontFamily="Marianne" color="grey.700" fontSize="zeta">
+                Mettre à disposition des différents acteurs les données clés de l&apos;apprentissage en temps réel
+              </Text>
+            </Box>
+          </Flex>
           <Tag marginBottom="1w" backgroundColor="bluefrance" color="white" ml="5">
             Beta-V1
           </Tag>
