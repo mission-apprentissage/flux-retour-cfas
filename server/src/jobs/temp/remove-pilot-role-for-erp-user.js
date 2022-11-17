dcef16562901a5ba1f35ce964e73a293926e6eab
stephane-ruhlmann <sj.ruhlmann@gmail.com>
2022-11-17 11:04:55 +0100

    chore: supprime script temporaire permissions ERP pilote

--- server/src/jobs/temp/remove-pilot-role-for-erp-user.js
+++
@@ -1,8 +0,0 @@
-const { runScript } = require("../scriptWrapper");
-const { apiRoles } = require("../../common/roles");
-
-runScript(async ({ db }) => {
-  await db
-    .collection("users")
-    .updateMany({ permissions: apiRoles.apiStatutsSeeder }, { $set: { permissions: [apiRoles.apiStatutsSeeder] } });
-}, "remove-pilot-role-for-erp-user");
