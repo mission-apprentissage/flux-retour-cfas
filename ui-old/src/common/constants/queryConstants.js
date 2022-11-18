d9b9b6698dba0360fa5b77e03388aec1aa04a51f
Antoine Bigard <bigard.antoine@gmail.com>
2022-11-18 10:39:07 +0100

    chore: clean p1

--- ui-old/src/common/constants/queryConstants.js
+++
@@ -1,1 +0,0 @@
-export const SEARCH_CFAS = "Tableau de bord de l'apprentissage";
--- ui-old/src/common/utils/pick.js
+++
@@ -1,5 +0,0 @@
-export const pick = (obj = {}, keys = []) => {
-  return keys.reduce((acc, key) => {
-    return obj[key] !== undefined ? { ...acc, [key]: obj[key] } : acc;
-  }, {});
-};
