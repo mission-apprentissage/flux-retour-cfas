c5816974797691edf62e8e68fb0386f6ac39dce5
Antoine Bigard <bigard.antoine@gmail.com>
2022-11-23 10:30:12 +0100

    chore: add checksum uai verif

--- server/src/common/utils/uaiUtils.js
+++ server/src/common/utils/uaiUtils.js
@@ -31,3 +31,48 @@ export const getDepartementCodeFromUai = (uai) => {
   const code = uai.slice(0, 3);
   return Number(code) < 10 ? `0${Number(code)}` : Number(code).toString();
 };
+
+const ALPHABET_23_LETTERS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "p",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+function computeChecksumUAI(numbers) {
+  if (!numbers || numbers.length !== 7) {
+    throw new Error(`Le code ne doit contenir que 7 caractères sans le checksum`);
+  }
+
+  return ALPHABET_23_LETTERS[numbers % 23];
+}
+
+export function algoUAI(code) {
+  if (!code || code.length !== 8) {
+    return false;
+  }
+
+  let numbers = code.substring(0, 7);
+  let checksum = code.substring(7, 8).toLowerCase();
+
+  return checksum === computeChecksumUAI(numbers);
+}
