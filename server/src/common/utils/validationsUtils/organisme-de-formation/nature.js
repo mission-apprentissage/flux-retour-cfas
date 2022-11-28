9552b97ea8524699f619b3c75bf06612454819df
Antoine Bigard <bigard.antoine@gmail.com>
2022-11-28 14:48:52 +0100

    chore: wip

--- server/src/common/utils/validationsUtils/organisme-de-formation/nature.js
+++ server/src/common/utils/validationsUtils/organisme-de-formation/nature.js
@@ -4,6 +4,7 @@ export const NATURE_ORGANISME_DE_FORMATION = {
   RESPONSABLE: "responsable",
   FORMATEUR: "formateur",
   RESPONSABLE_FORMATEUR: "responsable_formateur",
+  LIEU: "lieu_formation",
   INCONNUE: "inconnue",
 };
 
