7e5ea6f4bb53041a3cefb6ee4f5ec63686746363
Samir BENFARES <sbenfares@gmail.com>
2022-11-30 12:15:22 +0100

    refacto: hydrate formations

--- server/src/jobs/hydrate/formations/hydrate-formations.js
+++ server/src/jobs/hydrate/formations/hydrate-formations.js
@@ -2,7 +2,7 @@ import logger from "../../../common/logger.js";
 import { asyncForEach } from "../../../common/utils/asyncUtils.js";
 import { dossiersApprenantsMigrationDb } from "../../../common/model/collections.js";
 import { sleep } from "../../../common/utils/miscUtils.js";
-import { createFormation, existsFormation, findFormationById } from "../../../common/actions/formations.actions.js";
+import { createFormation, findFormationById, getFormationWithCfd } from "../../../common/actions/formations.actions.js";
 
 const SLEEP_TIME_BETWEEN_CREATION = 100; // 100ms to avoid flooding TCO and LBA APIs
 
@@ -11,6 +11,8 @@ const SLEEP_TIME_BETWEEN_CREATION = 100; // 100ms to avoid flooding TCO and LBA 
  */
 export const hydrateFormations = async () => {
   let createdFormationsTotal = 0;
+  let alreadyPresentFormationsTotal = 0;
+
   let dossiersApprenantUpdatedTotal = 0;
   let notCreatedFormationsTotal = 0;
 
@@ -19,60 +21,59 @@ export const hydrateFormations = async () => {
   logger.info(allCfds.length, "distinct CFD found in collection DossierApprenant");
 
   await asyncForEach(allCfds, async (cfd) => {
-    const formationExistsInDb = await existsFormation(cfd);
+    const formationFound = await getFormationWithCfd(cfd);
 
     // Gestion des nouveaux CFD
-    if (!formationExistsInDb) {
-      // Crée une formation pour chaque nouveau code CFD et maj les dossiers apprenants liés
-      const { createdFormationsCount, dossiersApprenantUpdatedCount, notCreatedFormationsCount } =
-        await createFormationInReferentielAndUpdateDossiersApprenants(cfd);
+    if (!formationFound) {
+      try {
+        // Crée une formation
+        const createdFormationId = await createFormation(cfd);
+        createdFormationsTotal++;
 
-      createdFormationsTotal += createdFormationsCount;
-      dossiersApprenantUpdatedTotal += dossiersApprenantUpdatedCount;
-      notCreatedFormationsTotal += notCreatedFormationsCount;
+        // MAJ les dossiers liés à cette nouvelle formation créé
+        const formationCreated = await findFormationById(createdFormationId);
+        const modifiedCount = await updateDossiersApprenantsFormation(formationCreated);
+        dossiersApprenantUpdatedTotal += modifiedCount;
 
-      await sleep(SLEEP_TIME_BETWEEN_CREATION);
+        // Wait for api calls
+        await sleep(SLEEP_TIME_BETWEEN_CREATION);
+      } catch (err) {
+        logger.error("error while creating formation for CFD", cfd, err);
+        notCreatedFormationsTotal++;
+      }
+    } else {
+      alreadyPresentFormationsTotal++;
+
+      // Gestion des CFD existants on update les dossiersApprenants liés
+      const modifiedCount = await updateDossiersApprenantsFormation(formationFound);
+      dossiersApprenantUpdatedTotal += modifiedCount;
     }
   });
 
   logger.info(`${createdFormationsTotal} formations created in DB`);
   logger.warn(`${notCreatedFormationsTotal} formations could not be created`);
+  logger.info(`${alreadyPresentFormationsTotal} formations already present in DB`);
   logger.info(`${dossiersApprenantUpdatedTotal} dossiers apprenants updated with formation info`);
 };
 
 /**
- * Fonction de création des formations dans le référentiel et MAJ des dossiers rattachés
- * @param {*} cfd
+ * Fonction de maj des dossiersApprenants lié à une formation
+ * va lier l'ID est les infos de formation (niveau & niveau_libelle pour le moment)
+ * @param {*} formation
  * @returns
  */
-const createFormationInReferentielAndUpdateDossiersApprenants = async (cfd) => {
-  let createdFormationsCount = 0;
-  let dossiersApprenantUpdatedCount = 0;
-  let notCreatedFormationsCount = 0;
-
-  try {
-    const createdFormationId = await createFormation(cfd);
-    createdFormationsCount++;
-    const formationCreated = await findFormationById(createdFormationId);
-    const dossiersApprenantsUpdateResults = await dossiersApprenantsMigrationDb().updateMany(
-      { formation_cfd: cfd },
-      {
-        $set: {
-          formation_id: createdFormationId, // Added formation id in dossierApprenant
-          // TODO add when dispo in TCO : duree: createdFormation.duree,
-          // TODO add when dispo in TCO : annee: createdFormation.annee,
-          niveau_formation: formationCreated.niveau,
-          niveau_formation_libelle: formationCreated.niveau_libelle,
-        },
-      }
-    );
-    dossiersApprenantUpdatedCount += dossiersApprenantsUpdateResults.modifiedCount;
-  } catch (err) {
-    logger.error("error while creating formation for CFD", cfd, err);
-    notCreatedFormationsCount++;
-  }
-
-  await sleep(SLEEP_TIME_BETWEEN_CREATION);
-
-  return { createdFormationsCount, dossiersApprenantUpdatedCount, notCreatedFormationsCount };
+const updateDossiersApprenantsFormation = async (formation) => {
+  const { modifiedCount } = await dossiersApprenantsMigrationDb().updateMany(
+    { formation_cfd: formation.cfd },
+    {
+      $set: {
+        formation_id: formation._id, // Added formation id in dossierApprenant
+        // TODO add when dispo in TCO : duree: createdFormation.duree,
+        // TODO add when dispo in TCO : annee: createdFormation.annee,
+        niveau_formation: formation.niveau,
+        niveau_formation_libelle: formation.niveau_libelle,
+      },
+    }
+  );
+  return modifiedCount;
 };
