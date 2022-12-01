a3d0384461a79068c9ef9fd3d6bae99f2fe21d9f
Samir BENFARES <sbenfares@gmail.com>
2022-12-01 16:55:56 +0100

    wip: rebuild tests indexes
    wip

---
+++ server/tests/integration/db/indexes/dossiersApprenants.indexes.test.js
@@ -0,0 +1,69 @@
+import { strict as assert } from "assert";
+import { createIndexes, dropIndexes } from "../../../../src/common/model/indexes/index.js";
+import { getDbCollectionIndexes } from "../../../../src/common/mongodb.js";
+import dossiersApprenantsModelDescriptor from "../../../../src/common/model/previous.models/toRemove.models/dossiersApprenants.model.js";
+import { createRandomDossierApprenant } from "../../../data/randomizedSample.js";
+
+describe("DossiersApprenantsMigration Indexes", () => {
+  let indexes = [];
+
+  beforeEach(async () => {
+    // Crée une entrée en base
+    const randomDossier = createRandomDossierApprenant();
+    await createRandomDossierApprenant(randomDossier);
+
+    // Re-créé les indexs après l'ajout d'une entrée en base & récupère les indexes
+    await dropIndexes();
+    await createIndexes();
+    indexes = await getDbCollectionIndexes(dossiersApprenantsModelDescriptor.collectionName);
+  });
+
+  it("Vérifie l'existence d'un index sur le champ uai_etablissement", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "uai_etablissement"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ siret_etablissement", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "siret_etablissement"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ formation_cfd", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "formation_cfd"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ etablissement_num_region", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "etablissement_num_region"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ etablissement_num_departement", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "etablissement_num_departement"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ annee_scolaire", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "annee_scolaire"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ etablissement_reseaux", async () => {
+    assert.equal(
+      indexes.some((item) => item.name === "etablissement_reseaux"),
+      true
+    );
+  });
+});
---
+++ server/tests/integration/db/indexes/users.indexes.test.js
@@ -0,0 +1,48 @@
+import { strict as assert } from "assert";
+import { createIndexes, dropIndexes } from "../../../../src/common/model/indexes/index.js";
+import { getDbCollectionIndexes } from "../../../../src/common/mongodb.js";
+import usersModelDescriptor from "../../../../src/common/model/previous.models/users.model.js";
+import { createUser } from "../../../../src/common/actions/users.actions.js";
+
+describe("Users Indexes", () => {
+  let dbIndexes = [];
+
+  beforeEach(async () => {
+    // Crée une entrée en base
+    await createUser(
+      { email: "of@test.fr", password: "Secret!Password1" },
+      {
+        nom: "of",
+        prenom: "test",
+        roles: ["of"],
+        account_status: "FORCE_RESET_PASSWORD",
+      }
+    );
+
+    // Re-créé les indexs après l'ajout d'une entrée en base & récupère les indexes
+    await dropIndexes();
+    await createIndexes();
+    dbIndexes = await getDbCollectionIndexes(usersModelDescriptor.collectionName);
+  });
+
+  it("Vérifie l'existence d'un index sur le champ username", async () => {
+    assert.equal(
+      dbIndexes.some((item) => item.name === "username"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ email", async () => {
+    assert.equal(
+      dbIndexes.some((item) => item.name === "email"),
+      true
+    );
+  });
+
+  it("Vérifie l'existence d'un index sur le champ email", async () => {
+    assert.equal(
+      dbIndexes.some((item) => item.name === "email"),
+      true
+    );
+  });
+});
