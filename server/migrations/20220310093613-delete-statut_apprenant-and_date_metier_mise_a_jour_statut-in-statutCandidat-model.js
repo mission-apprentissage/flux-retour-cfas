9afeeb2094eafed10dfc1b4494f05f566d429529
Samir BENFARES <sbenfares@gmail.com>
2022-12-07 09:44:07 +0100

    chore: remove useless migrations

--- server/migrations/20220310093613-delete-statut_apprenant-and_date_metier_mise_a_jour_statut-in-statutCandidat-model.js
+++
@@ -1,6 +0,0 @@
-export const up = async (db) => {
-  const collection = db.collection("statutsCandidats");
-  await collection.updateMany({}, { $unset: { statut_apprenant: "", date_metier_mise_a_jour_statut: "" } });
-};
-
-export const down = async () => {};
