c7894240b73e30fda021718b0e785a0b1a1ba4e6
Samir BENFARES <sbenfares@gmail.com>
2022-12-08 09:57:26 +0100

    chore: rationalisation des jobs & cron

--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-analyse-fiabilite-donnees-recues-job.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-analyse-fiabilite-donnees-recues-job.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 readonly LOG_FILEPATH="/var/log/data-jobs/log_analyse_fiabilite_donnees_recues_job_$(date +'%Y-%m-%d_%H%M%S').log"
 
 call_analyse_fiabilite_job_with_logs(){
-  docker exec flux_retour_cfas_server bash -c "node -r dotenv/config src/jobs/analyse-fiabilite-dossiers-apprenants-recus/index.js" >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli:fiabilisation analyse:dossiersApprenants-recus" || true >> ${LOG_FILEPATH}
 } 
 
 call_analyse_fiabilite_job_with_logs
