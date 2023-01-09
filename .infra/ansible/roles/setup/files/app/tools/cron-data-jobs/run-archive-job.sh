830b44983e5061613dfe103ef7c1db5e29d87b85
Samir BENFARES <sbenfares@gmail.com>
2023-01-09 17:02:47 +0100

    fix cron logs file

--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-archive-job.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-archive-job.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 readonly LOG_FILEPATH="/var/log/data-jobs/run_archive_job_$(date +'%Y-%m-%d_%H%M%S').log"
 
 call_archive_job_with_logs(){
-  docker exec flux_retour_cfas_server bash -c "yarn cli archive:dossiersApprenantsEffectifs" || true >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli archive:dossiersApprenantsEffectifs" || true
 } 
 
-call_archive_job_with_logs
+call_archive_job_with_logs >> ${LOG_FILEPATH}
--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
@@ -9,19 +9,19 @@ readonly LOG_FILEPATH="/var/log/data-jobs/run_daily_jobs_$(date +'%Y-%m-%d_%H%M%
 
 call_daily_jobs_with_logs(){
   # Remplissage des organismes et des formations liées
-  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:organismes-and-formations" || true >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:organismes-and-formations" || true
   
   # Remplissage des réseaux depuis csv fournis
-  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:reseaux-newFormat" || true >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:reseaux-newFormat" || true
 
   # Remplissage de la collection EffectifsApprenants
-  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:effectifsApprenants" || true >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:effectifsApprenants" || true
 
   # Purge des données inutiles
-  docker exec flux_retour_cfas_server bash -c "yarn cli purge:events" || true >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli purge:events" || true
 
   # Warm-up cache avec calculs effectifs coûteux
-  docker exec flux_retour_cfas_server bash -c "yarn cli cache:warmup" || true >> ${LOG_FILEPATH}
+  docker exec flux_retour_cfas_server bash -c "yarn cli cache:warmup" || true 
 } 
 
-call_daily_jobs_with_logs
+call_daily_jobs_with_logs >> ${LOG_FILEPATH}
