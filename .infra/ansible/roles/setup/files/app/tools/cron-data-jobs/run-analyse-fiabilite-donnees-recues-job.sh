b6037b9a576c607bb2350927d6f1c4347d946324
Samir Benfares <sbenfares@gmail.com>
2023-01-12 17:38:04 +0100

    chore(infra): desactivate cron jobs (temporary) (#2381)

--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-analyse-fiabilite-donnees-recues-job.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-analyse-fiabilite-donnees-recues-job.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 readonly LOG_FILEPATH="/var/log/data-jobs/run_analyse_fiabilite_donnees_recues_job_$(date +'%Y-%m-%d_%H%M%S').log"
 
 call_analyse_fiabilite_job_with_logs(){
-  docker exec flux_retour_cfas_server bash -c "yarn cli:fiabilisation analyse:dossiersApprenants-recus" || true
+  # TODO - Voir si on le réactive ?
+  # docker exec flux_retour_cfas_server bash -c "yarn cli:fiabilisation analyse:dossiersApprenants-recus" || true
 } 
 
 call_analyse_fiabilite_job_with_logs >> ${LOG_FILEPATH}
