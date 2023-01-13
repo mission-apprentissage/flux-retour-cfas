6adf7578ac52ceb274410a97be21ee01d14629bf
Samir BENFARES <sbenfares@gmail.com>
2023-01-13 23:25:32 +0100

    fix(server): reRun engine from dossiersApprenantMigration
    wip
    
    
    wip

--- server/src/jobs/patches/refacto-migration/rerun-engine/rerun-engine.effectifs.js
+++ server/src/jobs/patches/refacto-migration/rerun-engine/rerun-engine.effectifs.js
@@ -2,8 +2,10 @@ import cliProgress from "cli-progress";
 import { asyncForEach } from "../../../../common/utils/asyncUtils.js";
 import logger from "../../../../common/logger.js";
 import { createJobEvent } from "../../../../common/actions/jobEvents.actions.js";
-import { effectifsDb } from "../../../../common/model/collections.js";
+import { dossiersApprenantsMigrationDb } from "../../../../common/model/collections.js";
 import { runEngine } from "../../../../common/actions/engine/engine.actions.js";
+import { structureEffectifFromDossierApprenant } from "../../../../common/actions/effectifs.actions.js";
+import { findOrganismeByUai } from "../../../../common/actions/organismes/organismes.actions.js";
 
 const loadingBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 const JOB_NAME = "refacto-migration-rerun-effectifs-engine";
@@ -14,14 +16,23 @@ export const reRunEngineEffectifs = async () => {
   logger.info("Relancement de l'engine des effectifs...");
 
   let nbEffectifsUpdated = 0;
-  const allEffectifs = await effectifsDb().find().toArray();
+  const allDossiersApprenantsMigration = await dossiersApprenantsMigrationDb().find().toArray();
 
-  loadingBar.start(allEffectifs.length, 0);
+  loadingBar.start(allDossiersApprenantsMigration.length, 0);
 
-  await asyncForEach(allEffectifs, async (currentEffectif) => {
+  await asyncForEach(allDossiersApprenantsMigration, async (currentDossierApprenantMigration) => {
     try {
-      const effectifData = { ...currentEffectif, organisme_id: currentEffectif.organisme_id.toString() };
+      // Rebuild effectifData from currentDossierApprenantMigration
+      const organisme = await findOrganismeByUai(currentDossierApprenantMigration.uai_etablissement);
+
+      const effectifData = {
+        organisme_id: organisme._id.toString(),
+        ...(await structureEffectifFromDossierApprenant(currentDossierApprenantMigration)),
+      };
+
+      // Call run engine with effectifData
       const { effectif } = await runEngine({ effectifData }, null);
+
       if (effectif.updated) nbEffectifsUpdated++;
     } catch (error) {
       // Si erreur on la stocke avec l'objet
@@ -31,7 +42,7 @@ export const reRunEngineEffectifs = async () => {
         date: new Date(),
         action: "error",
         data: {
-          currentEffectif,
+          currentDossierApprenantMigration,
           error,
           errorStack,
           errorMessage,
@@ -45,7 +56,7 @@ export const reRunEngineEffectifs = async () => {
   loadingBar.stop();
 
   // Log & stats
-  logger.info(`--> ${allEffectifs.length} effectifs initiaux`);
+  logger.info(`--> ${allDossiersApprenantsMigration.length} dossiersApprenantsMigration initiaux`);
   logger.info(`--> ${nbEffectifsUpdated} effectifs maj`);
 
   await createJobEvent({
@@ -53,7 +64,7 @@ export const reRunEngineEffectifs = async () => {
     date: new Date(),
     action: "finishing",
     data: {
-      nbEffectifsInitiaux: allEffectifs.length,
+      nbDossiersMigrationInitiaux: allDossiersApprenantsMigration.length,
     },
   });
 };
