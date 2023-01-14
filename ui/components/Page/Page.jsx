577baa3e169f6854a15b8558d50a614bc14c54cc
Pablo <pablohanry9@gmail.com>
2023-01-14 22:41:55 +0100

    feat: created floating button

--- ui/components/Page/Page.jsx
+++ ui/components/Page/Page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, Link } from "@chakra-ui/react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import NavigationMenu from "./components/NavigationMenu";
 import ContactSection from "../ContactSection/ContactSection";
 import Section from "../Section/Section";
 import { useEspace } from "../../hooks/useEspace";
+import { Interrogation } from "../../theme/components/icons";
+import NavLink from "next/link";
 
 export function Page({ children, ...rest }) {
   let { isloaded, isReloaded, isMonOrganismePages, isOrganismePages, isMesOrganismesPages } = useEspace();
@@ -20,6 +22,18 @@ export function Page({ children, ...rest }) {
       <Box minH={"40vh"} flexGrow="1">
         <Section>{!espaceContextisLoading && children}</Section>
       </Box>
+      <Link
+        as={NavLink}
+        _target="blank"
+        href="https://www.notion.so/mission-apprentissage/Documentation-dbb1eddc954441eaa0ba7f5c6404bdc0"
+        position="fixed"
+        width="60px"
+        height="60px"
+        bottom="40px"
+        right="40px"
+      >
+        <Interrogation />
+      </Link>
       <ContactSection />
       <Footer />
     </Container>
