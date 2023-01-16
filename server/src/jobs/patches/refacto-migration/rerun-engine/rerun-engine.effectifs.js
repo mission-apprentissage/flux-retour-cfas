240cf130f5bc69cbc3c2baf87a641f9995b9c663
Samir BENFARES <sbenfares@gmail.com>
2023-01-16 11:13:04 +0100

    chore: fix reRunEffectifs : split by of & log update
    fix

--- server/src/jobs/patches/refacto-migration/rerun-engine/rerun-engine.effectifs.js
+++ server/src/jobs/patches/refacto-migration/rerun-engine/rerun-engine.effectifs.js
@@ -1,4 +1,3 @@
-import cliProgress from "cli-progress";
 import { asyncForEach } from "../../../../common/utils/asyncUtils.js";
 import logger from "../../../../common/logger.js";
 import { createJobEvent } from "../../../../common/actions/jobEvents.actions.js";
@@ -7,7 +6,6 @@ import { runEngine } from "../../../../common/actions/engine/engine.actions.js";
 import { structureEffectifFromDossierApprenant } from "../../../../common/actions/effectifs.actions.js";
 import { findOrganismeByUai } from "../../../../common/actions/organismes/organismes.actions.js";
 
-const loadingBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 const JOB_NAME = "refacto-migration-rerun-effectifs-engine";
 /**
  * Ce script effectue la migration de la collection cfas vers la nouvelle collection organismes
@@ -16,47 +14,59 @@ export const reRunEngineEffectifs = async () => {
   logger.info("Relancement de l'engine des effectifs...");
 
   let nbEffectifsUpdated = 0;
-  const allDossiersApprenantsMigration = await dossiersApprenantsMigrationDb().find().toArray();
 
-  loadingBar.start(allDossiersApprenantsMigration.length, 0);
+  // Parse all distinct organismes in dossiersApprenantsMigration
+  let allOrganismesInDossiersApprenantsMigration = await dossiersApprenantsMigrationDb().distinct("organisme_id");
+  let allDossiersApprenantsMigration = await dossiersApprenantsMigrationDb().countDocuments();
 
-  await asyncForEach(allDossiersApprenantsMigration, async (currentDossierApprenantMigration) => {
-    try {
-      // Rebuild effectifData from currentDossierApprenantMigration
-      const organisme = await findOrganismeByUai(currentDossierApprenantMigration.uai_etablissement);
+  await asyncForEach(allOrganismesInDossiersApprenantsMigration, async (currentOrganismeId) => {
+    const allDossiersApprenantsMigrationForOrganisme = await dossiersApprenantsMigrationDb()
+      .find({ organisme_id: currentOrganismeId })
+      .toArray();
 
-      const effectifData = {
-        organisme_id: organisme._id.toString(),
-        ...(await structureEffectifFromDossierApprenant(currentDossierApprenantMigration)),
-      };
+    // Rerun for each dossiersApprenantMigration for organisme
+    await asyncForEach(allDossiersApprenantsMigrationForOrganisme, async (currentDossierApprenantMigration) => {
+      try {
+        const effectifData = {
+          organisme_id: currentOrganismeId.toString(),
+          ...(await structureEffectifFromDossierApprenant(currentDossierApprenantMigration)),
+        };
 
-      // Call run engine with effectifData
-      const { effectif } = await runEngine({ effectifData }, null);
+        // Call run engine with effectifData
+        const { effectif } = await runEngine({ effectifData }, null);
 
-      if (effectif.updated) nbEffectifsUpdated++;
-    } catch (error) {
-      // Si erreur on la stocke avec l'objet
-      const { stack: errorStack, message: errorMessage } = error;
-      await createJobEvent({
-        jobname: JOB_NAME,
-        date: new Date(),
-        action: "error",
-        data: {
-          currentDossierApprenantMigration,
-          error,
-          errorStack,
-          errorMessage,
-        },
-      });
-    }
+        if (effectif.updated) nbEffectifsUpdated++;
+      } catch (error) {
+        // Si erreur on la stocke avec l'objet
+        const { stack: errorStack, message: errorMessage } = error;
+        await createJobEvent({
+          jobname: JOB_NAME,
+          date: new Date(),
+          action: "error",
+          data: {
+            currentDossierApprenantMigration,
+            error,
+            errorStack,
+            errorMessage,
+          },
+        });
+      }
+    });
 
-    loadingBar.increment();
+    await createJobEvent({
+      jobname: JOB_NAME,
+      date: new Date(),
+      action: "finished-reRun-organisme",
+      data: {
+        currentOrganismeId,
+        nbDossiersApprenantsForOrganisme: allDossiersApprenantsMigrationForOrganisme.length,
+        totalDossiersApprenantsMigration: allDossiersApprenantsMigration,
+        nbEffectifsUpdated: nbEffectifsUpdated,
+      },
+    });
   });
 
-  loadingBar.stop();
-
   // Log & stats
-  logger.info(`--> ${allDossiersApprenantsMigration.length} dossiersApprenantsMigration initiaux`);
   logger.info(`--> ${nbEffectifsUpdated} effectifs maj`);
 
   await createJobEvent({
@@ -64,7 +74,7 @@ export const reRunEngineEffectifs = async () => {
     date: new Date(),
     action: "finishing",
     data: {
-      nbDossiersMigrationInitiaux: allDossiersApprenantsMigration.length,
+      nbEffectifsUpdated: nbEffectifsUpdated,
     },
   });
 };
