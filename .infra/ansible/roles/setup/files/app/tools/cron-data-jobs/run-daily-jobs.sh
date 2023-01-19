a92bb54e12e1e403ae81e264d6bbe1c35c87f46f
Samir BENFARES <sbenfares@gmail.com>
2023-01-19 10:26:44 +0100

    chore: daily job run hydrate & purge

--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
@@ -9,17 +9,17 @@ readonly LOG_FILEPATH="/var/log/data-jobs/run_daily_jobs_$(date +'%Y-%m-%d_%H%M%
 
 call_daily_jobs_with_logs(){
 
-  # TODO : a lancer en manuel ? - Remplissage des organismes et des formations liées
-  # docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:organismes-and-formations" || true
+  # Remplissage des organismes et des formations liées
+  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:organismes-and-formations" || true
   
-  # TODO : a lancer en manuel ? - Remplissage des réseaux depuis csv fournis
+  # TODO : a lancer en manuel ou sur un timing différent (chaque semaine) ? - Remplissage des réseaux depuis csv fournis
   # docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:reseaux-newFormat" || true
 
   # TODO : a réactiver - Remplissage de la collection EffectifsApprenants
   # docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:effectifsApprenants" || true
 
-  # TODO : a réactiver - Purge des données inutiles
-  # docker exec flux_retour_cfas_server bash -c "yarn cli purge:events" || true
+  # Purge des données inutiles
+  docker exec flux_retour_cfas_server bash -c "yarn cli purge:events" || true
 } 
 
 call_daily_jobs_with_logs >> ${LOG_FILEPATH}
