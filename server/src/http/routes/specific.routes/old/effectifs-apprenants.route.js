6c6c1f307f2d86034520d068dcd7d8bc729285d0
Samir Benfares <sbenfares@gmail.com>
2023-01-24 08:26:56 +0100

    chore(server): effectifApprenants route temp 503 (#2443)

--- server/src/http/routes/specific.routes/old/effectifs-apprenants.route.js
+++ server/src/http/routes/specific.routes/old/effectifs-apprenants.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-import Joi from "joi";
+// import Joi from "joi";
 import tryCatch from "../../../middlewares/tryCatchMiddleware.js";
-import { JOB_NAMES, jobEventStatuts } from "../../../../common/constants/jobsConstants.js";
-import { findAndPaginate } from "../../../../common/utils/dbUtils.js";
-import { effectifsApprenantsDb } from "../../../../common/model/collections.js";
-import { sendTransformedPaginatedJsonStream } from "../../../../common/utils/httpUtils.js";
+// import { JOB_NAMES, jobEventStatuts } from "../../../../common/constants/jobsConstants.js";
+// import { findAndPaginate } from "../../../../common/utils/dbUtils.js";
+// import { effectifsApprenantsDb } from "../../../../common/model/collections.js";
+// import { sendTransformedPaginatedJsonStream } from "../../../../common/utils/httpUtils.js";
+// import { isJobInAction } from "../../../../common/actions/jobEvents.actions.js";
 
-export default ({ jobEvents }) => {
+export default () => {
   const router = express.Router();
 
   /**
@@ -15,30 +16,36 @@ export default ({ jobEvents }) => {
   router.get(
     "/",
     tryCatch(async (req, res) => {
-      const params = await Joi.object({
-        page: Joi.number(),
-        limit: Joi.number(),
-      }).validateAsync(req.query, { abortEarly: false });
+      // const params = await Joi.object({
+      //   page: Joi.number(),
+      //   limit: Joi.number(),
+      // }).validateAsync(req.query, { abortEarly: false });
 
-      const page = Number(params.page ?? 1);
-      const limit = Number(params.limit ?? 1000);
+      // const page = Number(params.page ?? 1);
+      // const limit = Number(params.limit ?? 1000);
 
-      if (!(await jobEvents.isJobInAction(JOB_NAMES.createEffectifsApprenantsCollection, jobEventStatuts.ended))) {
-        // Job RCO not ended, no data should be get
-        res.status(503).json({
-          status: "ERROR",
-          message: "Les données ne sont pas disponibles, merci de renvoyer une requête dans quelques instants",
-        });
-      } else {
-        const { find, pagination } = await findAndPaginate(
-          effectifsApprenantsDb(),
-          {},
-          { projection: { created_at: 0, updated_at: 0, _id: 0, __v: 0 }, page, limit: limit }
-        );
+      res.status(503).json({
+        status: "ERROR",
+        message: "Les données ne sont pas disponibles.",
+      });
 
-        //  Return JSON Stream
-        return sendTransformedPaginatedJsonStream(find.stream(), "effectifsApprenants", pagination, res);
-      }
+      // TODO : Remettre la construction des effectifsApprenants une fois décidé coté métier.
+      // if (!(await isJobInAction(JOB_NAMES.createEffectifsApprenantsCollection, jobEventStatuts.ended))) {
+      //   // Job RCO not ended, no data should be get
+      //   res.status(503).json({
+      //     status: "ERROR",
+      //     message: "Les données ne sont pas disponibles.",
+      //   });
+      // } else {
+      //   const { find, pagination } = await findAndPaginate(
+      //     effectifsApprenantsDb(),
+      //     {},
+      //     { projection: { created_at: 0, updated_at: 0, _id: 0, __v: 0 }, page, limit: limit }
+      //   );
+
+      //   //  Return JSON Stream
+      //   return sendTransformedPaginatedJsonStream(find.stream(), "effectifsApprenants", pagination, res);
+      // }
     })
   );
 
