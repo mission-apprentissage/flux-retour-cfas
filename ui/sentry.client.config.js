54d99fdacd2c9a0fe3ca4690eeb0b32a153302af
Sven <sven.gaubert@beta.gouv.fr>
2023-01-26 19:01:12 +0100

    fix: revert sentry (#2463)

--- ui/sentry.client.config.js
+++
@@ -1,3 +0,0 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
-import "./sentry.init";
--- ui/sentry.server.config.js
+++
@@ -1,2 +0,0 @@
-// This file configures the initialization of Sentry on the server.
-import "./sentry.init";
