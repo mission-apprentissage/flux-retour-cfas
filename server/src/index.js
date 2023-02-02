4007aa71b818d764a5c0dce4f0f1cf3323620c74
Sven Gaubert <svengau@gmail.com>
2023-02-02 07:35:47 +0100

    fix: force server to stop when unexpected error happens

--- server/src/index.js
+++ server/src/index.js
@@ -7,8 +7,15 @@ import createServices from "./services.js";
 import { connectToMongodb, configureDbSchemaValidation } from "./common/mongodb.js";
 import { modelDescriptors } from "./common/model/collections.js";
 
-process.on("unhandledRejection", (e) => logger.error("An unexpected error occurred", e));
-process.on("uncaughtException", (e) => logger.error("An unexpected error occurred", e));
+process.on("unhandledRejection", (e) => {
+  logger.error("An unhandled rejection error occurred", e);
+  throw e;
+});
+process.on("uncaughtException", (e) => {
+  logger.error("An uncaught exception occurred", e);
+  // eslint-disable-next-line no-process-exit
+  process.exit(1);
+});
 
 (async function () {
   await connectToMongodb(config.mongodb.uri);
@@ -16,7 +23,6 @@ process.on("uncaughtException", (e) => logger.error("An unexpected error occurre
 
   const components = await createComponents();
   const services = await createServices();
-
   const http = await server({ ...components, ...services });
   http.listen(5000, () => logger.info(`${config.appName} - Server ready and listening on port ${5000}`));
 })();
