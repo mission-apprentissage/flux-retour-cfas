f79dc9e0a5e28eca09b3567b0bdb2c789570cbdb
Samir BENFARES <sbenfares@gmail.com>
2023-02-06 23:36:54 +0100

    chore: remove indicateurs dossiers component & tests

--- server/src/common/components/components.js
+++ server/src/common/components/components.js
@@ -1,12 +1,9 @@
 import createEffectifs from "./effectifs.js";
-import createEffectifsFromDossiers from "./effectifs.dossiers.js";
 
 export default async (options = {}) => {
   const effectifs = options.effectifs || createEffectifs();
-  const effectifsFromDossiers = options.effectifsFromDossiers || createEffectifsFromDossiers();
 
   return {
     effectifs,
-    effectifsFromDossiers,
   };
 };
