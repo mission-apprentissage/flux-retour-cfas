93bb50714fa23848ac453b84bea63f6dfd955de0
Samir BENFARES <sbenfares@gmail.com>
2023-02-06 16:45:01 +0100

    chore: add migration for dropping collections
    fix(server): async drop

---
+++ server/migrations/20230206153259-drop-useless-collections.js
@@ -0,0 +1,10 @@
+export const up = async (db) => {
+  // Suppression des collections inutilisées
+  await db.collection("effectifsApprenants").drop();
+  await db.collection("referentielSiret").drop();
+  await db.collection("duplicatesEvents").drop();
+  await db.collection("cfas").drop();
+  await db.collection("archiveDossiersApprenants").drop();
+};
+
+export const down = async () => {};
