c2a1e5adbd925743e61d060a98afed51ec66ca5e
Samir BENFARES <sbenfares@gmail.com>
2023-02-15 10:40:36 +0100

    chore(infra): add hydate organismes referentiel to daily cron

--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
@@ -8,6 +8,8 @@ set -euo pipefail
 readonly LOG_FILEPATH="/var/log/data-jobs/run_daily_jobs_$(date +'%Y-%m-%d_%H%M%S').log"
 
 call_daily_jobs_with_logs(){
+  # Remplissage des organismes issus du référentiel
+  docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:organismes-referentiel" || true
 
   # Remplissage des organismes et des formations liées
   docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:organismes-and-formations" || true
