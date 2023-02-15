b16037f75f254aba7512fc7ac9d93d4e7ea3058a
Samir BENFARES <sbenfares@gmail.com>
2023-02-15 16:15:48 +0100

    fix(server): update seed sample organismes

--- server/src/jobs/seed/start/index.js
+++ server/src/jobs/seed/start/index.js
@@ -6,8 +6,10 @@ import {
   addContributeurOrganisme,
   createOrganisme,
   findOrganismeByUai,
+  findOrganismeByUaiAndSiret,
 } from "../../../common/actions/organismes/organismes.actions.js";
 import { userAfterCreate } from "../../../common/actions/users.afterCreate.actions.js";
+import { buildAdresseFromUai } from "../../../common/utils/uaiUtils.js";
 
 export const seedRoles = async () => {
   for (const key of Object.keys(defaultRolesAcls)) {
@@ -48,90 +50,99 @@ export const seedSampleOrganismes = async () => {
   // Create Organisme A reseau A erp A
   // Create Organisme B reseau A erp B
   // Create Organisme C reseau B erp A
-  // Create Organisme D pas de reseau, pas d'erp, pas de siret
 
-  const organismeK = await findOrganismeByUai("0333326L");
+  const organismeK = await findOrganismeByUaiAndSiret("0333326L", "51400512300062");
   if (!organismeK) {
-    await createOrganisme({
-      uai: "0333326L",
-      siret: "51400512300062",
-      nature: "responsable_formateur",
-    });
+    await createOrganisme(
+      {
+        uai: "0333326L",
+        siret: "51400512300062",
+        nature: "responsable_formateur",
+        ...buildAdresseFromUai("0333326L"),
+      },
+      { callLbaApi: false, buildFormationTree: false, buildInfosFromSiret: false }
+    );
   }
 
-  const organismeA = await findOrganismeByUai("0142321X");
+  const organismeA = await findOrganismeByUaiAndSiret("0142321X", "44492238900010");
   if (!organismeA) {
-    await createOrganisme({
-      uai: "0142321X",
-      siret: "44492238900010",
-      reseaux: ["CCI"],
-      nature: "responsable_formateur",
-      nom: "ADEN Formations (Caen)",
-    });
+    await createOrganisme(
+      {
+        uai: "0142321X",
+        siret: "44492238900010",
+        reseaux: ["CCI"],
+        nature: "responsable_formateur",
+        nom: "ADEN Formations (Caen)",
+        ...buildAdresseFromUai("0142321X"),
+      },
+      { callLbaApi: false, buildFormationTree: false, buildInfosFromSiret: false }
+    );
     logger.info("organisme A created");
   }
 
-  const organismeB = await findOrganismeByUai("0611309S");
+  const organismeB = await findOrganismeByUaiAndSiret("0611309S", "44492238900010");
   if (!organismeB) {
-    await createOrganisme({
-      uai: "0611309S",
-      siret: "44492238900044",
-      reseaux: ["CCI"],
-      erps: ["GESTI"],
-      nature: "inconnue",
-      nom: "ADEN Formations (Damigny)",
-    });
+    await createOrganisme(
+      {
+        uai: "0611309S",
+        siret: "44492238900044",
+        reseaux: ["CCI"],
+        erps: ["GESTI"],
+        nature: "inconnue",
+        nom: "ADEN Formations (Damigny)",
+        ...buildAdresseFromUai("0611309S"),
+      },
+      { callLbaApi: false, buildFormationTree: false, buildInfosFromSiret: false }
+    );
     logger.info("organisme B created");
   }
 
-  const organismeC = await findOrganismeByUai("0010856A");
+  const organismeC = await findOrganismeByUaiAndSiret("0010856A", "77931004400028");
   if (!organismeC) {
-    await createOrganisme({
-      uai: "0010856A",
-      siret: "77931004400028",
-      reseaux: ["UIMM"],
-      erps: ["YMAG"],
-      nature: "responsable_formateur",
-      nom: "AFPMA APPRENTISSAGE - Site de Peronnas",
-    });
+    await createOrganisme(
+      {
+        uai: "0010856A",
+        siret: "77931004400028",
+        reseaux: ["UIMM"],
+        erps: ["YMAG"],
+        nature: "responsable_formateur",
+        nom: "AFPMA APPRENTISSAGE - Site de Peronnas",
+        ...buildAdresseFromUai("0010856A"),
+      },
+      { callLbaApi: false, buildFormationTree: false, buildInfosFromSiret: false }
+    );
     logger.info("organisme C created");
   }
-  const organismeZ = await findOrganismeByUai("0261098C");
+
+  const organismeZ = await findOrganismeByUaiAndSiret("0261098C", "34497770700027");
   if (!organismeZ) {
-    await createOrganisme({
-      uai: "0261098C",
-      siret: "34497770700027",
-      reseaux: ["MFR"],
-      erps: ["GESTI"],
-      nature: "responsable_formateur",
-      nom: "MAISON FAMILIALE RURALE CFA - 26300 CHATEAUNEUF SUR ISERE",
-    });
+    await createOrganisme(
+      {
+        uai: "0261098C",
+        siret: "34497770700027",
+        reseaux: ["MFR"],
+        erps: ["GESTI"],
+        nature: "responsable_formateur",
+        nom: "MAISON FAMILIALE RURALE CFA - 26300 CHATEAUNEUF SUR ISERE",
+        ...buildAdresseFromUai("0261098C"),
+      },
+      { callLbaApi: false, buildFormationTree: false, buildInfosFromSiret: false }
+    );
     logger.info("organisme Z created");
   }
 
-  const organismeD = await findOrganismeByUai("0780762E");
-  if (!organismeD) {
-    await createOrganisme({
-      uai: "0780762E",
-      adresse: {
-        departement: "78",
-        region: "11",
-        academie: "25",
-      },
-      nature: "responsable_formateur",
-      nom: "AFTRAL CFA TL LE TREMBLAY EPT",
-    });
-    logger.info("organisme D created");
-  }
-
-  const organismeE = await findOrganismeByUai("0312755B");
+  const organismeE = await findOrganismeByUaiAndSiret("0312755B", "49917930700024");
   if (!organismeE) {
-    await createOrganisme({
-      uai: "0312755B",
-      siret: "49917930700024",
-      nature: "responsable_formateur",
-      nom: "MIDISUP",
-    });
+    await createOrganisme(
+      {
+        uai: "0312755B",
+        siret: "49917930700024",
+        nature: "responsable_formateur",
+        nom: "MIDISUP",
+        ...buildAdresseFromUai("0312755B"),
+      },
+      { callLbaApi: false, buildFormationTree: false, buildInfosFromSiret: false }
+    );
     logger.info("organisme E created");
   }
 };
