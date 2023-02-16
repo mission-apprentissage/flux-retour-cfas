2b0c0d4517afe36150c9ec79830d2e4f40c9032b
Samir BENFARES <sbenfares@gmail.com>
2023-02-16 16:24:07 +0100

    refacto(server): optim

--- server/migrations/20230206153259-drop-useless-collections.js
+++ server/migrations/20230206153259-drop-useless-collections.js
@@ -1,15 +1,23 @@
+import { collectionExistInDb } from "../src/common/mongodb.js";
+
 export const up = async (db) => {
   // Suppression des collections inutilisées (si elles existent)
+
   const collectionsInDb = await db.listCollections().toArray();
 
-  if (existInDb(collectionsInDb, "effectifsApprenants")) await db.collection("effectifsApprenants").drop();
-  if (existInDb(collectionsInDb, "referentielSiret")) await db.collection("referentielSiret").drop();
-  if (existInDb(collectionsInDb, "duplicatesEvents")) await db.collection("duplicatesEvents").drop();
-  if (existInDb(collectionsInDb, "cfas")) await db.collection("cfas").drop();
-  if (existInDb(collectionsInDb, "archiveDossiersApprenants")) await db.collection("archiveDossiersApprenants").drop();
-  if (existInDb(collectionsInDb, "dossiersApprenants")) await db.collection("dossiersApprenants").drop();
-};
+  const collectionsToRemove = [
+    "effectifsApprenants",
+    "referentielSiret",
+    "duplicatesEvents",
+    "cfas",
+    "archiveDossiersApprenants",
+    "dossiersApprenants",
+  ];
 
-const existInDb = (collectionsInDb, collectionName) => collectionsInDb.map(({ name }) => name).includes(collectionName);
+  for (const currentCollectionToRemove of collectionsToRemove) {
+    if (collectionExistInDb(collectionsInDb, currentCollectionToRemove))
+      await db.collection(currentCollectionToRemove).drop();
+  }
+};
 
 export const down = async () => {};
