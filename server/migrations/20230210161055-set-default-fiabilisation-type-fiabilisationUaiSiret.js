686023eaf5675157d7685f23471ce2dce0fc226b
Samir BENFARES <sbenfares@gmail.com>
2023-02-16 10:30:44 +0100

    fix(server): correction des migrations en filtrant sur les organismes avec siret

--- server/migrations/20230210161055-set-default-fiabilisation-type-fiabilisationUaiSiret.js
+++ server/migrations/20230210161055-set-default-fiabilisation-type-fiabilisationUaiSiret.js
@@ -2,5 +2,8 @@ import { FIABILISATION_TYPES } from "../src/common/constants/fiabilisationConsta
 
 export const up = async (db) => {
   const collection = db.collection("fiabilisationUaiSiret");
-  await collection.updateMany({ type: { $exists: false } }, { $set: { type: FIABILISATION_TYPES.A_FIABILISER } });
+  await collection.updateMany(
+    { siret: { $exists: true }, type: { $exists: false } },
+    { $set: { type: FIABILISATION_TYPES.A_FIABILISER } }
+  );
 };
