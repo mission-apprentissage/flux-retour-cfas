9366bf9411000fc4a597d61e4220b717126f7804
Maxime Dréau <maxime@dreau.fr>
2023-02-20 13:44:00 +0100

    style: improve tabs style according to mockups

--- ui/theme/components/tabs.js
+++ ui/theme/components/tabs.js
@@ -1,28 +1,54 @@
 const Tabs = {
   parts: ["tablist", "tab", "tabpanel"],
   baseStyle: {
-    tablist: {
-      px: [0, 24],
-      border: "none",
-      bg: "secondaryBackground",
-      color: "grey.750",
-    },
-    tabpanel: {
-      px: [8, 24],
-      color: "grey.100",
-      h: 1000,
-    },
-    tab: {
-      color: "grey.500",
-      _focus: { boxShadow: "none", outlineColor: "none" },
-      _focusVisible: { boxShadow: "0 0 0 3px #2A7FFE", outlineColor: "#2A7FFE" },
-    },
+    // FIXME: is it needed?
+    // tablist: {
+    //   px: [0, 24],
+    //   border: "none",
+    //   bg: "secondaryBackground",
+    //   color: "grey.750",
+    // },
+    // tabpanel: {
+    //   px: [8, 24],
+    //   color: "grey.100",
+    //   h: 1000,
+    // },
+    // tab: {
+    //   color: "grey.500",
+    //   _focus: { boxShadow: "none", outlineColor: "none" },
+    //   _focusVisible: { boxShadow: "0 0 0 3px #2A7FFE", outlineColor: "#2A7FFE" },
+    // },
   },
   variants: {
     line: {
       tab: {
-        fontSize: ["epsilon", "gamma"],
-        _selected: { color: "grey.800", borderBottom: "4px solid", borderColor: "grey.750" },
+        paddingX: "3w",
+        paddingY: "3v",
+        marginRight: "1w",
+        position: "relative",
+        bottom: "-1px",
+        color: "grey.700",
+        fontWeight: "400",
+        fontSize: "zeta",
+        _hover: {
+          background: "grey.100",
+        },
+        _selected: {
+          background: "white",
+          color: "bluefrance",
+          borderBottom: "none",
+          borderLeft: "1px solid",
+          borderRight: "1px solid",
+          borderTop: "2px solid",
+          borderTopColor: "bluefrance",
+          borderRightColor: "grey.300",
+          borderLeftColor: "grey.300",
+        },
+        _focus: { boxShadow: "none", outlineColor: "none" },
+        _focusVisible: { boxShadow: "0 0 0 3px #2A7FFE", outlineColor: "#2A7FFE" },
+
+        // only difference with search.tab variant
+        background: "#E3E3FD",
       },
     },
     search: {
