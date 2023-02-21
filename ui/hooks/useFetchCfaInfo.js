08cb67191061c49de571b96f6cf012de2adaff6c
Maxime Dréau <maxime@dreau.fr>
2023-02-21 16:27:20 +0100

    refactor: use enabled on hook

--- ui/hooks/useFetchCfaInfo.js
+++ ui/hooks/useFetchCfaInfo.js
@@ -4,11 +4,8 @@ import { fetchCfa } from "@/common/api/tableauDeBord";
 import { QUERY_KEYS } from "@/common/constants/queryKeys";
 
 const useFetchCfaInfo = (cfaUai) => {
-  const { data, isLoading, error } = useQuery([QUERY_KEYS.CFAS, cfaUai], () => {
-    if (!cfaUai) {
-      return;
-    }
-    return fetchCfa(cfaUai);
+  const { data, isLoading, error } = useQuery([QUERY_KEYS.CFAS, cfaUai], () => fetchCfa(cfaUai), {
+    enabled: !!cfaUai,
   });
 
   return { data, loading: isLoading, error };
