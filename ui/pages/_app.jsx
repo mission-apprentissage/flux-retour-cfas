d53b1ec8304fcd0790a3c685486e46ce80b38de2
Sven Gaubert <svengau@gmail.com>
2023-02-27 13:53:25 +0100

    feat(ui): mise en place de plausible

--- ui/pages/_app.jsx
+++ ui/pages/_app.jsx
@@ -6,6 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import { QueryClientProvider } from "@tanstack/react-query";
+import PlausibleProvider from "next-plausible";
 
 import Fonts from "../theme/Fonts";
 import theme from "../theme/index";
@@ -15,17 +16,19 @@ import { queryClient } from "@/common/queryClient";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
-      <ChakraProvider theme={theme} resetCSS>
-        <Fonts />
-        <QueryClientProvider client={queryClient}>
-          <AlertMessage />
-          <UserWrapper ssrAuth={pageProps.auth}>
-            <Component {...pageProps} />
-          </UserWrapper>
-        </QueryClientProvider>
-      </ChakraProvider>
-    </RecoilRoot>
+    <PlausibleProvider domain={process.env.NEXT_PUBLIC_BASE_HOST} trackLocalhost={false}>
+      <RecoilRoot>
+        <ChakraProvider theme={theme} resetCSS>
+          <Fonts />
+          <QueryClientProvider client={queryClient}>
+            <AlertMessage />
+            <UserWrapper ssrAuth={pageProps.auth}>
+              <Component {...pageProps} />
+            </UserWrapper>
+          </QueryClientProvider>
+        </ChakraProvider>
+      </RecoilRoot>
+    </PlausibleProvider>
   );
 }
 
