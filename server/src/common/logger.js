6af2fbf62454841bc5555014bcdd137ebabc4776
Sven Gaubert <svengau@gmail.com>
2023-02-28 08:53:35 +0100

    chore: remove slack logging

--- server/src/common/logger.js
+++ server/src/common/logger.js
@@ -1,12 +1,9 @@
-import util from "util";
 import bunyan from "bunyan";
 import PrettyStream from "bunyan-prettystream";
-import BunyanSlack from "bunyan-slack";
 import config from "../config.js";
 
 const createStreams = () => {
   const { type, level } = config.log;
-  const envName = config.env;
 
   const jsonStream = () => {
     return {
@@ -26,40 +23,8 @@ const createStreams = () => {
     };
   };
 
-  const slackStream = () => {
-    const stream = new BunyanSlack(
-      {
-        webhook_url: config.slackWebhookUrl,
-        customFormatter: (record, levelName) => {
-          if (record.type === "http") {
-            record = {
-              url: record.request.url.relative,
-              statusCode: record.response.statusCode,
-              ...(record.error ? { message: record.error.message } : {}),
-            };
-          }
-          return {
-            text: util.format(`[%s][${envName}] %O`, levelName.toUpperCase(), record),
-          };
-        },
-      },
-      (error) => {
-        console.log("Unable to send log to slack", error);
-      }
-    );
-
-    return {
-      name: "slack",
-      level: "error",
-      stream,
-    };
-  };
-
   const streams = type === "console" ? [consoleStream()] : [jsonStream()];
 
-  if (config.log.streams.includes("slack") && config.slackWebhookUrl) {
-    streams.push(slackStream());
-  }
   return streams;
 };
 
