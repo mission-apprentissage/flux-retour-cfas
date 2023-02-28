49f4eec7bf3e7b6bd9042ba98c7b31590df75600
Sven Gaubert <svengau@gmail.com>
2023-02-28 13:23:10 +0100

    chore: remove field organisme.contributeurs

--- server/src/jobs/clear/clear-all.js
+++ server/src/jobs/clear/clear-all.js
@@ -3,7 +3,6 @@ import logger from "../../common/logger.js";
 import * as RolesModelDescriptor from "../../common/model/roles.model.js";
 import * as PermissionsDescriptor from "../../common/model/permissions.model.js";
 import * as usersMigrationModelDescriptor from "../../common/model/usersMigration.model.js";
-import { findOrganismesByQuery, updateOrganisme } from "../../common/actions/organismes/organismes.actions.js";
 
 export const clearRoles = async () => {
   await clearCollection(RolesModelDescriptor.collectionName);
@@ -11,10 +10,6 @@ export const clearRoles = async () => {
 };
 
 export const clearUsers = async () => {
-  const organismes = await findOrganismesByQuery({ contributeurs: { $ne: [] } });
-  for (const organisme of organismes) {
-    await updateOrganisme(organisme._id, { ...organisme, contributeurs: [] });
-  }
   await clearCollection(PermissionsDescriptor.collectionName);
   await clearCollection(usersMigrationModelDescriptor.collectionName);
   logger.info("Clear users done !");
