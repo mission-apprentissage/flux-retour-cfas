edef79f471980cac00509d4c3efebeb7e5ae5ed2
Sven Gaubert <svengau@gmail.com>
2023-03-02 17:26:33 +0100

    fix: review

--- .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
+++ .infra/ansible/roles/setup/files/app/tools/cron-data-jobs/run-daily-jobs.sh
@@ -18,8 +18,8 @@ call_daily_jobs_with_logs(){
   docker exec flux_retour_cfas_server bash -c "yarn cli hydrate:reseaux" || true
 
   # Construction & application de la fiabilisation des couples UAI - SIRET
-  docker exec flux_retour_cfas_server bash -c "yarn cli:fiabilisation build:fiabilisation-uai-siret" || true
-  docker exec flux_retour_cfas_server bash -c "yarn cli:fiabilisation apply:fiabilisation-uai-siret" || true
+  docker exec flux_retour_cfas_server bash -c "yarn cli fiabilisation:uai-siret:build" || true
+  docker exec flux_retour_cfas_server bash -c "yarn cli fiabilisation:uai-siret:apply" || true
 
   # Mise à jour des organismes via APIs externes
   docker exec flux_retour_cfas_server bash -c "yarn cli update:organismes-with-apis" || true
