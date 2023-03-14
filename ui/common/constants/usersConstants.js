c7a48680867f1f43b4dce93c9f0d5c79fa1ab871
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2023-03-14 14:02:59 +0100

    feat: admin users status labels

--- ui/common/constants/usersConstants.js
+++ ui/common/constants/usersConstants.js
@@ -27,3 +27,12 @@ export function getUserOrganisationLabel(user) {
   };
   return labels[user?.organisation] || user?.organisation;
 }
+
+export const USER_STATUS_LABELS = {
+  PENDING_EMAIL_VALIDATION: "en attente de validation utilisateur",
+  PENDING_PASSWORD_SETUP: "création mdp en cours",
+  PENDING_PERMISSIONS_SETUP: "à valider administrateur",
+  PENDING_ADMIN_VALIDATION: "accès ouvert - en attente de 1ère utilisation",
+  DIRECT_PENDING_PASSWORD_SETUP: "direct - mdp à définir",
+  CONFIRMED: "accès confirmé",
+};
