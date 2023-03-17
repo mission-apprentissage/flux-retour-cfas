1fa766aa03d6c2fe05257547d84c3f48a63dab24
Sven <svengau@gmail.com>
2023-03-17 14:15:49 +0100

    fix: clean deadcode on server

--- server/src/common/constants/networksConstants.ts
+++ server/src/common/constants/networksConstants.ts
@@ -1,61 +1,47 @@
 /**
  * Noms des réseaux de CFAS
  */
-// TODO remove property nomFichier with the file jobs/hydrate/_toRemove/reseaux/hydrate-reseaux.js
 export const RESEAUX_CFAS = {
   ADEN: {
     nomReseau: "ADEN",
-    nomFichier: "",
   },
   CMA: {
     nomReseau: "CMA",
-    nomFichier: "cfas-cma",
   },
   AGRI: {
     nomReseau: "AGRI",
-    nomFichier: "cfas-agri-no-mfr",
   },
   ANASUP: {
     nomReseau: "ANASUP",
-    nomFichier: "cfas-anasup",
   },
   CCI: {
     nomReseau: "CCI",
-    nomFichier: "cfas-cci",
   },
   CFA_EC: {
     nomReseau: "CFA EC",
-    nomFichier: "",
   },
   COMP_DU_DEVOIR: {
     nomReseau: "Compagnons du devoir",
   },
   GRETA: {
     nomReseau: "GRETA",
-    nomFichier: "cfas-greta",
   },
   UIMM: {
     nomReseau: "UIMM",
-    nomFichier: "cfas-uimm",
   },
   BTP_CFA: {
     nomReseau: "BTP CFA",
-    nomFichier: "cfas-btp-cfa",
   },
   MFR: {
     nomReseau: "MFR",
-    nomFichier: "cfas-mfr",
   },
   AFTRAL: {
     nomReseau: "AFTRAL",
-    nomFichier: "cfas-aftral",
   },
   GRETA_VAUCLUSE: {
     nomReseau: "GRETA VAUCLUSE",
-    nomFichier: "",
   },
   CFA_SAT: {
     nomReseau: "CFA SAT",
-    nomFichier: "",
   },
 };
