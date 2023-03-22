8f22fd5ef40f3cbb823b1b003d34e59c3e1e28e5
Sven <svengau@gmail.com>
2023-03-22 14:00:50 +0100

    fix: ts import in js migration files

--- server/migrations/20230210155720-set-default-fiabilisation-statut-organismes.js
+++ server/migrations/20230210155720-set-default-fiabilisation-statut-organismes.js
@@ -1,9 +1,4 @@
-import { STATUT_FIABILISATION_ORGANISME } from "../src/common/constants/fiabilisationConstants.js";
-
 export const up = async (db) => {
   const collection = db.collection("organismes");
-  await collection.updateMany(
-    { siret: { $exists: true } },
-    { $set: { fiabilisation_statut: STATUT_FIABILISATION_ORGANISME.INCONNU } }
-  );
+  await collection.updateMany({ siret: { $exists: true } }, { $set: { fiabilisation_statut: "INCONNU" } });
 };
