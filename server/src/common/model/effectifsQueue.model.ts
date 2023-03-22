f6f9aaa2006d00c2554c5338b982ee51aab9cd42
Sven <svengau@gmail.com>
2023-03-22 18:05:29 +0100

    fix: fix pb index sur effectifsQueue

--- server/src/common/model/effectifsQueue.model.ts
+++ server/src/common/model/effectifsQueue.model.ts
@@ -2,19 +2,18 @@ import { object, string, date, arrayOf, any, objectId } from "./json-schema/json
 
 const collectionName = "effectifsQueue";
 
-const indexes = () => {
-  return [
-    [
-      {
-        annee_scolaire: 1,
-        id_erp_apprenant: 1,
-      },
-      { unique: true },
-    ],
-    [{ effectif_id: 1 }, { name: "effectif_id" }],
-    [{ processed_at: 1 }, { name: "processed_at" }],
-  ];
-};
+const indexes = [
+  [
+    {
+      annee_scolaire: 1,
+      id_erp_apprenant: 1,
+    },
+    { unique: true },
+  ],
+  [{ effectif_id: 1 }, { name: "effectif_id" }],
+  [{ processed_at: 1 }, { name: "processed_at" }],
+];
+
 /**
  * this schema doesn't contain any constraint
  */
