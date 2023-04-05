a08be35063c59515459836a03710c6aafdf34c46
Sven <svengau@gmail.com>
2023-04-05 05:23:24 +0200

    fix: upload

--- ui/common/utils/array.js
+++ ui/common/utils/array.js
@@ -2,12 +2,6 @@ import { normalize } from "./stringUtils";
 
 export function sortByNormalizedLabels(array) {
   return array
-    .map((element) => {
-      element.__label = normalize(element.label);
-    })
-    .sort(sortByLabel);
-}
-
-function sortByLabel(a, b) {
-  return a.__label < b.__label ? -1 : 1;
+    .map((element) => ({ ...element, __label: normalize(element.label) }))
+    .sort((a, b) => (a.__label < b.__label ? -1 : 1));
 }
