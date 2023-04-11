9b0d225dca8edbcbde161240f4c8892ed357dcaf
Maxime Dréau <maxime@dreau.fr>
2023-04-11 17:51:53 +0200

    feat: suppression des mentions crisp

--- ui/next.config.js
+++ ui/next.config.js
@@ -10,20 +10,17 @@ const contentSecurityPolicy = `
       default-src 'self' https://plausible.io;
       base-uri 'self';
       block-all-mixed-content;
-      font-src 'self' https://client.crisp.chat https: data:;
-      media-src https://client.crisp.chat;
+      font-src 'self' https: data:;
       frame-ancestors 'self' https://cfas.apprentissage.beta.gouv.fr;
-      frame-src 'self' https://game.crisp.chat https://plausible.io https://cfas.apprentissage.beta.gouv.fr;
-      img-src 'self' https://files.tableau-de-bord.apprentissage.beta.gouv.fr https://client.crisp.chat https://image.crisp.chat https://www.notion.so data: ${
+      frame-src 'self'  https://plausible.io https://cfas.apprentissage.beta.gouv.fr;
+      img-src 'self' https://files.tableau-de-bord.apprentissage.beta.gouv.fr https://www.notion.so data: ${
         process.env.NEXT_PUBLIC_ENV !== "production" ? "" : ""
       };
       object-src 'none';
-      script-src 'self' https://plausible.io https://client.crisp.chat https://settings.crisp.chat ${
-        process.env.NEXT_PUBLIC_ENV === "dev" ? "'unsafe-eval'" : ""
-      };
+      script-src 'self' https://plausible.io ${process.env.NEXT_PUBLIC_ENV === "dev" ? "'unsafe-eval'" : ""};
       script-src-attr 'none';
-      style-src 'self' https: https://client.crisp.chat https: *.plausible.io 'unsafe-inline';
-      connect-src 'self' https://geo.api.gouv.fr/ https://client.crisp.chat https://storage.crisp.chat wss://client.relay.crisp.chat wss://stream.relay.crisp.chat https://plausible.io;
+      style-src 'self' https:  https: *.plausible.io 'unsafe-inline';
+      connect-src 'self' https://geo.api.gouv.fr/ https://plausible.io;
       upgrade-insecure-requests;
 `;
 
