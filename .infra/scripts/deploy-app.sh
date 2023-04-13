b8fbe0287f7a84d7be34a72fa473dd82b3deeba4
Sven <sven.gaubert@beta.gouv.fr>
2023-04-13 17:45:55 +0200

    chore: add ansible version (#2904)

--- .infra/scripts/deploy-app.sh
+++ .infra/scripts/deploy-app.sh
@@ -5,6 +5,8 @@ readonly ANSIBLE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../ansible"
 readonly ENV_FILTER=${1:?"Merci de préciser un ou plusieurs environnements (ex. recette ou production)"}
 shift
 
+ansible-playbook  --version
+
 echo "Déploiement sur l'(es) environnement(s) ${ENV_FILTER}..."
 cd "${ANSIBLE_DIR}"
 ansible-playbook -i env.ini --limit "${ENV_FILTER}" deploy.yml "$@"
