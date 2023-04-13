81a80cd25c2a2386e4020d12e8dbc93ab9559130
Samir BENFARES <sbenfares@gmail.com>
2023-04-13 16:22:06 +0200

    chore(server): ajout de l'update des organismes non fiables sur uai fermée
    wip

---
+++ server/src/jobs/fiabilisation/uai-siret/update.utils.ts
@@ -0,0 +1,26 @@
+import { effectifsDb } from "../../../common/model/collections.js";
+
+/**
+ * Récupération de la liste des doublons pour 2 organismes
+ * @param organismeSource
+ * @param organismeWithDuplicates
+ */
+export const getEffectifsDuplicatesFromOrganismes = async (organismeSource, organismeWithDuplicates) => {
+  return await effectifsDb()
+    .aggregate([
+      { $match: { organisme_id: { $in: [organismeSource, organismeWithDuplicates] } } },
+      {
+        $group: {
+          _id: {
+            nom_apprenant: "$apprenant.nom",
+            prenom_apprenant: { $toLower: "$apprenant.prenom" },
+            date_de_naissance_apprenant: "$apprenant.date_de_naissance",
+            annee_scolaire: "$annee_scolaire",
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $match: { count: { $gt: 1 } } },
+    ])
+    .toArray();
+};
