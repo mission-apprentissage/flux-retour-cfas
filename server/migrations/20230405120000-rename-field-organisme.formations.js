fa4174b7b67297f65ad81aa0f5a2c713fd6bb066
Samir Benfares <sbenfares@gmail.com>
2023-04-17 10:15:56 +0200

    chore(server): ajout d'un script de patch de mauvaise transmission (#2900)

--- server/migrations/20230405120000-rename-field-organisme.formations.js
+++ server/migrations/20230405120000-rename-field-organisme.formations.js
@@ -1,3 +1,5 @@
 export const up = async (db) => {
-  await db.collection("organismes").updateMany({}, { $rename: { formations: "relatedFormations" } });
+  await db
+    .collection("organismes")
+    .updateMany({}, { $rename: { formations: "relatedFormations" } }, { bypassDocumentValidation: true });
 };
