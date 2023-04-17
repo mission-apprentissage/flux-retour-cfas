08d6d6399cf1eef39beac0211258a424d2ae4368
Sven <svengau@gmail.com>
2023-04-17 13:41:27 +0200

    feat: include ui tests in CI

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -12,6 +12,12 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/storybook/",
+    "<rootDir>/.history/",
+  ],
   testEnvironment: "jest-environment-jsdom",
   preset: "ts-jest",
   moduleNameMapper: {
