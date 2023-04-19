c31789795049e8f4bd768123c7605d02c62e6f12
Maxime Dréau <maxime@dreau.fr>
2023-04-19 17:35:41 +0200

    style: suppression block information login + oubli mdp

--- ui/pages/auth/connexion.tsx
+++ ui/pages/auth/connexion.tsx
@@ -1,11 +1,9 @@
-import { Flex } from "@chakra-ui/react";
 import Head from "next/head";
 import React from "react";
 
 import { getAuthServerSideProps } from "@/common/SSR/getAuthServerSideProps";
 import Page from "@/components/Page/Page";
 import Login from "@/modules/auth/connexion/Connexion";
-import InformationBlock from "@/modules/auth/inscription/components/InformationBlock";
 
 export const getServerSideProps = async (context) => ({ props: { ...(await getAuthServerSideProps(context)) } });
 
@@ -17,10 +15,7 @@ export default function ConnexionPage() {
       <Head>
         <title>{title}</title>
       </Head>
-      <Flex w="100%" mt={8} minH="40vh" direction={{ base: "column", md: "row" }}>
-        <Login w={{ base: "100%", md: "50%" }} h="100%" border="1px solid" borderColor="openbluefrance" />
-        <InformationBlock w={{ base: "100%", md: "50%" }} />
-      </Flex>
+      <Login w={{ base: "100%", md: "60%", lg: "50%" }} mx="auto" border="1px solid" borderColor="openbluefrance" />
     </Page>
   );
 }
