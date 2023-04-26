339411a740141712453bb5befb6fcf6ef45416dd
Maxime Dréau <totakoko@users.noreply.github.com>
2023-04-26 16:53:23 +0200

    feat: ajout du champ cle_ministere_educatif dans formations (#2734)

--- server/tests/integration/common/utils/miscUtils.test.ts
+++ server/tests/integration/common/utils/miscUtils.test.ts
@@ -20,31 +20,33 @@ describe("generateRandomAlphanumericPhrase", () => {
 });
 
 describe("stripEmptyFields", () => {
-  const output = stripEmptyFields({
-    a: 1,
-    b: 2,
-    c: 3,
-    d: null,
-    e: undefined,
-    f: "",
-    g: 0,
-    h: false,
-    i: [],
-    j: [null, "", undefined],
-    nested: {
+  it('supprime les champs undefined, null, et "" à la racine et imbriqués', () => {
+    const output = stripEmptyFields({
       a: 1,
-      b: undefined,
-      c: [],
-    },
-  });
-  assert.deepStrictEqual(output, {
-    a: 1,
-    b: 2,
-    c: 3,
-    g: 0,
-    h: false,
-    i: [],
-    j: [null, "", undefined],
-    nested: { a: 1, c: [] },
+      b: 2,
+      c: 3,
+      d: null,
+      e: undefined,
+      f: "",
+      g: 0,
+      h: false,
+      i: [],
+      j: [null, "", undefined],
+      nested: {
+        a: 1,
+        b: undefined,
+        c: [],
+      },
+    });
+    assert.deepStrictEqual(output, {
+      a: 1,
+      b: 2,
+      c: 3,
+      g: 0,
+      h: false,
+      i: [],
+      j: [null, "", undefined],
+      nested: { a: 1, c: [] },
+    });
   });
 });
