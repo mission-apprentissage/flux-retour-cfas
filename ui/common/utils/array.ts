d9077ea2a8239cf92627b2a78c3c21f984bbeebc
Sven <svengau@gmail.com>
2023-05-03 19:07:46 +0200

    fix: minor ui improvements

--- ui/common/utils/array.ts
+++ ui/common/utils/array.ts
@@ -1,7 +1,8 @@
 import { normalize } from "./stringUtils";
 
-export function sortByNormalizedLabels(array) {
-  return array
+export function sortByNormalizedLabels<T>(array: T): T {
+  return (array as any)
     .map((element) => ({ ...element, __label: normalize(element.label) }))
-    .sort((a, b) => (a.__label < b.__label ? -1 : 1));
+    .sort((a, b) => (a.__label < b.__label ? -1 : 1))
+    .map(({ __label, ...element }) => element);
 }
