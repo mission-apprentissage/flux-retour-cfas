c12162990a65b5f05b2e45413a68895116a90f94
Sven <svengau@gmail.com>
2023-05-09 11:25:49 +0200

    fix: fix script de migration

--- server/migrations/20230512000000-api_v3.js
+++ server/migrations/20230512000000-api_v3.js
@@ -1,9 +1,21 @@
 export const up = async (db) => {
   // migration du champ apprenant.handicap vers apprenant.rqth
-  await db.collection("effectifs").updateMany({ "apprenant.handicap": true }, { $set: { "apprenant.rqth": true } });
-  await db.collection("effectifs").updateMany({}, { $unset: { "apprenant.handicap": "" } });
+  await db
+    .collection("effectifs")
+    .updateMany(
+      { "apprenant.handicap": true },
+      { $set: { "apprenant.rqth": true } },
+      { bypassDocumentValidation: true }
+    );
+  await db
+    .collection("effectifs")
+    .updateMany({}, { $unset: { "apprenant.handicap": "" } }, { bypassDocumentValidation: true });
 
   // migration du champ apprenant.contrats vers contrats
-  await db.collection("effectifs").updateMany({}, { $set: { contrats: "$apprenant.contrats" } });
-  await db.collection("effectifs").updateMany({}, { $unset: { "apprenant.contrats": "" } });
+  await db
+    .collection("effectifs")
+    .updateMany({}, { $set: { contrats: "$apprenant.contrats" } }, { bypassDocumentValidation: true });
+  await db
+    .collection("effectifs")
+    .updateMany({}, { $unset: { "apprenant.contrats": "" } }, { bypassDocumentValidation: true });
 };
