0ebb21b3df27a84f4f9b24c794b718dec83a3919
Samir BENFARES <sbenfares@gmail.com>
2023-05-10 14:40:39 +0200

    chore(server): duplicate actions & tests

---
+++ server/src/common/actions/effectifs/effectifs.duplicates.actions.ts
@@ -0,0 +1,194 @@
+import { ObjectId } from "mongodb";
+
+import { effectifsDb } from "@/common/model/collections";
+
+/**
+ * Méthode de récupération de la liste des doublons au sein d'un organisme
+ * @param organisme_id
+ */
+export const getDuplicatesEffectifsForOrganismeId = async (organisme_id: ObjectId) => {
+  return await effectifsDb()
+    .aggregate([
+      { $match: { organisme_id } },
+      {
+        $addFields: {
+          sanitizedNom: { $regexFindAll: { input: { $toLower: "$apprenant.nom" }, regex: /[A-Za-zÀ-ÖØ-öø-ÿ]/ } },
+        },
+      },
+      {
+        $addFields: {
+          sanitizedPrenom: { $regexFindAll: { input: { $toLower: "$apprenant.prenom" }, regex: /[A-Za-zÀ-ÖØ-öø-ÿ]/ } },
+        },
+      },
+      {
+        $addFields: {
+          sanitizedNom: {
+            $reduce: { input: "$sanitizedNom.match", initialValue: "", in: { $concat: ["$$value", "$$this"] } },
+          },
+        },
+      },
+      {
+        $addFields: {
+          sanitizedPrenom: {
+            $reduce: { input: "$sanitizedPrenom.match", initialValue: "", in: { $concat: ["$$value", "$$this"] } },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            nom_apprenant: "$sanitizedNom",
+            prenom_apprenant: "$sanitizedPrenom",
+            date_de_naissance_apprenant: "$apprenant.date_de_naissance",
+            annee_scolaire: "$annee_scolaire",
+            formation_cfd: "$formation.cfd",
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $match: { count: { $gt: 1 } } },
+    ])
+    .toArray();
+};
+
+/**
+ * TODO : Clean
+ * Récupération de la liste des doublons pour un SIREN
+ * @param siren
+ */
+export const getEffectifsDuplicatesFromSIREN = async (siren: string) => {
+  const sample = await effectifsDb()
+    .aggregate([
+      {
+        $lookup: {
+          from: "organismes",
+          localField: "organisme_id",
+          foreignField: "_id",
+          as: "organismes_info",
+        },
+      },
+      { $unwind: "$organismes_info" },
+      {
+        $project: {
+          organisme_uai: "$organismes_info.uai",
+          organisme_siren: { $substr: ["$organismes_info.siret", 0, 9] },
+          nom_apprenant: { $toUpper: "$sanitizedName" },
+          prenom_apprenant: { $toUpper: "$apprenant.prenom" },
+          date_de_naissance_apprenant: "$apprenant.date_de_naissance",
+          annee_scolaire: "$annee_scolaire",
+        },
+      },
+      { $match: { organisme_siren: "786384263" } },
+    ])
+    .toArray();
+
+  console.log(sample);
+
+  return await effectifsDb()
+    .aggregate([
+      {
+        $lookup: {
+          from: "organismes",
+          localField: "organisme_id",
+          foreignField: "_id",
+          as: "organismes_info",
+        },
+      },
+      { $unwind: "$organismes_info" },
+      {
+        $project: {
+          organisme_uai: "$organismes_info.uai",
+          organisme_siren: { $substr: ["$organismes_info.siret", 0, 9] },
+        },
+      },
+      { $match: { organisme_siren: siren } },
+      {
+        $group: {
+          _id: {
+            nom_apprenant: "$apprenant.nom",
+            prenom_apprenant: { $toLower: "$apprenant.prenom" },
+            date_de_naissance_apprenant: "$apprenant.date_de_naissance",
+            annee_scolaire: "$annee_scolaire",
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $match: { count: { $gt: 1 } } },
+    ])
+    .toArray();
+};
+
+/**
+ * TODO : Clean
+ * Récupération de la liste des doublons d'effectifs sur la base d'un SIREN commun
+ * @returns
+ */
+export const getEffectifsDuplicatesOnSIREN = async () => {
+  return await effectifsDb()
+    .aggregate([
+      {
+        $lookup: {
+          from: "organismes",
+          localField: "organisme_id",
+          foreignField: "_id",
+          as: "organismes_info",
+        },
+      },
+      { $unwind: "$organismes_info" },
+      {
+        $project: {
+          organisme_uai: "$organismes_info.uai",
+          organisme_siren: { $substr: ["$organismes_info.siret", 0, 9] },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            organisme_siren: "$organisme_siren",
+            nom_apprenant: "$apprenant.nom",
+            prenom_apprenant: "$apprenant.prenom",
+            date_de_naissance_apprenant: "$apprenant.date_de_naissance",
+            annee_scolaire: "$annee_scolaire",
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $match: { count: { $gt: 1 } } },
+    ])
+    .toArray();
+};
+
+/**
+ * TODO : Clean
+ * @returns
+ */
+export const tmpSanitized = async () => {
+  const sanitizedTest = await effectifsDb().aggregate([
+    {
+      $addFields: {
+        sanitizedNom: { $regexFindAll: { input: { $toUpper: "$apprenant.nom" }, regex: /[A-Za-zÀ-ÖØ-öø-ÿ]/ } },
+      },
+    },
+    {
+      $addFields: {
+        sanitizedPrenom: { $regexFindAll: { input: { $toUpper: "$apprenant.prenom" }, regex: /[A-Za-zÀ-ÖØ-öø-ÿ]/ } },
+      },
+    },
+    {
+      $addFields: {
+        sanitizedNom: {
+          $reduce: { input: "$sanitizedNom.match", initialValue: "", in: { $concat: ["$$value", "$$this"] } },
+        },
+      },
+    },
+    {
+      $addFields: {
+        sanitizedPrenom: {
+          $reduce: { input: "$sanitizedPrenom.match", initialValue: "", in: { $concat: ["$$value", "$$this"] } },
+        },
+      },
+    },
+  ]);
+
+  return sanitizedTest;
+};
---
+++ server/tests/integration/common/actions/effectifs.duplicates.actions.test.ts
@@ -0,0 +1,123 @@
+import { strict as assert } from "assert";
+
+import { ObjectId } from "mongodb";
+
+import { getDuplicatesEffectifsForOrganismeId } from "@/common/actions/effectifs/effectifs.duplicates.actions";
+import { Organisme } from "@/common/model/@types";
+import { effectifsDb, organismesDb } from "@/common/model/collections";
+import { createSampleEffectif, createRandomOrganisme } from "@tests/data/randomizedSample";
+import { id } from "@tests/utils/testUtils";
+
+const sampleOrganismeId = new ObjectId(id(1));
+
+const sampleOrganisme: Organisme = {
+  _id: sampleOrganismeId,
+  ...createRandomOrganisme({ siret: "19040492100016" }),
+};
+
+/**
+ * Fonction utilitaire d'ajout en doublon d'effectif
+ * @param sampleEffectif
+ * @param nbDuplicates
+ */
+const insertDuplicateEffectifs = async (sampleEffectif, nbDuplicates = 2) => {
+  for (let index = 0; index < nbDuplicates; index++)
+    await effectifsDb().insertOne({ ...sampleEffectif, id_erp_apprenant: `ID_ERP_${index}` });
+};
+
+describe("Test des actions Effectifs Duplicates", () => {
+  beforeEach(async () => {
+    // Création d'un organisme de test sans appels API externes
+    await organismesDb().insertOne(sampleOrganisme);
+  });
+
+  it("Permet de vérifier la récupération de doublons d'effectifs", async () => {
+    // Ajout de 2 doublons d'effectifs
+    const sampleEffectif = createSampleEffectif({ organisme: sampleOrganisme });
+    await insertDuplicateEffectifs(sampleEffectif);
+
+    const duplicates = await getDuplicatesEffectifsForOrganismeId(sampleOrganismeId);
+
+    // Vérification de la récupération d'une liste avec un doublon identifié 2 fois sur les champs de la clé d'unicité
+    assert.equal(duplicates.length, 1);
+    assert.equal(duplicates[0].count, 2);
+
+    assert.equal(duplicates[0]._id.nom_apprenant.toLowerCase(), sampleEffectif.apprenant.nom.toLowerCase());
+    assert.equal(duplicates[0]._id.prenom_apprenant.toLowerCase(), sampleEffectif.apprenant.prenom.toLowerCase());
+    assert.deepEqual(duplicates[0]._id.date_de_naissance_apprenant, sampleEffectif.apprenant.date_de_naissance);
+    assert.equal(duplicates[0]._id.annee_scolaire, sampleEffectif.annee_scolaire);
+    assert.equal(duplicates[0]._id.formation_cfd, sampleEffectif.formation?.cfd);
+  });
+
+  it("Permet de vérifier la récupération de doublons d'effectifs avec un prénom multi-casse", async () => {
+    // Ajout de 2 doublons d'effectifs
+    const sampleEffectif = createSampleEffectif({ organisme: sampleOrganisme, apprenant: { prenom: "SYlvAiN" } });
+    await insertDuplicateEffectifs(sampleEffectif, 5);
+
+    const duplicates = await getDuplicatesEffectifsForOrganismeId(sampleOrganismeId);
+
+    // Vérification de la récupération d'une liste avec un doublon identifié 5 fois sur les champs de la clé d'unicité
+    assert.equal(duplicates.length, 1);
+    assert.equal(duplicates[0].count, 5);
+
+    assert.equal(duplicates[0]._id.nom_apprenant.toLowerCase(), sampleEffectif.apprenant.nom.toLowerCase());
+    assert.equal(duplicates[0]._id.prenom_apprenant.toLowerCase(), sampleEffectif.apprenant.prenom.toLowerCase());
+    assert.deepEqual(duplicates[0]._id.date_de_naissance_apprenant, sampleEffectif.apprenant.date_de_naissance);
+    assert.equal(duplicates[0]._id.annee_scolaire, sampleEffectif.annee_scolaire);
+    assert.equal(duplicates[0]._id.formation_cfd, sampleEffectif.formation?.cfd);
+  });
+
+  it("Permet de vérifier la récupération de doublons d'effectifs avec un nom multi-casse", async () => {
+    // Ajout de 2 doublons d'effectifs
+    const sampleEffectif = createSampleEffectif({ organisme: sampleOrganisme, apprenant: { nom: "mBaPpe" } });
+    await insertDuplicateEffectifs(sampleEffectif);
+
+    const duplicates = await getDuplicatesEffectifsForOrganismeId(sampleOrganismeId);
+
+    // Vérification de la récupération d'une liste avec un doublon identifié 2 fois sur les champs de la clé d'unicité
+    assert.equal(duplicates.length, 1);
+    assert.equal(duplicates[0].count, 2);
+
+    assert.equal(duplicates[0]._id.nom_apprenant.toLowerCase(), sampleEffectif.apprenant.nom.toLowerCase());
+    assert.equal(duplicates[0]._id.prenom_apprenant.toLowerCase(), sampleEffectif.apprenant.prenom.toLowerCase());
+    assert.deepEqual(duplicates[0]._id.date_de_naissance_apprenant, sampleEffectif.apprenant.date_de_naissance);
+    assert.equal(duplicates[0]._id.annee_scolaire, sampleEffectif.annee_scolaire);
+    assert.equal(duplicates[0]._id.formation_cfd, sampleEffectif.formation?.cfd);
+  });
+
+  it("Permet de vérifier la récupération de doublons d'effectifs avec un prénom avec caractères spéciaux, accents et espace", async () => {
+    // Ajout de 2 doublons d'effectifs
+    const sampleEffectif = createSampleEffectif({ organisme: sampleOrganisme, apprenant: { prenom: "JeAn- éDouArd" } });
+    await insertDuplicateEffectifs(sampleEffectif, 5);
+
+    const duplicates = await getDuplicatesEffectifsForOrganismeId(sampleOrganismeId);
+
+    // Vérification de la récupération d'une liste avec un doublon identifié 5 fois sur les champs de la clé d'unicité
+    assert.equal(duplicates.length, 1);
+    assert.equal(duplicates[0].count, 5);
+
+    assert.equal(duplicates[0]._id.nom_apprenant.toLowerCase(), sampleEffectif.apprenant.nom.toLowerCase());
+    assert.equal(duplicates[0]._id.prenom_apprenant.toLowerCase(), "jeanédouard"); // Transformation du prenom_apprenant en champ normalisé
+    assert.deepEqual(duplicates[0]._id.date_de_naissance_apprenant, sampleEffectif.apprenant.date_de_naissance);
+    assert.equal(duplicates[0]._id.annee_scolaire, sampleEffectif.annee_scolaire);
+    assert.equal(duplicates[0]._id.formation_cfd, sampleEffectif.formation?.cfd);
+  });
+
+  it("Permet de vérifier la récupération de doublons d'effectifs avec un nom avec caractères spéciaux, accents et espace", async () => {
+    // Ajout de 2 doublons d'effectifs
+    const sampleEffectif = createSampleEffectif({ organisme: sampleOrganisme, apprenant: { nom: "M' BaPpé" } });
+    await insertDuplicateEffectifs(sampleEffectif, 5);
+
+    const duplicates = await getDuplicatesEffectifsForOrganismeId(sampleOrganismeId);
+
+    // Vérification de la récupération d'une liste avec un doublon identifié 5 fois sur les champs de la clé d'unicité
+    assert.equal(duplicates.length, 1);
+    assert.equal(duplicates[0].count, 5);
+
+    assert.equal(duplicates[0]._id.nom_apprenant.toLowerCase(), "mbappé"); // Transformation du nom en champ normalisé
+    assert.equal(duplicates[0]._id.prenom_apprenant.toLowerCase(), sampleEffectif.apprenant.prenom.toLowerCase());
+    assert.deepEqual(duplicates[0]._id.date_de_naissance_apprenant, sampleEffectif.apprenant.date_de_naissance);
+    assert.equal(duplicates[0]._id.annee_scolaire, sampleEffectif.annee_scolaire);
+    assert.equal(duplicates[0]._id.formation_cfd, sampleEffectif.formation?.cfd);
+  });
+});
