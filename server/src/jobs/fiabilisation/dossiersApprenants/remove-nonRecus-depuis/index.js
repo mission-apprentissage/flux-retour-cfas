ede666b64461b19279a998395bda4c53a2e39a2f
Samir BENFARES <sbenfares@gmail.com>
2023-06-01 17:32:30 +0200

    chore(server): transformation rupturants en abandons 180 jrs
    wip
    
    
    wip
    
    
    wip

--- server/src/jobs/fiabilisation/dossiersApprenants/remove-nonRecus-depuis/index.js
+++ server/src/jobs/fiabilisation/dossiersApprenants/remove-nonRecus-depuis/index.js
@@ -1,8 +1,16 @@
-import { inscritsSansContratsIndicator } from "@/common/actions/effectifs/indicators";
+import { addDays } from "date-fns";
+
+import {
+  abandonsIndicator,
+  inscritsSansContratsIndicator,
+  rupturantsIndicator,
+} from "@/common/actions/effectifs/indicators";
+import { CODES_STATUT_APPRENANT } from "@/common/constants/dossierApprenant";
 import logger from "@/common/logger";
 import { effectifsDb } from "@/common/model/collections";
 
 const CURRENT_ANNEES_SCOLAIRES = ["2022-2023", "2023-2023"];
+const filterStages = [{ $match: { annee_scolaire: { $in: CURRENT_ANNEES_SCOLAIRES } } }];
 
 /**
  * Aggregation pour filtre sur le nb de jours dans ce statut
@@ -22,6 +30,8 @@ const getAggregateNbJoursDepuisStatutStages = (nbJours) => [
   },
   { $match: { nb_jours_depuis_statut: { $gte: nbJours } } },
 ];
+
+const getNbAbandonsADate = async () => (await abandonsIndicator.getListAtDate(new Date(), filterStages)).length;
 
 /**
  * Méthode de suppression des effectifs inscrits sans contrats pour les années scolaires courantes
@@ -46,4 +56,73 @@ export const removeInscritsSansContratsDepuis = async (nbJours = 90) => {
   return {
     deletedCount,
   };
+};
+
+/**
+ * Méthode de transformation des effectifs rupturants en abandons (dans ce statut depuis nbJours)
+ * @param {*} nbJours
+ */
+export const transformRupturantsToAbandonsDepuis = async (nbJours = 180) => {
+  logger.info(`${await getNbAbandonsADate()} abandons à date avant lancement du script`);
+
+  const dateAbandonToSet = addDays(new Date(), -180);
+
+  const rupturantsToTransform = await effectifsDb()
+    .aggregate([
+      ...filterStages,
+      ...rupturantsIndicator.getAtDateAggregationPipeline(new Date()),
+      ...getAggregateNbJoursDepuisStatutStages(nbJours),
+    ])
+    .toArray();
+
+  let nbUpdated = 0;
+  await Promise.all(
+    rupturantsToTransform.map(async (item) => {
+      const effectif = await effectifsDb().findOne({ _id: item._id });
+      if (!effectif) {
+        throw new Error(`Unable to find effectif ${item._id.toString()}`);
+      } else {
+        await updateEffectifRupturantToAbandon(effectif, dateAbandonToSet);
+        nbUpdated++;
+      }
+    })
+  );
+
+  logger.info(`${await getNbAbandonsADate()} abandons à date après lancement du script`);
+  logger.info(
+    `Transformation de ${nbUpdated} effectifs rupturants dans ce statut depuis ${nbJours} jours en abandons effectuée avec succès !`
+  );
+
+  return {
+    nbUpdated,
+  };
+};
+
+/**
+ * Fonction d'update d'un effectif en y ajoutant un élément de type ABANDON à son historique
+ * @param {*} effectif
+ * @param {*} abandonDate
+ */
+const updateEffectifRupturantToAbandon = async (effectif, abandonDate) => {
+  try {
+    // Ajout d'une entrée manuelle "ABANDON" à la date
+    effectif.apprenant.historique_statut.push({
+      valeur_statut: CODES_STATUT_APPRENANT.abandon,
+      date_statut: abandonDate,
+      date_reception: abandonDate,
+    });
+
+    await effectifsDb().findOneAndUpdate(
+      { _id: effectif._id },
+      {
+        $set: {
+          ...effectif,
+          updated_at: new Date(),
+        },
+      },
+      { returnDocument: "after", bypassDocumentValidation: true }
+    );
+  } catch (err) {
+    logger.error(err);
+  }
 };
