2b64da77edd4d2f7e96030821c65bcd1bd449140
Raphaël Huchet <raph@selego.co>
2023-06-06 11:37:17 +0200

    fix: remove user creation

--- server/tests/integration/http/password.route.test.ts
+++ server/tests/integration/http/password.route.test.ts
@@ -1,7 +1,6 @@
 import { AxiosInstance } from "axiosist";
 
 import { setTime } from "@/common/utils/timeUtils";
-import { createOrganisation, createUser } from "@tests/utils/helpers";
 import { initTestApp } from "@tests/utils/testUtils";
 
 const date = "2022-10-10T00:00:00.000Z";
@@ -17,23 +16,17 @@ describe("Password", () => {
   });
 
   describe("POST /v1/password/forgotten-password - forgotten password", () => {
-    beforeEach(async () => {
-      await Promise.all([createOrganisation(), createUser()]);
-    });
-
     it("retourne 400 si l'email est invalide", async () => {
       const response = await httpClient.post("/api/v1/password/forgotten-password", {
         email: "oops",
-        password: "oops",
       });
 
       expect(response.status).toBe(400);
     });
 
     it("retourne 200 si le mail est valide", async () => {
-      const response = await httpClient.post("/api/v1/auth/login", {
+      const response = await httpClient.post("/api/v1/password/forgotten-password", {
         email: "user@example.org",
-        password: "MDP-azerty123",
       });
       expect(response.status).toBe(200);
     });
