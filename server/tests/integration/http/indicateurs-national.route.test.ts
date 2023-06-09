9b21fc716ff5f91588941b7316c61164901431cd
Maxime Dréau <maxime@dreau.fr>
2023-06-09 14:12:10 +0200

    fix: tests

--- server/tests/integration/http/indicateurs-national.route.test.ts
+++ server/tests/integration/http/indicateurs-national.route.test.ts
@@ -1,13 +1,17 @@
 import { AxiosInstance } from "axiosist";
 
+import { organismesDb } from "@/common/model/collections";
+import { organismes } from "@tests/utils/permissions";
 import { initTestApp } from "@tests/utils/testUtils";
 
 let app: Awaited<ReturnType<typeof initTestApp>>;
 let httpClient: AxiosInstance;
+
 describe("GET /api/v1/indicateurs/national - liste des indicateurs sur les effectifs et organismes au national ", () => {
   beforeEach(async () => {
     app = await initTestApp();
     httpClient = app.httpClient;
+    await organismesDb().insertMany(organismes);
   });
 
   it("Accès public", async () => {
@@ -15,6 +19,14 @@ describe("GET /api/v1/indicateurs/national - liste des indicateurs sur les effec
     const response = await httpClient.get(`/api/v1/indicateurs/national?date=${date}`);
 
     expect(response.status).toEqual(200);
-    expect(response.status).toStrictEqual({ indicateursEffectifs: [], indicateursOrganismes: [] });
+    expect(response.data).toStrictEqual({
+      indicateursEffectifs: [],
+      indicateursOrganismes: {
+        total: 4,
+        responsablesFormateurs: 2,
+        responsables: 1,
+        formateurs: 1,
+      },
+    });
   });
 });
