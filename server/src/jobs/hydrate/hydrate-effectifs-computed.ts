76f60df65b262811047d642051771a78fac2f50f
Maxime Dréau <totakoko@users.noreply.github.com>
2023-06-12 17:27:13 +0200

    fix: bypass validation en attendant de corriger le schéma effectifs (#3048)

--- server/src/jobs/hydrate/hydrate-effectifs-computed.ts
+++ server/src/jobs/hydrate/hydrate-effectifs-computed.ts
@@ -5,44 +5,49 @@ export async function hydrateEffectifsComputed() {
   logger.info("Hydrating effectifs._computed...");
   while (
     (await effectifsDb()
-      .aggregate([
-        {
-          $lookup: {
-            from: "organismes",
-            localField: "organisme_id",
-            foreignField: "_id",
-            as: "_organisme",
+      .aggregate(
+        [
+          {
+            $lookup: {
+              from: "organismes",
+              localField: "organisme_id",
+              foreignField: "_id",
+              as: "_organisme",
+            },
           },
-        },
-        {
-          $addFields: {
-            _computed: {
-              organisme: {
-                region: { $first: "$_organisme.adresse.region" },
-                departement: { $first: "$_organisme.adresse.departement" },
-                academie: { $first: "$_organisme.adresse.academie" },
-                reseaux: { $first: "$_organisme.reseaux" },
-                uai: { $first: "$_organisme.uai" },
-                siret: { $first: "$_organisme.siret" },
-                bassinEmploi: { $first: "$_organisme.adresse.bassinEmploi" },
+          {
+            $addFields: {
+              _computed: {
+                organisme: {
+                  region: { $first: "$_organisme.adresse.region" },
+                  departement: { $first: "$_organisme.adresse.departement" },
+                  academie: { $first: "$_organisme.adresse.academie" },
+                  reseaux: { $first: "$_organisme.reseaux" },
+                  uai: { $first: "$_organisme.uai" },
+                  siret: { $first: "$_organisme.siret" },
+                  bassinEmploi: { $first: "$_organisme.adresse.bassinEmploi" },
+                },
               },
             },
           },
-        },
-        {
-          $project: {
-            _organisme: 0,
+          {
+            $project: {
+              _organisme: 0,
+            },
           },
-        },
-        {
-          $merge: {
-            into: "effectifs",
-            on: "_id",
-            whenMatched: "replace",
-            whenNotMatched: "discard",
+          {
+            $merge: {
+              into: "effectifs",
+              on: "_id",
+              whenMatched: "replace",
+              whenNotMatched: "discard",
+            },
           },
-        },
-      ])
+        ],
+        {
+          bypassDocumentValidation: true,
+        }
+      )
       .next()) !== null
   ) {
     //
