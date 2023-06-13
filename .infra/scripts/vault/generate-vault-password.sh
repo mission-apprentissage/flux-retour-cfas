7b4a1b4eb71eeebbca7f8cc8ba3919c74dbabef3
Samir Benfares <sbenfares@gmail.com>
2023-06-13 09:58:36 +0200

    chore(infra): renew-vault with raph (#3050)

--- .infra/scripts/vault/generate-vault-password.sh
+++ .infra/scripts/vault/generate-vault-password.sh
@@ -27,7 +27,7 @@ function create_password_file() {
   echo "Fetching gpg keys and add them as a recipients..."
   for key in "${keys[@]}"; do
     echo $key
-    gpg --quiet --recv-keys "$key"
+    gpg --quiet --keyserver keyserver.ubuntu.com --recv-keys "$key"
     recipients+=("--recipient $key")
   done
 
