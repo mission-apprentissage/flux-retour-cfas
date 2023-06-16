4d788492acaf9fb7080c140dec8c84182c46bc77
Raphaël Huchet <raph@selego.co>
2023-06-16 10:52:28 +0200

    chore: review fix

--- ui/pages/organismes.tsx
+++ ui/pages/organismes.tsx
@@ -1,15 +1,15 @@
 import { Box, Center, Container, Heading, Spinner, Text } from "@chakra-ui/react";
 import { SortingState } from "@tanstack/react-table";
 import { isBefore, subMonths } from "date-fns";
-import Head from "next/head";
 import { useRouter } from "next/router";
 import React, { useEffect, useMemo, useState } from "react";
 
 import { _get } from "@/common/httpClient";
 import { OrganisationType } from "@/common/internal/Organisation";
 import { formatDateNumericDayMonthYear } from "@/common/utils/dateUtils";
+import { normalize } from "@/common/utils/stringUtils";
 import Link from "@/components/Links/Link";
-import Page from "@/components/Page/Page";
+import SimplePage from "@/components/Page/SimplePage";
 import withAuth from "@/components/withAuth";
 import { useOrganisationOrganismes } from "@/hooks/organismes";
 import useAuth from "@/hooks/useAuth";
@@ -91,9 +91,9 @@ function MesOrganismes() {
   const organismesWithNormalizedNames = useMemo(() => {
     return (organismes || []).map((organisme) => ({
       ...organisme,
-      normalizedName: (organisme.nom || "").trim().toLocaleLowerCase(),
-      normalizedUai: (organisme.uai || "").trim().toLocaleLowerCase(),
-      normalizedCommune: (organisme.adresse?.commune || "").trim().toLocaleLowerCase(),
+      normalizedName: normalize(organisme.nom || ""),
+      normalizedUai: normalize(organisme.uai || ""),
+      normalizedCommune: normalize(organisme.adresse?.commune || ""),
     }));
   }, [organismes]);
 
@@ -101,7 +101,7 @@ function MesOrganismes() {
   const filteredOrganismes = useMemo(() => {
     if (searchValue.length < 2) return organismes;
 
-    const normalizedSearchValue = searchValue.trim().toLocaleLowerCase();
+    const normalizedSearchValue = normalize(searchValue);
     return organismesWithNormalizedNames.filter(
       (organisme) =>
         organisme.normalizedName.includes(normalizedSearchValue) ||
@@ -112,138 +112,138 @@ function MesOrganismes() {
   }, [organismesWithNormalizedNames, searchValue]);
 
   return (
-    <Page>
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <Box w="100%" pt={[4, 6]} px={[1, 1, 2, 4]} mb={16}>
-        <Container maxW="xl" px={0}>
-          <Heading textStyle="h2" color="grey.800" mb={5}>
-            {getHeaderTitleFromOrganisationType(organisationType)}
-          </Heading>
+    <SimplePage title={title}>
+      <Container maxW="xl" p="8">
+        <Heading as="h1" color="#465F9D" fontSize="beta" fontWeight="700" mb="4w">
+          {getHeaderTitleFromOrganisationType(organisationType)}
+        </Heading>
 
-          {isLoading && !organismes && (
-            <Center>
-              <Spinner />
-            </Center>
-          )}
-          {!isLoading && organismes && (
-            <>
-              <Input
-                {...{
-                  name: "search_organisme",
-                  fieldType: "text",
-                  mask: "C",
-                  maskBlocks: [
-                    {
-                      name: "C",
-                      mask: "Pattern",
-                      pattern: "^.*$",
-                    },
-                  ],
-                  placeholder:
-                    "Rechercher un organisme par nom, UAI, SIRET ou ville (indiquez au moins deux caractères)",
-                  value: searchValue,
-                  onSubmit: (value: string) => setSearchValue(value.trim()),
-                }}
-              />
+        {isLoading && !organismes && (
+          <Center>
+            <Spinner />
+          </Center>
+        )}
+        {!isLoading && organismes && (
+          <>
+            <Input
+              {...{
+                name: "search_organisme",
+                fieldType: "text",
+                mask: "C",
+                maskBlocks: [
+                  {
+                    name: "C",
+                    mask: "Pattern",
+                    pattern: "^.*$",
+                  },
+                ],
+                placeholder: "Rechercher un organisme par nom, UAI, SIRET ou ville (indiquez au moins deux caractères)",
+                value: searchValue,
+                onSubmit: (value: string) => setSearchValue(value.trim()),
+              }}
+            />
 
-              <NewTable
-                mt={4}
-                data={filteredOrganismes || []}
-                loading={false}
-                sortingState={sort}
-                onSortingChange={(state) => setSort(state)}
-                columns={[
-                  {
-                    header: () => "Nom de l’organisme",
-                    accessorKey: "nom",
-                    cell: ({ row }) => (
-                      <>
-                        <Link
-                          href={`/organismes/${organismes[row.id]._id}`}
-                          display="block"
-                          fontSize="1rem"
-                          width="var(--chakra-sizes-lg)"
-                          title={row.original.nom}
-                        >
-                          {row.original.nom ?? "Organisme inconnu"}
-                        </Link>
-                        <Text fontSize="xs" pt={2} color="#777777" whiteSpace="nowrap">
-                          UAI : {(row.original as any).uai} - SIRET : {(row.original as any).siret}
-                        </Text>
-                      </>
-                    ),
-                  },
-                  {
-                    accessorKey: "nature",
-                    header: () => "Nature",
-                    cell: ({ getValue }) => <NatureOrganismeTag nature={getValue()} />,
-                  },
-                  {
-                    accessorKey: "ferme",
-                    header: () => "État",
-                    cell: ({ getValue }) => (
-                      <div>
-                        {getValue() ? (
-                          <Text color="flatwarm" fontWeight="bold">
-                            Fermé
-                          </Text>
-                        ) : (
-                          <Text>Ouvert</Text>
-                        )}
-                      </div>
-                    ),
-                  },
-                  {
-                    accessorKey: "last_transmission_date",
-                    header: () => "Transmission au tdb",
-                    cell: ({ getValue }) => {
-                      const lastTransmissionDate = getValue();
-                      if (!lastTransmissionDate) return <Text color="tomato">Ne transmet pas</Text>;
-                      if (isMoreThanOrEqualOneMonthAgo(lastTransmissionDate)) {
-                        return (
-                          <Text color="orange">
-                            Ne transmet plus <br />
-                            depuis le {formatDateNumericDayMonthYear(lastTransmissionDate)}
-                          </Text>
-                        );
-                      }
-                      return <Text color="green">{formatDateNumericDayMonthYear(lastTransmissionDate)}</Text>;
-                    },
-                  },
-                  {
-                    accessorKey: "adresse",
-                    header: () => "Localisation",
-                    cell: ({ row }) => (
-                      <Box>
-                        {row.original.adresse?.commune || ""}
-                        <Text fontSize="xs" pt={2} color="#777777" whiteSpace="nowrap">
-                          {row.original.adresse.code_postal || ""}
-                          {row.original.adresse.code_insee &&
-                          row.original.adresse.code_postal !== row.original.adresse.code_insee
-                            ? ` (Insee: ${row.original.adresse.code_insee})`
-                            : ""}
-                        </Text>
-                      </Box>
-                    ),
-                  },
-                  {
-                    accessorKey: "more",
-                    header: () => "Voir",
-                    cell: ({ row }) => (
-                      <Link href={`/organismes/${organismes[row.id]._id}`} flexGrow={1}>
-                        <ArrowDropRightLine />
+            <NewTable
+              mt={4}
+              data={filteredOrganismes || []}
+              loading={false}
+              sortingState={sort}
+              onSortingChange={(state) => setSort(state)}
+              columns={[
+                {
+                  header: () => "Nom de l’organisme",
+                  accessorKey: "nom",
+                  cell: ({ row }) => (
+                    <>
+                      <Link
+                        href={`/organismes/${organismes[row.id]._id}`}
+                        display="block"
+                        fontSize="1rem"
+                        width="var(--chakra-sizes-lg)"
+                        title={row.original.nom}
+                      >
+                        {row.original.nom ?? "Organisme inconnu"}
                       </Link>
-                    ),
+                      <Text fontSize="xs" pt={2} color="#777777" whiteSpace="nowrap">
+                        UAI : {(row.original as any).uai} - SIRET : {(row.original as any).siret}
+                      </Text>
+                    </>
+                  ),
+                },
+                {
+                  accessorKey: "nature",
+                  header: () => "Nature",
+                  cell: ({ getValue }) => <NatureOrganismeTag nature={getValue()} />,
+                },
+                {
+                  accessorKey: "ferme",
+                  header: () => "État",
+                  cell: ({ getValue }) => (
+                    <div>
+                      {getValue() ? (
+                        <Text color="flatwarm" fontWeight="bold">
+                          Fermé
+                        </Text>
+                      ) : (
+                        <Text>Ouvert</Text>
+                      )}
+                    </div>
+                  ),
+                },
+                {
+                  accessorKey: "last_transmission_date",
+                  header: () => "Transmission au tdb",
+                  cell: ({ getValue }) => {
+                    const lastTransmissionDate = getValue();
+                    if (!lastTransmissionDate) return <Text color="tomato">Ne transmet pas</Text>;
+                    if (isMoreThanOrEqualOneMonthAgo(lastTransmissionDate)) {
+                      return (
+                        <Text color="orange">
+                          Ne transmet plus <br />
+                          depuis le {formatDateNumericDayMonthYear(lastTransmissionDate)}
+                        </Text>
+                      );
+                    }
+                    return <Text color="green">{formatDateNumericDayMonthYear(lastTransmissionDate)}</Text>;
                   },
-                ]}
-              />
-            </>
-          )}
-        </Container>
-      </Box>
-    </Page>
+                },
+                {
+                  accessorKey: "adresse",
+                  sortingFn: (a, b) => {
+                    const communeA = a.original.adresse?.commune || "";
+                    const communeB = b.original.adresse?.commune || "";
+                    return communeA.localeCompare(communeB);
+                  },
+                  header: () => "Localisation",
+                  cell: ({ row }) => (
+                    <Box>
+                      {row.original.adresse?.commune || ""}
+                      <Text fontSize="xs" pt={2} color="#777777" whiteSpace="nowrap">
+                        {row.original.adresse.code_postal || ""}
+                        {row.original.adresse.code_insee &&
+                        row.original.adresse.code_postal !== row.original.adresse.code_insee
+                          ? ` (Insee: ${row.original.adresse.code_insee})`
+                          : ""}
+                      </Text>
+                    </Box>
+                  ),
+                },
+                {
+                  accessorKey: "more",
+                  enableSorting: false,
+                  header: () => "Voir",
+                  cell: ({ row }) => (
+                    <Link href={`/organismes/${organismes[row.id]._id}`} flexGrow={1}>
+                      <ArrowDropRightLine />
+                    </Link>
+                  ),
+                },
+              ]}
+            />
+          </>
+        )}
+      </Container>
+    </SimplePage>
   );
 }
 
