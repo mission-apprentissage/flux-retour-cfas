06a14f9a5995c7e5ac5c2caac0c540beb9ab0d9b
Maxime Dréau <maxime@dreau.fr>
2023-07-06 12:20:33 +0200

    feat: poc intégration dashboard metabase signé dans l'UI

--- ui/common/SSR/getAuthServerSideProps.ts
+++ ui/common/SSR/getAuthServerSideProps.ts
@@ -1,19 +1,21 @@
-import axios from "axios";
+import { GetServerSidePropsContext } from "next";
 
-const isInitialServerSideProps = (context) => context.req?.url?.indexOf("/_next/data/") === -1;
+const isInitialServerSideProps = (context: GetServerSidePropsContext) =>
+  context.req?.url?.indexOf("/_next/data/") === -1;
 
 export const getAuthServerSideProps = async (context) => {
   if (!isInitialServerSideProps(context)) {
     return {};
   }
   try {
-    const { status, data } = await axios.get(`${process.env.SERVER_URI}/api/v1/session`, {
+    const res = await fetch(`${process.env.SERVER_URI}/api/v1/session`, {
       headers: context.req.headers,
     });
     return {
-      auth: status === 200 ? data : null,
+      auth: res.status === 200 ? await res.json() : null,
     };
-  } catch (e) {
+  } catch (err) {
+    console.error("ssr auth err", err);
     return { auth: null };
   }
 };
---
+++ ui/pages/indicateurs/graphiques.tsx
@@ -0,0 +1,55 @@
+import { Container, Heading } from "@chakra-ui/react";
+import { sign } from "jsonwebtoken";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Head from "next/head";
+
+import { AuthContext } from "@/common/internal/AuthContext";
+import { getAuthServerSideProps } from "@/common/SSR/getAuthServerSideProps";
+import SimplePage from "@/components/Page/SimplePage";
+import withAuth from "@/components/withAuth";
+
+export const getServerSideProps: GetServerSideProps<{ iframeUrl: string; auth: any }> = async (context) => {
+  const { auth } = (await getAuthServerSideProps(context)) as { auth: AuthContext };
+
+  let iframeUrl = "";
+  if (
+    auth?.organisation.type === "DREETS" ||
+    auth?.organisation.type === "CONSEIL_REGIONAL" ||
+    auth?.organisation.type === "DRAAF"
+  ) {
+    const METABASE_SITE_URL = process.env.METABASE_SITE_URL;
+    const METABASE_SECRET_KEY = process.env.METABASE_SECRET_KEY;
+
+    const payload = {
+      resource: { dashboard: 86 },
+      params: {
+        "r%C3%A9gion": auth.organisation.code_region,
+      },
+      exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
+    };
+    const token = sign(payload, METABASE_SECRET_KEY);
+
+    iframeUrl = `${METABASE_SITE_URL}/embed/dashboard/${token}#bordered=true&titled=true`;
+  }
+
+  return { props: { iframeUrl, auth } };
+};
+
+function IndicateursGraphiquesPage({ iframeUrl }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <SimplePage>
+      <Head>
+        <title>Mes indicateurs - graphiques</title>
+      </Head>
+
+      <Container maxW="xl" p="8">
+        <Heading as="h1" color="#465F9D" fontSize="beta" fontWeight="700" mb={3}>
+          Mes indicateurs
+        </Heading>
+        <iframe src={iframeUrl} frameBorder="0" style={{ height: "1000px", width: "100%" }} allowTransparency />
+      </Container>
+    </SimplePage>
+  );
+}
+
+export default withAuth(IndicateursGraphiquesPage);
