d50cefdd0bf680cfceb34aad3c54c648fed2d20b
Samir BENFARES <sbenfares@gmail.com>
2023-07-07 11:11:52 +0200

    chore(server): ajout d'un script de remplissage de la collection organismesSoltea

--- server/src/common/model/@types/OrganismeSoltea.ts
+++ server/src/common/model/@types/OrganismeSoltea.ts
@@ -5,4 +5,9 @@ export interface OrganismeSoltea {
   commune?: string;
   code_postal?: string;
   departement?: string;
+  ligne1_adresse?: string;
+  ligne2_adresse?: string;
+  ligne3_adresse?: string;
+  ligne4_adresse?: string;
+  ligne5_adresse?: string;
 }
