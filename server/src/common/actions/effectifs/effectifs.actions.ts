32727b24fee24c383996685293cea529e7cd3c71
Raphaël Huchet <raph@selego.co>
2023-08-14 16:46:56 +0200

    chore: clean dead code backend

--- server/src/common/actions/effectifs/effectifs.actions.ts
+++ server/src/common/actions/effectifs/effectifs.actions.ts
@@ -8,11 +8,9 @@ import {
   organismeLookup,
 } from "@/common/actions/helpers/filters";
 import {
-  getEffectifsAnonymesRestriction,
   getIndicateursEffectifsRestriction,
   requireOrganismeIndicateursAccess,
 } from "@/common/actions/helpers/permissions";
-import { DEPARTEMENTS_BY_CODE } from "@/common/constants/territoires";
 import { effectifsDb, organismesDb } from "@/common/model/collections";
 import { AuthContext } from "@/common/model/internal/AuthContext";
 import { mergeObjectsBy } from "@/common/utils/mergeObjectsBy";
@@ -97,12 +95,7 @@ export const getIndicateurs = async (
   };
 };
 
-/**
- * Récupération des effectifs pour tous les indicateurs du TdB
- * @param {*} filters
- * @param {*} options
- * @returns
- */
+// Récupération des effectifs pour tous les indicateurs du TdB
 export const getEffectifsCountAtDate = async (
   filters: any,
   { additionalFilterStages = [], groupedBy, projection }: any
@@ -132,243 +125,7 @@ export const getEffectifsCountAtDate = async (
   return mergeObjectsBy([...apprentis, ...inscritsSansContrat, ...rupturants, ...abandons], "_id");
 };
 
-/**
- * Récupération des effectifs par niveau de formation à une date donnée
- * @param {*} filters
- * @returns [{
- *  niveau_formation: string
- *  niveau_formation_libelle: string
- *  effectifs: {
- *    apprentis: number
- *    inscritsSansContrat: number
- *    rupturants: number
- *    abandons: number
- *  }
- * }]
- */
-export const getEffectifsCountByNiveauFormationAtDate = async (ctx: AuthContext, filters: LegacyEffectifsFilters) => {
-  // compute number of apprentis, abandons, inscrits sans contrat and rupturants
-  const filtersWithRestriction = await checkIndicateursFiltersPermissions(ctx, filters);
-  const effectifsByNiveauFormation = await getEffectifsCountAtDate(filtersWithRestriction, {
-    additionalFilterStages: [{ $match: { "formation.niveau": { $ne: null } } }],
-    projection: { "formation.niveau": 1, "formation.niveau_libelle": 1 },
-    groupedBy: { _id: "$formation.niveau", niveau_libelle: { $first: "$formation.niveau_libelle" } },
-  });
-
-  return effectifsByNiveauFormation.map(({ _id: niveauFormation, niveau_libelle, ...effectifs }: any) => ({
-    niveau_formation: niveauFormation,
-    niveau_formation_libelle: niveau_libelle,
-    effectifs: {
-      apprentis: effectifs.apprentis || 0,
-      inscritsSansContrat: effectifs.inscritsSansContrat || 0,
-      rupturants: effectifs.rupturants || 0,
-      abandons: effectifs.abandons || 0,
-    },
-  }));
-};
-
-/**
- * Récupération des effectifs par formation à une date donnée
- * @param {*} filters
- * @returns [{
- *  formation_cfd: string
- *  intitule: string
- *  effectifs: {
- *    apprentis: number
- *    inscritsSansContrat: number
- *    rupturants: number
- *    abandons: number
- *  }
- * }]
- */
-export const getEffectifsCountByFormationAtDate = async (ctx: AuthContext, filters: LegacyEffectifsFilters) => {
-  const filtersWithRestriction = await checkIndicateursFiltersPermissions(ctx, filters);
-  const effectifsByFormation = await getEffectifsCountAtDate(filtersWithRestriction, {
-    projection: { "formation.cfd": 1, "formation.libelle_long": 1 },
-    groupedBy: {
-      _id: "$formation.cfd",
-      // we will send formation.libelle_long along with the grouped effectifs so we need to project it
-      libelle_long_formation: { $first: "$formation.libelle_long" },
-    },
-  });
-
-  return effectifsByFormation.map(({ _id: cfd, libelle_long_formation, ...effectifs }: any) => ({
-    formation_cfd: cfd,
-    intitule: libelle_long_formation,
-    effectifs: {
-      apprentis: effectifs.apprentis || 0,
-      inscritsSansContrat: effectifs.inscritsSansContrat || 0,
-      rupturants: effectifs.rupturants || 0,
-      abandons: effectifs.abandons || 0,
-    },
-  }));
-};
-
-/**
- * Récupération des effectifs par annee_formation à une date donnée
- * @param {*} filters
- * @returns [{
- *  annee_formation: string
- *  effectifs: {
- *    apprentis: number
- *    inscritsSansContrat: number
- *    rupturants: number
- *    abandons: number
- *  }
- * }]
- */
-export const getEffectifsCountByAnneeFormationAtDate = async (ctx: AuthContext, filters: LegacyEffectifsFilters) => {
-  const filtersWithRestriction = await checkIndicateursFiltersPermissions(ctx, filters);
-  const effectifsByAnneeFormation = await getEffectifsCountAtDate(filtersWithRestriction, {
-    projection: { "formation.annee": 1 },
-    groupedBy: { _id: "$formation.annee" },
-  });
-
-  return effectifsByAnneeFormation.map(({ _id: anneeFormation, ...effectifs }: any) => ({
-    annee_formation: anneeFormation,
-    effectifs: {
-      apprentis: effectifs.apprentis || 0,
-      inscritsSansContrat: effectifs.inscritsSansContrat || 0,
-      rupturants: effectifs.rupturants || 0,
-      abandons: effectifs.abandons || 0,
-    },
-  }));
-};
-
-/**
- * Récupération des effectifs par uai_etablissement à une date donnée
- * @param {*} filters
- * @returns [{
- *  uai_etablissement: string
- *  siret_etablissement: string
- *  nom_etablissement: string
- *  effectifs: {
- *    apprentis: number
- *    inscritsSansContrat: number
- *    rupturants: number
- *    abandons: number
- *  }
- * }]
- */
-export const getEffectifsCountByCfaAtDate = async (ctx: AuthContext, filters: LegacyEffectifsFilters) => {
-  // on descend au niveau des organismes, donc restriction comme pour les effectifs anonymes
-  (filters as EffectifsFiltersWithRestriction).restrictionMongo = await getEffectifsAnonymesRestriction(ctx);
-  const effectifsCountByCfa = await getEffectifsCountAtDate(filters, {
-    // we need to project these fields to give information about the CFAs
-    additionalFilterStages: [{ $lookup: organismeLookup }],
-    projection: {
-      "organisme.uai": 1,
-      "organisme.siret": 1,
-      "organisme.nom": 1,
-      "organisme.nature": 1,
-    },
-    groupedBy: {
-      _id: { $first: "$organisme.uai" },
-      // we will send information about the organisme along with the grouped effectifs so we project it
-      nom_etablissement: { $first: { $first: "$organisme.nom" } },
-      siret_etablissement: { $first: { $first: "$organisme.siret" } },
-      nature: { $first: { $first: "$organisme.nature" } },
-    },
-  });
-  return effectifsCountByCfa.map(({ _id: uai, nom_etablissement, siret_etablissement, nature, ...effectifs }: any) => ({
-    uai_etablissement: uai,
-    siret_etablissement,
-    nom_etablissement,
-    nature,
-    effectifs: {
-      apprentis: effectifs.apprentis || 0,
-      inscritsSansContrat: effectifs.inscritsSansContrat || 0,
-      rupturants: effectifs.rupturants || 0,
-      abandons: effectifs.abandons || 0,
-    },
-  }));
-};
-
-/**
- * Récupération des effectifs par siret_etablissement à une date donnée
- * @param {*} filters
- * @returns [{
- *  siret_etablissement: string
- *  nom_etablissement: string
- *  effectifs: {
- *    apprentis: number
- *    inscritsSansContrat: number
- *    rupturants: number
- *    abandons: number
- *  }
- * }]
- */
-export const getEffectifsCountBySiretAtDate = async (ctx: AuthContext, filters: LegacyEffectifsFilters) => {
-  const filtersWithRestriction = await checkIndicateursFiltersPermissions(ctx, filters);
-  const effectifsCountByCfa = await getEffectifsCountAtDate(filtersWithRestriction, {
-    additionalFilterStages: [{ $lookup: organismeLookup }, { $match: { "organisme.siret": { $ne: null } } }],
-    // we need to project these fields to give information about the CFAs
-    projection: {
-      "organisme.siret": 1,
-      "organisme.nom": 1,
-    },
-    groupedBy: {
-      _id: { $first: "$organisme.siret" },
-      // we will send information about the organisme along with the grouped effectifs so we project it
-      nom_etablissement: { $first: { $first: "$organisme.nom" } },
-    },
-  });
-
-  return effectifsCountByCfa.map(({ _id: siret, nom_etablissement, ...effectifs }: any) => ({
-    siret_etablissement: siret,
-    nom_etablissement,
-    effectifs: {
-      apprentis: effectifs.apprentis || 0,
-      inscritsSansContrat: effectifs.inscritsSansContrat || 0,
-      rupturants: effectifs.rupturants || 0,
-      abandons: effectifs.abandons || 0,
-    },
-  }));
-};
-
-/**
- * Récupération des effectifs par etablissement_num_departement à une date donnée
- * @param {*} filters
- * @returns [{
- *  etablissement_num_departement: string
- *  etablissement_nom_departement: string
- *  effectifs: {
- *    apprentis: number
- *    inscritsSansContrat: number
- *    rupturants: number
- *    abandons: number
- *  }
- * }]
- */
-export const getEffectifsCountByDepartementAtDate = async (ctx: AuthContext, filters: LegacyEffectifsFilters) => {
-  const filtersWithRestriction = await checkIndicateursFiltersPermissions(ctx, filters);
-  const effectifsCountByDepartement = await getEffectifsCountAtDate(filtersWithRestriction, {
-    additionalFilterStages: [{ $lookup: organismeLookup }],
-    projection: {
-      "organisme.adresse.departement": 1,
-    },
-    groupedBy: {
-      _id: { $first: "$organisme.adresse.departement" },
-    },
-  });
-
-  return effectifsCountByDepartement.map(({ _id: codeDepartement, ...effectifs }: any) => ({
-    etablissement_num_departement: codeDepartement,
-    etablissement_nom_departement: DEPARTEMENTS_BY_CODE[codeDepartement]?.nom || "Inconnu",
-    effectifs: {
-      apprentis: effectifs.apprentis || 0,
-      inscritsSansContrat: effectifs.inscritsSansContrat || 0,
-      rupturants: effectifs.rupturants || 0,
-      abandons: effectifs.abandons || 0,
-    },
-  }));
-};
-
-/**
- * Méthode de récupération de la liste des effectifs en base
- * @param {*} query
- * @returns
- */
+// Méthode de récupération de la liste des effectifs en base
 export const getAllEffectifs = async (
   query = {},
   { page, limit, sort } = { page: 1, limit: 10, sort: { created_at: -1 } as { [key: string]: number } }
@@ -416,12 +173,8 @@ export const getAllEffectifs = async (
   return result;
 };
 
-/**
- * Méthode de récupération d'un effectif et de ses détails (formation, doublons, ...) depuis son id
- * @param {*} _id
- * @returns
- */
-export const getDetailedEffectifById = async (_id) => {
+// Méthode de récupération d'un effectif et de ses détails (formation, doublons, ...) depuis son id
+export const getDetailedEffectifById = async (_id: any) => {
   const organisme = await effectifsDb()
     .aggregate(
       [
