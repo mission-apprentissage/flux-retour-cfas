b789b5728d0d68887059d9872e37148b28164402
Antoine Bigard <bigard.antoine@gmail.com>
2023-08-28 16:51:32 +0200

    chore: remove previous infra

--- .infra_previous/ansible/roles/setup/files/app/cli.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-#Needs to be run as sudo
-
-echo "****************************"
-echo "[$(date +'%Y-%m-%d_%H%M%S')] Running ${BASH_SOURCE[0]} $*"
-echo "****************************"
-docker exec flux_retour_cfas_server yarn --silent cli "$@"
--- .infra_previous/ansible/roles/setup/files/app/tools/gpg/decrypt.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-gpg --decrypt --default-key "mna_devops" $@
