5a5fad4a987c8056e80388617fa004c0fab61ac8
Maxime Dréau <totakoko@users.noreply.github.com>
2023-08-29 09:04:41 +0200

    feat: intégration de l'onglet indicateurs - vue graphique (#3145)

---
+++ ui/modules/indicateurs/permissions-onglet-graphique.ts
@@ -0,0 +1,22 @@
+import { OrganisationType } from "@/common/internal/Organisation";
+
+export function canViewOngletIndicateursVueGraphique(organisationType: OrganisationType): boolean {
+  switch (organisationType) {
+    case "ORGANISME_FORMATION_FORMATEUR":
+    case "ORGANISME_FORMATION_RESPONSABLE":
+    case "ORGANISME_FORMATION_RESPONSABLE_FORMATEUR":
+    case "TETE_DE_RESEAU":
+      return false;
+
+    case "DREETS":
+    case "DRAAF":
+    case "DDETS":
+    case "CONSEIL_REGIONAL":
+    case "CARIF_OREF_REGIONAL":
+    case "ACADEMIE":
+    case "OPERATEUR_PUBLIC_NATIONAL":
+    case "CARIF_OREF_NATIONAL":
+    case "ADMINISTRATEUR":
+      return true;
+  }
+}
