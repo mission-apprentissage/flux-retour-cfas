41752fd420e63b9803308b9bbf99c5081c625561
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-08-31 00:32:09 +0200

    fix: preview

--- .infra/files/scripts/seed.sh
+++ .infra/files/scripts/seed.sh
@@ -2,18 +2,22 @@
 
 set -euo pipefail
 
-readonly TARGET_DB="{{ vault[env_type].MNA_TDB_MONGODB_URI }}"
+readonly TARGET_DB=${1:?"Merci de préciser le nom de la base de donnée cible"}
+shift
 readonly SEED_ARCHIVE=$(mktemp seed_archive.XXXXXXXXXX)
+readonly PASSPHRASE=$(mktemp passphrase.XXXXXXXXXX)
 
 delete_cleartext() {
-  rm -f "$SEED_GZ" "$PASSPHRASE_FILE"
+  rm -f "$SEED_ARCHIVE" "$PASSPHRASE"
 }
 
 trap delete_cleartext EXIT
 
-chmod 600 "$PASSPHRASE"
-chmod 600 "$SEED_ARCHIVE"
-
 echo "{{ vault.SEED_GPG_PASSPHRASE }}" > "$PASSPHRASE"
+
+chmod 600 "$PASSPHRASE"
+
+rm -r "$SEED_ARCHIVE"
 gpg -d --batch --passphrase-file "$PASSPHRASE" -o "$SEED_ARCHIVE" "/opt/app/configs/mongodb/seed.gpg"
-cat "$SEED_GZ" | docker compose -f "$ROOT_DIR/docker-compose.yml" exec -iT mongodb mongorestore --archive --nsInclude="flux-retour-cfas.*" --uri="${TARGET_DB}" --drop --gzip
+chmod 600 "$SEED_ARCHIVE"
+cat "$SEED_ARCHIVE" | docker compose -f "/opt/app/docker-compose.preview-system.yml" exec -iT mongodb mongorestore --archive --nsFrom="flux-retour-cfas.*" --nsTo="$TARGET_DB.*" --drop --gzip
