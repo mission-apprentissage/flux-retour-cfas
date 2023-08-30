793799ebc61cba4b371ad3c4e1718a12c6f7bf3f
Antoine Bigard <bigard.antoine@gmail.com>
2023-08-30 11:57:54 +0200

    fix: var tdb public url

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,14 +1,13 @@
 import env from "env-var";
 
-const env_type = env.get("MNA_TDB_ENV").required().asString();
 const config = {
   email: env.get("MNA_TDB_EMAIL").default("tableau-de-bord@apprentissage.beta.gouv.fr").asString(),
   email_from: env.get("MNA_TDB_EMAIL_FROM").default("Tableau de bord de l'apprentissage").asString(),
   appName: env.get("MNA_TDB_NAME").default("Flux Retour Cfas").asString(),
   version: env.get("PUBLIC_VERSION").default("0.0.0-local").asString(),
   port: env.get("MNA_TDB_SERVER_PORT").default(5000).asPortNumber(),
-  env: env_type,
-  publicUrl: `${env_type === "local" ? "http" : "https"}://${env.get("MNA_TDB_PUBLIC_URL").required().asString()}`,
+  env: env.get("MNA_TDB_ENV").required().asString(),
+  publicUrl: env.get("MNA_TDB_PUBLIC_URL").required().asString(),
   bodyParserLimit: env.get("MNA_TDB_BODY_PARSER_LIMIT").default("10mb").asString(),
   mongodb: {
     uri: env.get("MNA_TDB_MONGODB_URI").required().asString(),
