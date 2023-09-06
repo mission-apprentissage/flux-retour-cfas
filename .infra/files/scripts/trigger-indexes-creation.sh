90b88b7381ac6c7bc62840f31f5bbbd1a2785a61
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-09-06 16:35:48 +0200

    chore: show logs

--- .infra/files/scripts/trigger-indexes-creation.sh
+++ .infra/files/scripts/trigger-indexes-creation.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 #Needs to be run as sudo
 
 readonly LOG_DIR="/var/log/data-jobs"
-readonly LOG_FILEPATH="${LOG_DIR}/run-indexes-creation_$(date +'%Y-%m-%d_%H%M%S').log"
 
 if [ ! -d "$LOG_DIR" ]; then
     sudo mkdir -p "$LOG_DIR"
@@ -12,7 +11,7 @@ fi
 
 trigger_indexes_creation(){
     echo "Création des index mongoDb ..."
-    docker compose run --rm --no-deps server yarn cli indexes:create --queued 2>&1 | tee "$LOG_FILEPATH"
+    docker compose run --rm --no-deps server yarn cli indexes:create --queued
 } 
 
 trigger_indexes_creation
