da3de7d9d97be36437adf5c5c8c65491ba5c3f45
Samir Benfares <sbenfares@gmail.com>
2023-09-15 09:12:01 +0200

    feat(server): update du réseau CFA EC (#3198)

--- server/src/jobs/hydrate/reseaux/hydrate-reseaux.ts
+++ server/src/jobs/hydrate/reseaux/hydrate-reseaux.ts
@@ -36,8 +36,8 @@ const INPUT_FILES = [
   "reseaux/referentiel-reseau-greta.csv", // GRETA
   "reseaux/referentiel-reseau-en.csv", // EDUC. NAT
   // "reseaux/referentiel-reseau-ccca-btp.csv", // TODO Fichier non fourni pour l'instant
-  "reseaux/referentiel-reseau-cfa-ec.csv", // CFA EC
   "reseaux/referentiel-reseau-amue.csv", // AMUE
+  "reseaux/referentiel-reseau-cfa-ec.csv", // CFA EC
 ];
 
 /**
