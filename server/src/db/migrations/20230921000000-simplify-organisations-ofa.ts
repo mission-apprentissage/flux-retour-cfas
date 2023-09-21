f3fc0dae2d90658f6a7e26fd9d75a17e90f26de2
Maxime Dréau <maxime@dreau.fr>
2023-09-21 14:56:21 +0200

    refactor: migration des données organisations

---
+++ server/src/db/migrations/20230921000000-simplify-organisations-ofa.ts
@@ -0,0 +1,20 @@
+import { Db } from "mongodb";
+
+export const up = async (db: Db) => {
+  await db.collection("organisations").updateMany(
+    {
+      type: {
+        $in: [
+          "ORGANISME_FORMATION_FORMATEUR",
+          "ORGANISME_FORMATION_RESPONSABLE",
+          "ORGANISME_FORMATION_RESPONSABLE_FORMATEUR",
+        ],
+      },
+    },
+    {
+      $set: {
+        type: "ORGANISME_FORMATION",
+      },
+    }
+  );
+};
