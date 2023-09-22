37c12c124e714068ac1e9ba9a777fa9262ae26cc
Raphaël Huchet <raph@selego.co>
2023-09-22 13:06:46 +0200

    chore: ajout de nouveaux champs dans les informations d'un effectif

--- ui/modules/mon-espace/effectifs/engine/cerfaForm/blocks/formation/formationSchema.ts
+++ ui/modules/mon-espace/effectifs/engine/cerfaForm/blocks/formation/formationSchema.ts
@@ -16,12 +16,39 @@ export const formationSchema = {
     min: 1,
     precision: 0,
   },
-  "formation.date_debut_formation": {
+  "formation.duree_theorique": {
+    fieldType: "numberStepper",
+    required: true,
+    label: "Durée théorique en années :",
+    requiredMessage: "Le nombre d'années théorique est obligatoire",
+    validateMessage: " n'est pas un nombre d'année valide",
+    mask: "C",
+    maskBlocks: [
+      {
+        name: "C",
+        mask: "Pattern",
+        pattern: "^\\d*$",
+      },
+    ],
+    min: 1,
+    max: 4,
+    precision: 0,
+  },
+  "formation.annee": {
+    fieldType: "number",
+    label: "Année de la formation concernée",
+  },
+  "formation.date_inscription": {
     fieldType: "date",
-    label: "Date de début de formation :",
+    label: "Date d'inscription en formation :",
     showInfo: false,
   },
-  "formation.date_fin_formation": {
+  "formation.date_entree": {
+    fieldType: "date",
+    label: "Date d'entrée en formation :",
+    showInfo: false,
+  },
+  "formation.date_fin": {
     fieldType: "date",
     label: "Date de fin de formation :",
     showInfo: false,
