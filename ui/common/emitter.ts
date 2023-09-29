edaf1a5aee2cd9e28c8bcb48863c9532c6bda16e
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2023-09-29 15:24:53 +0200

    chore: suppression de quelques fonctions inutilisées (#3253)

--- ui/common/emitter.ts
+++ ui/common/emitter.ts
@@ -1,6 +1,5 @@
 import EventEmitter from "events";
 
 const emitter = new EventEmitter();
-const subscribeToHttpEvent = (eventName, callback) => emitter.on(eventName, callback);
 
-export { emitter, subscribeToHttpEvent };
+export { emitter };
--- ui/common/utils/stringUtils.ts
+++ ui/common/utils/stringUtils.ts
@@ -1,25 +1,5 @@
 import { validateSiret } from "@/common/domain/siret";
 
-/**
- * Returns true if substring is found in given string. Case insensitive.
- * @param {string} str
- * @param {string} substr
- */
-export const stringContains = (str, substr) => str.toLowerCase().indexOf(substr.toLowerCase()) > -1;
-
-/**
- * Returns true if both strings are equal. Case insensitive.
- * @param {string} str
- * @param {string} substr
- */
-export const stringEqualsCaseInsensitive = (str1 = "", str2 = "") => {
-  return str1.toLowerCase() === str2.toLowerCase();
-};
-
-export const toPrettyYearLabel = (year) => (year === 1 ? `${year}ère année` : `${year}ème année`);
-
-export const pluralize = (text, value, pluralCharacter = "s") => (value > 1 ? `${text}${pluralCharacter}` : `${text}`);
-
 export const formatNumber = (number) => {
   if (!number) return number;
   return Number(number).toLocaleString();
