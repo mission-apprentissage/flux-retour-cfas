f27fe100ba19ab27602c0bcf9e11628b47dac5e9
Samir BENFARES <sbenfares@gmail.com>
2023-10-10 20:37:52 +0200

    feat(ui): hide filter uai on organismes fiables

--- ui/modules/organismes/tabs/OrganismesFiablesPanelContent.tsx
+++ ui/modules/organismes/tabs/OrganismesFiablesPanelContent.tsx
@@ -43,7 +43,6 @@ function OrganismesFiablesPanelContent({ organismes }: { organismes: OrganismeNo
         showFilterQualiopi
         showFilterPrepaApprentissage
         showFilterLocalisation
-        showFilterUai
       />
     </Stack>
   );
