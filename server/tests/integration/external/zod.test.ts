2746a272068e9fc6ef7a07d3c54ed65e937e8662
Maxime Dréau <maxime@dreau.fr>
2023-10-19 11:31:41 +0200

    test: ajoute le test sur le preprocess

--- server/tests/integration/external/zod.test.ts
+++ server/tests/integration/external/zod.test.ts
@@ -18,4 +18,35 @@ describe("zod", () => {
       });
     });
   });
+
+  // https://github.com/colinhacks/zod/pull/2719
+  it("preprocess validates with sibling errors", () => {
+    expect(() => {
+      z.object({
+        // Must be first
+        missing: z.string().refine(() => false),
+        preprocess: z.preprocess((data: any) => data?.trim(), z.string().regex(/ asdf/)),
+      }).parse({ preprocess: " asdf" });
+    }).toThrow(
+      JSON.stringify(
+        [
+          {
+            code: "invalid_type",
+            expected: "string",
+            received: "undefined",
+            path: ["missing"],
+            message: "Required",
+          },
+          {
+            validation: "regex",
+            code: "invalid_string",
+            message: "Invalid",
+            path: ["preprocess"],
+          },
+        ],
+        null,
+        2
+      )
+    );
+  });
 });
