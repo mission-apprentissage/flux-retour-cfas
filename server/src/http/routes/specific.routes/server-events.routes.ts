9cd3cb013096019e45a09fb3ec35d4159c7d48a8
Maxime Dréau <maxime@dreau.fr>
2023-10-24 12:22:07 +0200

    refactor: remove obsolete routes

--- server/src/http/routes/specific.routes/server-events.routes.ts
+++
@@ -1,9 +0,0 @@
-let clients: any[] = [];
-
-export function sendServerEventsForUser(userId, message) {
-  clients
-    .filter((client) => client.userId.toString() === userId.toString())
-    .map((client) => {
-      client.response.write(`data: ${JSON.stringify(message)}\n\n`);
-    });
-}
