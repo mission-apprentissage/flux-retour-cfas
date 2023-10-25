be6f72e1035735025e1967e6b02a88087096a80c
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-10-25 14:51:33 +0200

    chore: fix globalTeardown

--- server/tests/jest/globalTeardown.ts
+++ server/tests/jest/globalTeardown.ts
@@ -4,12 +4,25 @@ import { MongoClient } from "mongodb";
 export default async () => {
   dotenv.config({ path: "./server/.env.test" });
 
-  const workerId = `${process.env.JEST_WORKER_ID}`;
-
+  const workerId = `${process.env.JEST_WORKER_ID ?? ""}`;
   const client = new MongoClient(process.env.MNA_TDB_MONGODB_URI?.replace("{{JEST_WORKER_ID}}", workerId) ?? "");
   try {
+    if (process.env.CI) {
+      return;
+    }
     await client.connect();
-    await client.db().dropDatabase();
+    const dbs = await client.db().admin().listDatabases();
+    await Promise.all(
+      dbs.databases.map((db) => {
+        if (db.name.startsWith(`TDB-test-${workerId}`)) {
+          return client.db(db.name).dropDatabase();
+        }
+
+        return;
+      })
+    );
+  } catch (e) {
+    console.error(e);
   } finally {
     await client.close();
   }
