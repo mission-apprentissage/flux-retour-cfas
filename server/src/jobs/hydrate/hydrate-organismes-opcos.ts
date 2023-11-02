306b9369df640b6cf8eeea6515e7e0cb5fc36302
Maxime Dréau <totakoko@users.noreply.github.com>
2023-11-02 11:01:07 +0100

    feat: ajout OPCO EP (#3333)

--- server/src/jobs/hydrate/hydrate-organismes-opcos.ts
+++ server/src/jobs/hydrate/hydrate-organismes-opcos.ts
@@ -6,7 +6,9 @@ import { __dirname } from "@/common/utils/esmUtils";
 import { readJsonFromCsvFile } from "@/common/utils/fileUtils";
 import { getStaticFilePath } from "@/common/utils/getStaticFilePath";
 
-const PATH_TO_OPCO2I_CSV = "opcos/2i.csv";
+// le nom doit correspondre à la clé de l'opco et au nom du fichier CSV
+// dans le dossier server/static/opcos
+const OPCOS = ["2i", "ep"];
 
 const jobLogger = parentLogger.child({
   module: "job:hydrate:opcos",
@@ -14,101 +16,103 @@ const jobLogger = parentLogger.child({
 
 /**
  * Remplit le champ organismes.opcos.
- * Pour l'instant, uniquement 2i.
+ * Pour l'instant, uniquement 2i et ep.
  */
 export const hydrateOrganismesOPCOs = async () => {
-  const logger = jobLogger.child({ opco: "2i" });
+  for (const opco of OPCOS) {
+    const logger = jobLogger.child({ opco });
 
-  const organismes2i = await organismesDb()
-    .find(
-      { opcos: "2i" },
-      {
-        projection: {
-          uai: 1,
-          siret: 1,
-          opcos: 1,
-        },
-      }
-    )
-    .toArray();
-  logger.info({ count: organismes2i.length }, "organismes existants");
-
-  const codes_rncp = (readJsonFromCsvFile(getStaticFilePath(PATH_TO_OPCO2I_CSV), ";") as { code_rncp: string }[]).map(
-    (row) => row.code_rncp
-  );
-  logger.info({ count: codes_rncp.length }, "rncp chargés");
-
-  const formationsRNCP = await formationsCatalogueDb()
-    .find(
-      {
-        rncp_code: {
-          $in: codes_rncp,
-        },
-      },
-      {
-        projection: {
-          etablissement_gestionnaire_siret: 1,
-          etablissement_gestionnaire_uai: 1,
-          etablissement_formateur_siret: 1,
-          etablissement_formateur_uai: 1,
-        },
-      }
-    )
-    .toArray();
-  logger.info({ count: formationsRNCP.length }, "formations correspondantes"); // ~6k
-
-  const organismesRNCPMap = formationsRNCP.reduce((acc, formation) => {
-    acc.set(`${formation.etablissement_gestionnaire_siret}-${formation.etablissement_gestionnaire_uai ?? null}`, {
-      siret: formation.etablissement_gestionnaire_siret,
-      uai: formation.etablissement_gestionnaire_uai ?? null,
-    });
-    acc.set(`${formation.etablissement_formateur_siret}-${formation.etablissement_formateur_uai ?? null}`, {
-      siret: formation.etablissement_formateur_siret,
-      uai: formation.etablissement_formateur_uai ?? null,
-    });
-    return acc;
-  }, new Map<string, { siret: string; uai: string | null }>());
-
-  logger.info({ count: organismesRNCPMap.size }, "organismes correspondants"); // ~1.6k
-
-  await PromisePool.for([...organismesRNCPMap.values()])
-    .handleError((err) => {
-      throw err;
-    })
-    .process(async (organisme) => {
-      await organismesDb().updateOne(
+    const organismes = await organismesDb()
+      .find(
+        { opcos: opco },
         {
-          siret: organisme.siret,
-          uai: organisme.uai as any,
-        },
-        {
-          $addToSet: {
-            opcos: "2i",
+          projection: {
+            uai: 1,
+            siret: 1,
+            opcos: 1,
           },
         }
-      );
-    });
+      )
+      .toArray();
+    logger.info({ count: organismes.length }, "organismes existants");
 
-  const ancienOrganismes2i = organismes2i.filter(
-    (organisme) => !organismesRNCPMap.has(`${organisme.siret}-${organisme.uai ?? null}`)
-  );
-  logger.info({ count: ancienOrganismes2i.length }, "nettoyage anciens organismes");
+    const codes_rncp = (
+      readJsonFromCsvFile(getStaticFilePath(`opcos/${opco}.csv`), ";") as { code_rncp: string }[]
+    ).map((row) => row.code_rncp);
+    logger.info({ count: codes_rncp.length }, "rncp chargés");
 
-  await PromisePool.for(ancienOrganismes2i)
-    .handleError((err) => {
-      throw err;
-    })
-    .process(async (organisme) => {
-      await organismesDb().updateOne(
+    const formationsRNCP = await formationsCatalogueDb()
+      .find(
         {
-          siret: organisme.siret,
-          uai: organisme.uai as any,
+          rncp_code: {
+            $in: codes_rncp,
+          },
         },
         {
-          $pull: {
-            opcos: "2i",
+          projection: {
+            etablissement_gestionnaire_siret: 1,
+            etablissement_gestionnaire_uai: 1,
+            etablissement_formateur_siret: 1,
+            etablissement_formateur_uai: 1,
           },
         }
-      );
-    });
+      )
+      .toArray();
+    logger.info({ count: formationsRNCP.length }, "formations correspondantes"); // ~6k
+
+    const organismesRNCPMap = formationsRNCP.reduce((acc, formation) => {
+      acc.set(`${formation.etablissement_gestionnaire_siret}-${formation.etablissement_gestionnaire_uai ?? null}`, {
+        siret: formation.etablissement_gestionnaire_siret,
+        uai: formation.etablissement_gestionnaire_uai ?? null,
+      });
+      acc.set(`${formation.etablissement_formateur_siret}-${formation.etablissement_formateur_uai ?? null}`, {
+        siret: formation.etablissement_formateur_siret,
+        uai: formation.etablissement_formateur_uai ?? null,
+      });
+      return acc;
+    }, new Map<string, { siret: string; uai: string | null }>());
+
+    logger.info({ count: organismesRNCPMap.size }, "organismes correspondants"); // ~1.6k
+
+    await PromisePool.for([...organismesRNCPMap.values()])
+      .handleError((err) => {
+        throw err;
+      })
+      .process(async (organisme) => {
+        await organismesDb().updateOne(
+          {
+            siret: organisme.siret,
+            uai: organisme.uai as any,
+          },
+          {
+            $addToSet: {
+              opcos: opco,
+            },
+          }
+        );
+      });
+
+    const ancienOrganismes = organismes.filter(
+      (organisme) => !organismesRNCPMap.has(`${organisme.siret}-${organisme.uai ?? null}`)
+    );
+    logger.info({ count: ancienOrganismes.length }, "nettoyage anciens organismes");
+
+    await PromisePool.for(ancienOrganismes)
+      .handleError((err) => {
+        throw err;
+      })
+      .process(async (organisme) => {
+        await organismesDb().updateOne(
+          {
+            siret: organisme.siret,
+            uai: organisme.uai as any,
+          },
+          {
+            $pull: {
+              opcos: opco,
+            },
+          }
+        );
+      });
+  }
 };
--- server/static/opcos/readme.md
+++ server/static/opcos/readme.md
@@ -1,3 +1,6 @@
 # Liste des fichiers OPCOs
 
 - `2i.csv` : provient du fichier rncp_opco_2i.xlsx transmis par l'OPCO avec la première colonne extraite à la main
+- `ep.csv` : provient du fichier RNCP_TDBAP_Vdef.xlsx transmis par l'OPCO avec la première colonne extraite à la main
+
+Les fichiers ont été transformés avec la commande `cat server/static/opcos/ep.csv | sort | uniq` pour éliminer les doublons
