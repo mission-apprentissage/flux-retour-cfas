c0d4d5fc00379947e1c4d4fcd18c994138a25dcf
Samir Benfares <sbenfares@gmail.com>
2023-11-23 15:28:03 +0100

    feat(server): ajout d'un job de correction de l'enseigne / raison sociale des OFA inconnus (#3386)
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
    Co-authored-by: Antoine Bigard <bigard.antoine@gmail.com>

---
+++ server/src/jobs/fiabilisation/ofa-inconnus/index.ts
@@ -0,0 +1,63 @@
+import { ObjectId } from "bson";
+
+import { findDataFromSiret } from "@/common/actions/infoSiret.actions";
+import { InfoSiret } from "@/common/actions/infoSiret.actions-struct";
+import logger from "@/common/logger";
+import { organismesDb } from "@/common/model/collections";
+
+/**
+ * Méthode de remplissage de la raison sociale et de l'enseigne pour les OFA Inconnus
+ * via l'API Entreprise
+ */
+export const hydrateRaisonSocialeEtEnseigneOFAInconnus = async () => {
+  const ofaInconnus = await organismesDb()
+    .find({ $or: [{ raison_sociale: { $exists: false } }, { enseigne: { $exists: false } }] })
+    .toArray();
+
+  logger.info(`${ofaInconnus.length} OFA inconnus à corriger via l'API Entreprise`);
+
+  let nbOfaUpdated = 0;
+
+  for (const { _id, siret } of ofaInconnus) {
+    if (await updateEnseigneRaisonSocialeOFAInconnu(_id, siret)) nbOfaUpdated++;
+  }
+
+  logger.info(`${nbOfaUpdated} OFA mis à jour via l'API Entreprise`);
+
+  return {
+    nbOfaUpdated,
+  };
+};
+
+/**
+ * Fonction de MAJ de la raison sociale / enseigne d'un OF depuis son siret
+ * Si on récupère depuis l'API Entreprise l'enseigne OU la raison sociale alors on MAJ l'organisme avec
+ * Si l'enseigne est null, on lui affecte la raison sociale
+ * Si la raison sociale est nulle, on lui affecte l'enseigne
+ */
+const updateEnseigneRaisonSocialeOFAInconnu = async (idOfa: ObjectId, siretOfa: string) => {
+  const dataSiret: InfoSiret = await findDataFromSiret(siretOfa);
+
+  if (dataSiret.messages.api_entreprise_status === "OK") {
+    try {
+      if (dataSiret.result.enseigne || dataSiret.result.raison_sociale) {
+        await organismesDb().findOneAndUpdate(
+          { _id: idOfa },
+          {
+            $set: {
+              enseigne: dataSiret.result.enseigne ?? dataSiret.result.raison_sociale,
+              raison_sociale: dataSiret.result.raison_sociale ?? dataSiret.result.enseigne,
+            },
+          }
+        );
+        return true;
+      }
+
+      return false;
+    } catch (err) {
+      logger.error(`updateEnseigneRaisonSocialeOFAInconnu > Erreur > ${err}`);
+    }
+  } else {
+    logger.error(`updateEnseigneRaisonSocialeOFAInconnu > Erreur API > ${dataSiret.messages.api_entreprise_info}`);
+  }
+};
