c77bb302344715b9f08df00b9c60a59218e9c2fd
Raphaël Huchet <raph@selego.co>
2023-11-27 12:20:06 +0100

    fix: ssr pour les mentions légales

--- ui/pages/mentions-legales.tsx
+++ ui/pages/mentions-legales.tsx
@@ -1,4 +1,7 @@
+import { Spinner } from "@chakra-ui/react";
+import { GetServerSidePropsContext } from "next";
 import { ExtendedRecordMap } from "notion-types";
+import { useEffect, useState } from "react";
 
 import { _get } from "@/common/httpClient";
 import { NotionDoc } from "@/components/NotionDoc";
@@ -6,15 +9,28 @@ import SimplePage from "@/components/Page/SimplePage";
 
 import "react-notion-x/src/styles.css";
 
-export async function getServerSideProps() {
-  const data = await _get("/mentions-legales");
-  return { props: { data } };
-}
+const isInitialServerSideProps = (context: GetServerSidePropsContext) =>
+  context.req?.url?.indexOf("/_next/data/") === -1;
 
-export default function Home({ data }: { data: ExtendedRecordMap }) {
-  return (
-    <SimplePage title="Mentions légales">
-      <NotionDoc recordMap={data} />
-    </SimplePage>
-  );
+export const getServerSideProps = async (context) => {
+  const isInitial = isInitialServerSideProps(context);
+  if (!isInitial) return { props: {} };
+  const dataSSR = await _get("/mentions-legales");
+  return { props: { dataSSR } };
+};
+
+export default function Home({ dataSSR }: { dataSSR: ExtendedRecordMap }) {
+  const [data, setData] = useState(dataSSR);
+  const [isLoading, setIsLoading] = useState(!dataSSR);
+
+  useEffect(() => {
+    if (dataSSR) return;
+    (async () => {
+      setIsLoading(true);
+      setData(await _get("/mentions-legales"));
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return <SimplePage title="Mentions légales">{isLoading ? <Spinner /> : <NotionDoc recordMap={data} />}</SimplePage>;
 }
