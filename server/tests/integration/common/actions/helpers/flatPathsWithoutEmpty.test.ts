7ed672db5cf8a78978bc101397beece2ed24748c
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2023-11-30 09:14:48 +0100

    fix: on ne lock plus les champs empty (+rétrocorrection) (#3398)

---
+++ server/tests/integration/common/actions/helpers/flatPathsWithoutEmpty.test.ts
@@ -0,0 +1,52 @@
+import { flatPathsWithoutEmpty } from "@/common/actions/effectifs.actions";
+
+describe("flatPathsWithoutEmpty()", () => {
+  it("should return an array of paths", () => {
+    const paths = flatPathsWithoutEmpty({
+      a: {
+        b: {
+          c: "c",
+          d: "d",
+        },
+        e: "e",
+      },
+      f: "f",
+    });
+    expect(paths).toHaveLength(4);
+    expect(paths).toEqual(["a.b.c", "a.b.d", "a.e", "f"]);
+  });
+  it("should not consider empty values", () => {
+    const paths = flatPathsWithoutEmpty({
+      a: {
+        b: {
+          c: "c",
+          d: "d",
+          h: "",
+        },
+        e: "e",
+        i: null,
+      },
+      f: "f",
+      g: undefined,
+    });
+    expect(paths).toHaveLength(4);
+    expect(paths).toEqual(["a.b.c", "a.b.d", "a.e", "f"]);
+  });
+  it("should keep zero values", () => {
+    const paths = flatPathsWithoutEmpty({
+      a: {
+        b: {
+          c: "c",
+          d: "d",
+          h: 0,
+        },
+        e: "e",
+        i: null,
+      },
+      f: "f",
+      g: undefined,
+    });
+    expect(paths).toHaveLength(5);
+    expect(paths).toEqual(["a.b.c", "a.b.d", "a.b.h", "a.e", "f"]);
+  });
+});
