87dd858f02508bda23903e0ba6f17729a17cfc5b
Raphaël Huchet <raph@selego.co>
2023-12-05 11:23:28 +0100

    feat: ajout du tutoriel video pour le téléversement

---
+++ ui/theme/components/icons/DownloadSimple.tsx
@@ -0,0 +1,15 @@
+import { Icon } from "@chakra-ui/react";
+import React from "react";
+
+export default function DownloadSimple(props) {
+  return (
+    <Icon width="12px" height="14px" viewBox="0 0 12 14" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 12.6665H12V13.9998H0V12.6665ZM6.66667 5.99984H11.3333L6 11.3332L0.666667 5.99984H5.33333V0.666504H6.66667V5.99984Z"
+        fill="currentColor"
+      />
+    </Icon>
+  );
+}
