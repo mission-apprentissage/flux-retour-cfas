88f387ee64a9a1838e635a058e1dacfa83cdfa8c
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2023-12-08 09:27:00 +0100

    fix: dernière situation (sifa) vérifiée lors de l'import (api+téléversement) (#3418)

---
+++ server/tests/integration/common/validation/zodPrimitives.test.ts
@@ -0,0 +1,17 @@
+import { primitivesV3 } from "@/common/validation/utils/zodPrimitives";
+
+describe("Regex primitivesV3", () => {
+  describe("derniere_situation", () => {
+    it("should validate", () => {
+      expect(primitivesV3.derniere_situation.safeParse(1009)).toStrictEqual({ success: true, data: 1009 });
+      expect(primitivesV3.derniere_situation.safeParse("1009")).toStrictEqual({ success: true, data: 1009 });
+    });
+    it("should not validate", () => {
+      expect(() => primitivesV3.derniere_situation.parse("word")).toThrow();
+      expect(() => primitivesV3.derniere_situation.parse("1234word")).toThrow();
+      expect(() => primitivesV3.derniere_situation.parse(1234)).toThrow();
+      expect(() => primitivesV3.derniere_situation.parse(99837273)).toThrow();
+      expect(() => primitivesV3.derniere_situation.parse(1)).toThrow();
+    });
+  });
+});
