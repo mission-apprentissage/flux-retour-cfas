e5f8cdda3a3090704d4cdeafb28e0c8e0dfeb2d1
Raphaël Huchet <raph@selego.co>
2023-12-11 16:58:29 +0100

    fix: mise à jour des organismes pour réparer l'encart admin et le paramétrage quand il est déjà fait

---
+++ server/src/db/migrations/20231211152119-mode_de_transmission.ts
@@ -0,0 +1,24 @@
+import { Db } from "mongodb";
+
+export const up = async (db: Db) => {
+  const currentDate = new Date(); // Récupère la date actuelle
+
+  await db.collection("organismes").updateMany(
+    {
+      api_key: { $ne: "" },
+      erps: { $exists: true, $not: { $size: 0 } },
+      mode_de_transmission: { $exists: false },
+      mode_de_transmission_configuration_date: { $exists: false },
+      mode_de_transmission_configuration_author_fullname: { $exists: false },
+      api_configuration_date: { $exists: false },
+    },
+    {
+      $set: {
+        mode_de_transmission: "API",
+        mode_de_transmission_configuration_date: currentDate,
+        mode_de_transmission_configuration_author_fullname: "rap2h",
+        api_configuration_date: currentDate,
+      },
+    }
+  );
+};
