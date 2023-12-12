9a8fcc4eeffe2baeebf0c492ec24a07b62062d11
Raphaël Huchet <rap2hpoutre@users.noreply.github.com>
2023-12-12 11:52:28 +0100

    fix: mise à jour des organismes pour réparer l'encart admin et le paramétrage quand il est déjà fait (#3421)

---
+++ server/src/db/migrations/20231211152119-mode_de_transmission.ts
@@ -0,0 +1,23 @@
+import { Db } from "mongodb";
+
+export const up = async (db: Db) => {
+  const currentDate = new Date(); // Récupère la date actuelle
+
+  await db.collection("organismes").updateMany(
+    {
+      api_key: { $exists: true },
+      erps: { $exists: true, $not: { $size: 0 } },
+      mode_de_transmission_configuration_date: { $exists: false },
+      mode_de_transmission_configuration_author_fullname: { $exists: false },
+      api_configuration_date: { $exists: false },
+    },
+    {
+      $set: {
+        mode_de_transmission: "API",
+        mode_de_transmission_configuration_date: currentDate,
+        mode_de_transmission_configuration_author_fullname: "migration 2023-12",
+        api_configuration_date: currentDate,
+      },
+    }
+  );
+};
