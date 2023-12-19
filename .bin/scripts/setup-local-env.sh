2fd28d85a78c025c181febeaba073b3631d6c257
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2023-12-19 11:56:25 +0100

    chore: fix setup script (#3438)

--- .bin/scripts/setup-local-env.sh
+++ .bin/scripts/setup-local-env.sh
@@ -6,6 +6,11 @@ ANSIBLE_CONFIG="${ROOT_DIR}/.infra/ansible/ansible.cfg" ansible all \
   --limit "local" \
   -m template \
   -a "src=\"${ROOT_DIR}/.infra/.env_server\" dest=\"${ROOT_DIR}/server/.env\"" \
+  --extra-vars "@${ROOT_DIR}/.infra/vault/vault.yml" \
+  --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh"
+ANSIBLE_CONFIG="${ROOT_DIR}/.infra/ansible/ansible.cfg" ansible all \
+  --limit "local" \
+  -m template \
   -a "src=\"${ROOT_DIR}/.infra/.env_ui\" dest=\"${ROOT_DIR}/ui/.env\"" \
   --extra-vars "@${ROOT_DIR}/.infra/vault/vault.yml" \
   --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh"
@@ -16,6 +21,7 @@ echo "NEXT_PUBLIC_ENV=local" >> "${ROOT_DIR}/ui/.env"
 echo "NEXT_PUBLIC_VERSION=0.0.0-local" >> "${ROOT_DIR}/ui/.env"
 echo "NEXT_PUBLIC_API_PORT=5001" >> "${ROOT_DIR}/ui/.env"
 
+yarn services:start
 yarn build:dev
 yarn cli migrations:up
 yarn cli indexes:create
