75a3a98b1170a0cb1108072c52f790a0395a5ec2
Paul Gaucher <paul@paulinformatique.com>
2024-01-25 17:02:19 +0100

    fix: mise à jour du tooltip UAI n-1

--- server/src/common/model/effectifs.model/parts/apprenant.part.ts
+++ server/src/common/model/effectifs.model/parts/apprenant.part.ts
@@ -130,8 +130,9 @@ export const apprenantSchema = object(
       description: "Situation de l'apprenant N-1",
     }),
     dernier_organisme_uai: string({
-      description: `Numéro UAI de l’établissement fréquenté l’année dernière (N-1), si déjà en apprentissage, mettre l’UAI du site de formation 
-        ou département.   \nPour les apprentis en emploi l'année dernière, le numéro UAI n-1 à indiquer est le "995" qui signifie "Non concerné"`,
+      description: `Numéro UAI de l’établissement fréquenté l’année dernière (N-1), si déjà en apprentissage, mettre l’UAI du site de formation
+        ou département.   \n* Pour les apprentis en emploi l'année dernière, le numéro UAI n-1 à indiquer est le **995** qui signifie "Non concerné".
+           \n* Si cette information n'est pas connue, le numéro UAI n-1 à indiquer est le **993** qui signifie "Inconnu" `,
       pattern: DERNIER_ORGANISME_UAI_PATTERN,
     }),
     type_cfa: string({
