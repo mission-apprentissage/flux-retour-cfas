ba19cdf6cbedd263656578d16e969b2c799a3a7e
Nicolas KREMER <nico.kremer@gmail.com>
2024-03-08 17:42:33 +0100

    feat: ajout de l attribut lang dans la balise html (#3591)

--- ui/pages/_document.tsx
+++ ui/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="fr">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
