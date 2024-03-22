abfa35e5997192901a75dbbac8000bad7f707e98
Paul G <ohpaul.g@gmail.com>
2024-03-22 16:11:03 +0100

    fix: api de création de l'effectif (#3613)
    Co-authored-by: Paul Gaucher <paul@paulinformatique.com>

---
+++ server/src/common/validation/effectifsCreationSchema.ts
@@ -0,0 +1,21 @@
+import { zApprenant } from "shared/models/data/effectifs/apprenant.part";
+import { zContrat } from "shared/models/data/effectifs/contrat.part";
+import { zFormationEffectif } from "shared/models/data/effectifs/formation.part";
+import { z } from "zod";
+
+import { primitivesV1, primitivesV3 } from "@/common/validation/utils/zodPrimitives";
+
+export const effectifCreationSchema = z.object({
+  annee_scolaire: primitivesV1.formation.annee_scolaire,
+  apprenant: zApprenant.omit({ historique_statut: true }),
+  contrats: z.array(zContrat),
+  formation: zFormationEffectif,
+  organisme: z.object({
+    organisme_responsable_id: z.string(),
+    organisme_formateur_id: z.string(),
+    organisme_lieu_id: z.string(),
+    type_cfa: primitivesV3.type_cfa,
+  }),
+});
+
+export type IEffectifCreationSchema = z.output<typeof effectifCreationSchema>;
---
+++ shared/utils/crypt.ts
@@ -0,0 +1,24 @@
+export const cyrb53Hash = (str: string, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed;
+  let h2 = 0x41c6ce57 ^ seed;
+  for (let i = 0, ch; i < str.length; i++) {
+    ch = str.charCodeAt(i);
+    h1 = Math.imul(h1 ^ ch, 2654435761);
+    h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return (h2 >>> 0).toString(16).padStart(8, "0") + (h1 >>> 0).toString(16).padStart(8, "0");
+};
+
+export const normalize = (str: string) => {
+  return str === null || str === undefined
+    ? ""
+    : str
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "");
+};
