133977b8907ad21abad13d38c46406547d9eb8ce
Nicolas KREMER <nico.kremer@gmail.com>
2024-04-03 14:22:38 +0200

    fix: add utils date

--- shared/utils/anneeScolaire.ts
+++ shared/utils/anneeScolaire.ts
@@ -36,8 +36,3 @@ export function getSIFADate(date: Date): Date {
     "UTC"
   );
 }
-
-export function addDaysUTC(date: Date, days: number) {
-  const result = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + days));
-  return result;
-}
