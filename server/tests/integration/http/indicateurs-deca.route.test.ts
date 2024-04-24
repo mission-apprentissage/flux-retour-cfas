cf6a0ef79491ed768c3184ac9cdebdb7187c2f65
Paul Gaucher <paul@paulinformatique.com>
2024-04-24 17:09:32 +0200

    fix: mise a jour des tests unitaires sur DECA

---
+++ server/tests/integration/http/indicateurs-deca.route.test.ts
@@ -0,0 +1,327 @@
+import { ObjectId } from "mongodb";
+import { IOrganisation, IOrganisme } from "shared/models";
+
+import { organismesDb, effectifsQueueDb, effectifsDECADb } from "@/common/model/collections";
+import { processEffectifsQueue } from "@/jobs/ingestion/process-ingestion";
+import { useMongo } from "@tests/jest/setupMongo";
+import { RequestAsOrganisationFunc, initTestApp } from "@tests/utils/testUtils";
+
+let app: Awaited<ReturnType<typeof initTestApp>>;
+let requestAsOrganisation: RequestAsOrganisationFunc;
+
+const idResp = new ObjectId();
+const idForm = new ObjectId();
+
+const organismeResponsable: IOrganisme = {
+  _id: idResp,
+  fiabilisation_statut: "FIABLE",
+  ferme: false,
+  created_at: new Date(),
+  updated_at: new Date(),
+  uai: "0000000A",
+  siret: "00000000000018",
+  nature: "responsable",
+  adresse: {
+    code_postal: "33400",
+    code_insee: "33522",
+    commune: "Talence",
+    departement: "33",
+    region: "75",
+    academie: "4",
+    complete: "680 CRS DE LA LIBERATION 33400 TALENCE",
+    bassinEmploi: "7505",
+  },
+  nom: "MON ORGANISME FORMATEUR",
+  organismesFormateurs: [
+    {
+      _id: idForm,
+    },
+  ],
+};
+
+const organismeFormateur: IOrganisme = {
+  _id: idForm,
+  fiabilisation_statut: "FIABLE",
+  ferme: false,
+  created_at: new Date(),
+  updated_at: new Date(),
+  uai: "0000000B",
+  siret: "00000000000019",
+  nature: "formateur",
+  adresse: {
+    code_postal: "33400",
+    code_insee: "33522",
+    commune: "Talence",
+    departement: "33",
+    region: "75",
+    academie: "4",
+    complete: "680 CRS DE LA LIBERATION 33400 TALENCE",
+    bassinEmploi: "7505",
+  },
+  nom: "MON ORGANISME RESPONSABLE",
+  organismesResponsables: [
+    {
+      _id: idResp,
+    },
+  ],
+};
+
+const organisation = {
+  _id: idResp,
+  type: "ORGANISME_FORMATION",
+  uai: organismeResponsable.uai,
+  siret: organismeResponsable.siret,
+  created_at: new Date(),
+};
+
+const createEff = (org) => ({
+  _id: new ObjectId(),
+  nom_apprenant: new ObjectId().toString(),
+  prenom_apprenant: new ObjectId().toString(),
+  date_de_naissance_apprenant: "1993-05-19T00:00:00.000Z",
+  annee_scolaire: `2023-2024`,
+  statut_apprenant: 0,
+  date_metier_mise_a_jour_statut: `2023-12-28T04:05:47.647Z`,
+  id_erp_apprenant: new ObjectId().toString(),
+  api_version: "v3",
+  source: "SOURCE_TEST",
+  source_organisme_id: "9999999",
+  date_inscription_formation: `2023-09-01T00:00:00.000Z`,
+  date_entree_formation: `2023-09-01T00:00:00.000Z`,
+  date_fin_formation: `2024-06-30T00:00:00.000Z`,
+
+  etablissement_responsable_uai: org.uai,
+  etablissement_responsable_siret: org.siret,
+  etablissement_formateur_uai: org.uai,
+  etablissement_formateur_siret: org.siret,
+  etablissement_lieu_de_formation_uai: org.uai,
+  etablissement_lieu_de_formation_siret: org.siret,
+  created_at: new Date(),
+});
+
+const createEffDECA = (org) => ({
+  _id: new ObjectId(),
+  apprenant: {
+    historique_statut: [
+      {
+        valeur_statut: 0,
+        date_statut: new Date("2023-12-28T04:05:47.647Z"),
+        date_reception: new Date("2024-04-24T09:23:11.020Z"),
+      },
+    ],
+    nom: new ObjectId().toString(),
+    prenom: new ObjectId().toString(),
+    date_de_naissance: new Date("1993-05-19T00:00:00.000Z"),
+    adresse: {},
+  },
+  contrats: [],
+  formation: {
+    periode: [],
+    date_inscription: new Date("2023-09-01T00:00:00.000Z"),
+    date_fin: new Date("2024-06-30T00:00:00.000Z"),
+    date_entree: new Date("2023-09-01T00:00:00.000Z"),
+  },
+  is_lock: {
+    apprenant: {
+      nom: true,
+      prenom: true,
+      date_de_naissance: true,
+      historique_statut: true,
+    },
+    formation: {
+      cfd: true,
+      periode: true,
+    },
+    contrats: true,
+  },
+  validation_errors: [],
+  _computed: {
+    organisme: {
+      region: org.adresse.region,
+      departement: org.adresse.departement,
+      academie: org.adresse.academie,
+      bassinEmploi: org.adresse.bassinEmploi,
+      uai: org.uai,
+      siret: org.siret,
+      fiable: true,
+    },
+    statut: {
+      en_cours: "ABANDON",
+      historique: [
+        {
+          mois: "09",
+          annee: "2023",
+          valeur: "INSCRIT",
+        },
+        {
+          mois: "10",
+          annee: "2023",
+          valeur: "INSCRIT",
+        },
+        {
+          mois: "11",
+          annee: "2023",
+          valeur: "ABANDON",
+        },
+        {
+          mois: "12",
+          annee: "2023",
+          valeur: "ABANDON",
+        },
+        {
+          mois: "01",
+          annee: "2024",
+          valeur: "ABANDON",
+        },
+        {
+          mois: "02",
+          annee: "2024",
+          valeur: "ABANDON",
+        },
+        {
+          mois: "03",
+          annee: "2024",
+          valeur: "ABANDON",
+        },
+        {
+          mois: "04",
+          annee: "2024",
+          valeur: "ABANDON",
+        },
+      ],
+      parcours: [
+        {
+          valeur: "INSCRIT",
+          date: new Date("2023-09-01T00:00:00.000Z"),
+        },
+        {
+          valeur: "ABANDON",
+          date: new Date("2023-11-30T00:00:00.000Z"),
+        },
+      ],
+    },
+  },
+  updated_at: new Date("2024-04-24T09:23:11.040Z"),
+  created_at: new Date("2024-04-24T09:23:11.021Z"),
+  annee_scolaire: "2023-2024",
+  source: "SOURCE_TEST",
+  source_organisme_id: "9999999",
+  id_erp_apprenant: new ObjectId().toString(),
+  organisme_id: org._id,
+  organisme_formateur_id: org._id,
+  organisme_responsable_id: org._id,
+  isDecaCompatible: true,
+});
+
+const testDeca = async (nbEffR, nbEffF, nbEffR_DECA, nbEffF_DECA, expected_nbEffR, expected_nbEffF, fromDECA) => {
+  const effR = [...new Array(nbEffR)].map(() => createEff(organismeResponsable));
+  const effF = [...new Array(nbEffF)].map(() => createEff(organismeFormateur));
+
+  const effRDeca: any[] = [...new Array(nbEffR_DECA)].map(() => createEffDECA(organismeResponsable));
+  const effFDeca: any[] = [...new Array(nbEffF_DECA)].map(() => createEffDECA(organismeFormateur));
+
+  effR.length && (await effectifsQueueDb().insertMany(effR));
+  effF.length && (await effectifsQueueDb().insertMany(effF));
+  effRDeca.length && (await effectifsDECADb().insertMany(effRDeca));
+  effFDeca.length && (await effectifsDECADb().insertMany(effFDeca));
+
+  await processEffectifsQueue();
+  const responseResp = await requestAsOrganisation(
+    organisation as IOrganisation,
+    "get",
+    `/api/v1/organismes/${idResp.toString()}/indicateurs/effectifs?date=2024-04-24T15:04:33.359Z`
+  );
+
+  const responseForm = await requestAsOrganisation(
+    organisation as IOrganisation,
+    "get",
+    `/api/v1/organismes/${idForm.toString()}/indicateurs/effectifs?date=2024-04-24T15:04:33.359Z`
+  );
+  console.log(responseResp.data);
+  console.log(responseForm.data);
+  fromDECA
+    ? expect((await effectifsDECADb().find({ isDecaCompatible: true }).toArray()).length).toBeGreaterThan(0)
+    : expect((await effectifsDECADb().find({ isDecaCompatible: true }).toArray()).length).toBe(0);
+  expect(responseResp.data.abandons).toBe(expected_nbEffR);
+  expect(responseForm.data.abandons).toBe(expected_nbEffF);
+};
+
+describe("Test des indicateurs avec des données DECA", () => {
+  useMongo();
+  beforeEach(async () => {
+    app = await initTestApp();
+    requestAsOrganisation = app.requestAsOrganisation;
+  });
+
+  beforeEach(async () => {
+    await organismesDb().insertMany([organismeResponsable, organismeFormateur]);
+  });
+
+  it("Indicateur avec DECA - 1", async () => {
+    await testDeca(1, 0, 0, 0, 1, 0, false);
+  });
+
+  it("Indicateur avec DECA - 2", async () => {
+    await testDeca(0, 0, 1, 0, 1, 0, true);
+  });
+
+  it("Indicateur avec DECA - 3", async () => {
+    await testDeca(0, 0, 0, 1, 1, 1, true);
+  });
+
+  it("Indicateur avec DECA - 4", async () => {
+    await testDeca(0, 1, 0, 0, 1, 1, false);
+  });
+
+  it("Indicateur avec DECA - 5", async () => {
+    await testDeca(0, 1, 0, 1, 1, 1, false);
+  });
+
+  it("Indicateur avec DECA - 6", async () => {
+    await testDeca(0, 0, 1, 0, 1, 0, true);
+  });
+
+  it("Indicateur avec DECA - 7", async () => {
+    await testDeca(0, 0, 1, 1, 2, 1, true);
+  });
+
+  it("Indicateurs avec DECA - 8", async () => {
+    await testDeca(0, 1, 1, 0, 1, 1, false);
+  });
+
+  it("Indicateurs avec DECA - 9", async () => {
+    await testDeca(0, 1, 1, 1, 1, 1, false);
+  });
+
+  it("Indicateurs avec DECA - 10", async () => {
+    await testDeca(1, 0, 0, 0, 1, 0, false);
+  });
+
+  it("Indicateurs avec DECA - 11", async () => {
+    await testDeca(1, 0, 0, 1, 1, 0, false);
+  });
+
+  it("Indicateurs avec DECA - 12", async () => {
+    await testDeca(1, 1, 0, 0, 2, 1, false);
+  });
+
+  it("Indicateurs avec DECA - 13", async () => {
+    await testDeca(1, 1, 0, 1, 2, 1, false);
+  });
+
+  it("Indicateurs avec DECA - 14", async () => {
+    await testDeca(1, 0, 1, 0, 1, 0, false);
+  });
+
+  it("Indicateurs avec DECA - 15", async () => {
+    await testDeca(1, 0, 1, 1, 1, 0, false);
+  });
+
+  it("Indicateurs avec DECA - 16", async () => {
+    await testDeca(1, 1, 1, 0, 2, 1, false);
+  });
+
+  it("Indicateurs avec DECA - 17", async () => {
+    await testDeca(1, 1, 1, 1, 2, 1, false);
+  });
+});
