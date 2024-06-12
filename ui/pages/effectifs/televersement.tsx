bf3662127775e95ead1b98df28405d181fe67e88
Nicolas KREMER <nico.kremer@gmail.com>
2024-06-12 12:09:29 +0200

    feat: creation du conposant de page non autorisee

--- ui/pages/effectifs/televersement.tsx
+++ ui/pages/effectifs/televersement.tsx
@@ -1,6 +1,4 @@
-import { Container, Text } from "@chakra-ui/react";
-
-import SimplePage from "@/components/Page/SimplePage";
+import UnauthorizedPage from "@/components/Page/UnauthorizedPage";
 import withAuth from "@/components/withAuth";
 import { useEffectifsOrganismeOrganisation } from "@/modules/mon-espace/effectifs/useEffectifsOrganisme";
 import Televersement from "@/modules/organismes/Televersement";
@@ -8,13 +6,7 @@ import Televersement from "@/modules/organismes/Televersement";
 function TeleversementPage() {
   const { organisme } = useEffectifsOrganismeOrganisation();
   if (!organisme) {
-    return (
-      <SimplePage>
-        <Container maxW="xl" p="8">
-          <Text mb={16}>Vous ne disposez pas des droits nécessaires pour visualiser cette page.</Text>
-        </Container>
-      </SimplePage>
-    );
+    return <UnauthorizedPage />;
   }
 
   return <Televersement organismeId={organisme._id} isMine={true} />;
