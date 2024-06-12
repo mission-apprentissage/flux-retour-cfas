dae9245b23f765aca3482bad9759fade597b2efd
Nicolas KREMER <nico.kremer@gmail.com>
2024-06-12 11:12:11 +0200

    fix: redirection page sut le sortie d imposture

--- ui/pages/effectifs/televersement.tsx
+++ ui/pages/effectifs/televersement.tsx
@@ -1,10 +1,22 @@
+import { Container, Text } from "@chakra-ui/react";
+
+import SimplePage from "@/components/Page/SimplePage";
 import withAuth from "@/components/withAuth";
 import { useEffectifsOrganismeOrganisation } from "@/modules/mon-espace/effectifs/useEffectifsOrganisme";
 import Televersement from "@/modules/organismes/Televersement";
 
 function TeleversementPage() {
   const { organisme } = useEffectifsOrganismeOrganisation();
-  if (!organisme) return null;
+  if (!organisme) {
+    return (
+      <SimplePage>
+        <Container maxW="xl" p="8">
+          <Text mb={16}>Vous ne disposez pas des droits nécessaires pour visualiser cette page.</Text>
+        </Container>
+      </SimplePage>
+    );
+  }
+
   return <Televersement organismeId={organisme._id} isMine={true} />;
 }
 
