ca381bc9604f71d90043a3e656414df97b6f3edb
Nicolas KREMER <nico.kremer@gmail.com>
2024-08-02 17:56:26 +0200

    feat: ajout opco afdas rncp

--- server/src/jobs/hydrate/hydrate-organismes-opcos.ts
+++ server/src/jobs/hydrate/hydrate-organismes-opcos.ts
@@ -9,7 +9,7 @@ import { getStaticFilePath } from "@/common/utils/getStaticFilePath";
 
 // le nom doit correspondre à la clé de l'opco et au nom du fichier CSV
 // dans le dossier server/static/opcos
-export const OPCOS = ["2i", "ep", "akto", "atlas", "mobilite", "uniformation", "ocapiat"];
+export const OPCOS = ["2i", "ep", "akto", "atlas", "mobilite", "uniformation", "ocapiat", "afdas"];
 
 const jobLogger = parentLogger.child({
   module: "job:hydrate:opcos",
