3707cd6557e365093da2c86ecfc3ef44515913cc
Paul Gaucher <paul@paulinformatique.com>
2024-08-07 14:57:02 +0200

    feat: ajout index sur effectifv2

--- shared/models/data/v2/effectif.v2.model.ts
+++ shared/models/data/v2/effectif.v2.model.ts
@@ -2,7 +2,9 @@ import type { CreateIndexesOptions, IndexSpecification } from "mongodb";
 import { z } from "zod";
 import { zObjectId } from "zod-mongodb-schema";
 
-const indexes: [IndexSpecification, CreateIndexesOptions][] = [];
+const indexes: [IndexSpecification, CreateIndexesOptions][] = [
+  [{ formation_id: 1, "_computed.nom": 1, "_computed.prenom": 1, "_computed.date_de_naissance": 1 }, {}],
+];
 
 const collectionName = "effectifV2";
 
