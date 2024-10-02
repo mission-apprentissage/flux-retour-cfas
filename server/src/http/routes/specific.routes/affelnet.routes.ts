9b0bfb78158af8e91dddd269ffa5314aa77a69c1
Paul Gaucher <paul@paulinformatique.com>
2024-10-02 17:13:07 +0200

    fix: ajout des champs additionels

--- server/src/http/routes/specific.routes/affelnet.routes.ts
+++ server/src/http/routes/specific.routes/affelnet.routes.ts
@@ -21,6 +21,13 @@ const AFFELNET_FIELDS = [
   { label: "Prenom 1", value: "prenom_1" },
   { label: "Prenom 2", value: "prenom_2" },
   { label: "Prenom 3", value: "prenom_3" },
+  { label: "Adresse 1", value: "adresse_1" },
+  { label: "Adresse 2", value: "adresse_2" },
+  { label: "Adresse 3", value: "adresse_3" },
+  { label: "Adresse 4", value: "adresse_4" },
+  { label: "Code Postal", value: "code_postal" },
+  { label: "Ville", value: "ville" },
+  { label: "Pays", value: "pays" },
   { label: "Mail Responsable 1", value: "mail_responsable_1" },
   { label: "Mail Responsable 2", value: "mail_responsable_2" },
   { label: "Telephone Responsable 1", value: "telephone_responsable_1" },
@@ -30,7 +37,24 @@ const AFFELNET_FIELDS = [
   { label: "Libelle Etab Origine", value: "libelle_etab_origine" },
   { label: "Nombre Voeux", value: "nombre_voeux" },
   { label: "Formation(s) demandée(s)", value: "formations_demandees" },
+  { label: "Uai Etab Formateur", value: "uai_etablissement_formateur" },
+  { label: "Uai Etab Responsable", value: "uai_etablissement_responsable" },
+  { label: "Uai Cio Etab Accueil", value: "uai_cio_etablissement_accueil" },
+  { label: "Type Etab Accueil", value: "type_etablissement_accueil" },
+  { label: "Libelle Public Etab Accueil", value: "libelle_pulic_etablissement_accueil" },
+  { label: "Contrat signé", value: "contrat_signe" },
 ];
+
+const computeFields = (data) => {
+  const maxContrats = Math.max(...data.map((d) => (d.contrats ? d.contrats.length : 0)));
+  const extraFields: Array<{ label: string; value: string }> = [];
+  for (let i = 0; i < maxContrats; i++) {
+    extraFields.push({ label: `Date début contrat ${i + 1}`, value: `date_debut_contrat_${i + 1}` });
+    extraFields.push({ label: `Date fin contrat ${i + 1}`, value: `date_fin_contrat_${i + 1}` });
+  }
+
+  return [...AFFELNET_FIELDS, ...extraFields];
+};
 
 const getRegionAndDepartementFromOrganisation = (
   orga: IOrganisationOperateurPublicRegion | IOrganisationOperateurPublicAcademie,
@@ -127,13 +151,15 @@ const exportNonConcretisee = async (req) => {
     ...voeu,
     formations_demandees: formations_demandees.join(", "),
     contrat_signe: contrats && contrats.length ? "Oui" : "Non",
-    ...contrats.reduce((acc, curr, index) => {
-      return {
-        ...acc,
-        [`date_debut_contrat_${index + 1}`]: format(new Date(curr.date_debut_contrat), "dd/MM/yyyy"),
-        [`date_fin_contrat_${index + 1}`]: format(new Date(curr.date_fin_contrat), "dd/MM/yyyy"),
-      };
-    }, {}),
+    ...(contrats
+      ? contrats.reduce((acc, curr, index) => {
+          return {
+            ...acc,
+            [`date_debut_contrat_${index + 1}`]: format(new Date(curr.date_debut_contrat), "dd/MM/yyyy"),
+            [`date_fin_contrat_${index + 1}`]: format(new Date(curr.date_fin_contrat), "dd/MM/yyyy"),
+          };
+        }, {})
+      : []),
   }));
 
   const ids = listVoeux.map((voeu) => voeu._id);
@@ -146,7 +172,7 @@ const exportNonConcretisee = async (req) => {
     new ObjectId(orga._id)
   );
 
-  const json2csvParser = new Parser({ fields: AFFELNET_FIELDS, delimiter: ";", withBOM: true });
+  const json2csvParser = new Parser({ fields: computeFields(listVoeux), delimiter: ";", withBOM: true });
   const csv = await json2csvParser.parse(transformedVoeux);
 
   return csv;
@@ -167,8 +193,8 @@ const exportConcretisee = async (req) => {
     ...contrats.reduce((acc, curr, index) => {
       return {
         ...acc,
-        [`date_debut_contrat_${index + 1}`]: format(new Date(curr.date_debut_contrat), "dd/MM/yyyy"),
-        [`date_fin_contrat_${index + 1}`]: format(new Date(curr.date_fin_contrat), "dd/MM/yyyy"),
+        [`date_debut_contrat_${index + 1}`]: format(new Date(curr.date_debut), "dd/MM/yyyy"),
+        [`date_fin_contrat_${index + 1}`]: format(new Date(curr.date_fin), "dd/MM/yyyy"),
       };
     }, {}),
   }));
@@ -183,7 +209,7 @@ const exportConcretisee = async (req) => {
     new ObjectId(orga._id)
   );
 
-  const json2csvParser = new Parser({ fields: AFFELNET_FIELDS, delimiter: ";", withBOM: true });
+  const json2csvParser = new Parser({ fields: computeFields(listVoeux), delimiter: ";", withBOM: true });
   const csv = await json2csvParser.parse(transformedVoeux);
 
   return csv;
