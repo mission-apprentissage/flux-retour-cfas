29c7c083cc703ea12991d0230a19235c0e40a162
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2024-10-24 11:17:00 +0200

    fix: regression de l'UI suite au MAJ

--- ui/modules/dashboard/DashboardOrganisme.tsx
+++ ui/modules/dashboard/DashboardOrganisme.tsx
@@ -15,7 +15,6 @@ import {
   Link,
   Collapse,
 } from "@chakra-ui/react";
-import { PieCustomLayerProps, ResponsivePie } from "@nivo/pie";
 import { useQuery } from "@tanstack/react-query";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
@@ -71,6 +70,7 @@ import InfoTransmissionDonnees from "../organismes/InfoTransmissionDonnees";
 import ContactsModal from "./ContactsModal";
 import { FileDownloadIcon } from "./icons";
 import IndicateursGrid from "./IndicateursGrid";
+import { Pie } from "./Pie";
 
 const FiabilisationInfo = () => {
   const [show, setShow] = useState(false);
@@ -846,17 +846,7 @@ const DashboardOrganisme = ({ organisme, modePublique }: Props) => {
                     </VStack>
 
                     <Box flex="1" minH="250px">
-                      <ResponsivePie
-                        margin={{ top: 32, right: 32, bottom: 32, left: 32 }}
-                        data={indicateursOrganismesPieData}
-                        innerRadius={0.6}
-                        cornerRadius={3}
-                        activeOuterRadiusOffset={8}
-                        enableArcLinkLabels={false}
-                        colors={{ datum: "data.color" }}
-                        enableArcLabels={false}
-                        layers={["arcs", CenteredMetric]}
-                      />
+                      <Pie data={indicateursOrganismesPieData} />
                     </Box>
                   </Flex>
                 </Box>
@@ -1016,32 +1006,6 @@ const DashboardOrganisme = ({ organisme, modePublique }: Props) => {
     </Box>
   );
 };
-
-function CenteredMetric({ dataWithArc, centerX, centerY }: PieCustomLayerProps<any>) {
-  const total = dataWithArc.reduce((acc, datum) => acc + datum.value, 0);
-  return (
-    <>
-      <text
-        x={centerX}
-        y={centerY - 10}
-        textAnchor="middle"
-        dominantBaseline="central"
-        style={{ fill: "#3A3A3A", fontSize: "28px", fontWeight: "bold" }}
-      >
-        {`${total}`}
-      </text>
-      <text
-        x={centerX}
-        y={centerY + 15}
-        textAnchor="middle"
-        dominantBaseline="central"
-        style={{ fill: "#666666", fontSize: "14px" }}
-      >
-        OFA
-      </text>
-    </>
-  );
-}
 
 export default withAuth(DashboardOrganisme);
 
