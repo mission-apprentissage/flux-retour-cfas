7959369adf8c7ceec7438aaa4de48f3ec7492807
Paul Gaucher <paul@paulinformatique.com>
2024-12-10 16:27:15 +0100

    fix: replacxe drop with deleteMany

--- server/src/db/migrations/20241120163553-hydrate:contrats-deca-raw.ts
+++ server/src/db/migrations/20241120163553-hydrate:contrats-deca-raw.ts
@@ -3,7 +3,7 @@ import { addJob } from "job-processor";
 import { effectifsDECADb } from "@/common/model/collections";
 
 export const up = async () => {
-  await effectifsDECADb().drop();
+  await effectifsDECADb().deleteMany({});
   await addJob({ name: "indexes:create", queued: true });
   await addJob({ name: "hydrate:contrats-deca-raw", queued: true });
 };
