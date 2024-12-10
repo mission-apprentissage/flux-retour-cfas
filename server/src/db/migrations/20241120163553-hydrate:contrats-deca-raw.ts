0d1bacf0f8ec3478bea19079fc649e0dfe7becf0
Paul Gaucher <paul@paulinformatique.com>
2024-12-10 16:19:33 +0100

    fix: add index after deca drop

--- server/src/db/migrations/20241120163553-hydrate:contrats-deca-raw.ts
+++ server/src/db/migrations/20241120163553-hydrate:contrats-deca-raw.ts
@@ -4,5 +4,6 @@ import { effectifsDECADb } from "@/common/model/collections";
 
 export const up = async () => {
   await effectifsDECADb().drop();
+  await addJob({ name: "indexes:create", queued: true });
   await addJob({ name: "hydrate:contrats-deca-raw", queued: true });
 };
