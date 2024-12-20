7d3b8f4eee00cdcece585c3ff9fe8e699058d8a6
Nicolas KREMER <nico.kremer@gmail.com>
2024-12-20 12:44:53 +0100

    fix: mise a jour row skeleton

--- ui/components/skeletons/RowsSkeleton.tsx
+++ ui/components/skeletons/RowsSkeleton.tsx
@@ -1,34 +1,35 @@
 import { Skeleton, Td, Tr } from "@chakra-ui/react";
-import { memo } from "react";
+import React from "react";
 
-interface Props {
+const RowsSkeleton = ({
+  nbRows = 5,
+  nbColumns = 5,
+  height = "1rem",
+}: {
   nbRows?: number;
   nbColumns?: number;
   height?: string;
-}
+}) => {
+  const rows = Array.from({ length: nbRows }, (_, i) => i);
+  const columns = Array.from({ length: nbColumns }, (_, j) => j);
 
-const Cell = memo(({ height }: { height: string }) => (
-  <Td>
-    <Skeleton width="100%" height={height} startColor="grey.300" endColor="galt" />
-  </Td>
-));
-Cell.displayName = "Cell";
+  return (
+    <>
+      {rows.map((i) => {
+        return (
+          <Tr textAlign="left" key={i}>
+            {columns.map((j) => {
+              return (
+                <Td key={j}>
+                  <Skeleton width="100%" height={height} startColor="grey.300" endColor="galt" />
+                </Td>
+              );
+            })}
+          </Tr>
+        );
+      })}
+    </>
+  );
+};
 
-const Row = memo(({ cols, height }: { cols: number; height: string }) => (
-  <Tr>
-    {Array.from({ length: cols }, (_, i) => (
-      <Cell key={i} height={height} />
-    ))}
-  </Tr>
-));
-Row.displayName = "Row";
-
-const RowsSkeleton = ({ nbRows = 5, nbColumns = 5, height = "1rem" }: Props) => (
-  <>
-    {Array.from({ length: nbRows }, (_, i) => (
-      <Row key={i} cols={nbColumns} height={height} />
-    ))}
-  </>
-);
-
-export default memo(RowsSkeleton);
+export default RowsSkeleton;
