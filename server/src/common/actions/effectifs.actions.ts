8b9754b15ab012d9d057200f3c0d3bcf467ab689
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-01-29 17:47:23 +0100

    fix: ajout de la visibilité de la source de la donneées pour les ML (#4027)

--- server/src/common/actions/effectifs.actions.ts
+++ server/src/common/actions/effectifs.actions.ts
@@ -535,6 +535,7 @@ export const buildEffectifForMissionLocale = (
     annee_scolaire: effectif.annee_scolaire,
     source: effectif.source,
     a_risque: effectif.a_risque,
+    transmitted_at: effectif.transmitted_at,
   };
   return result as IEffecifMissionLocale;
 };
