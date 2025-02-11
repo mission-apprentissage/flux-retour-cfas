c9aaa2d644037b982ab3832112acf77c37d0eba3
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-02-11 11:32:57 +0100

    fix: changement du type des reseaux dans computed

--- shared/models/data/effectifs.model.ts
+++ shared/models/data/effectifs.model.ts
@@ -104,7 +104,7 @@ export const zEffectifComputedOrganisme = z.object({
   region: zAdresse.shape.region.nullish(),
   departement: zAdresse.shape.departement.nullish(),
   academie: zAdresse.shape.academie.nullish(),
-  reseaux: z.array(zodEnumFromObjKeys(TETE_DE_RESEAUX_BY_ID)).describe("Réseaux du CFA, s'ils existent").nullish(),
+  reseaux: z.array(z.string()).describe("Réseaux du CFA, s'ils existent").nullish(),
   bassinEmploi: z.string({}).nullish(),
 
   // 2 champs utiles seulement pour les indicateurs v1
