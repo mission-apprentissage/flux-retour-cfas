51bbce9223ba159f4b85d9f94385fecfddc8db19
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-02-12 09:26:44 +0100

    feat: affichache la donnée de contact ML pour un CFA

--- ui/modules/dashboard/DashboardOrganisme.tsx
+++ ui/modules/dashboard/DashboardOrganisme.tsx
@@ -1,5 +1,13 @@
-import { ArrowForwardIcon, ViewIcon, ChevronUpIcon, ChevronDownIcon, WarningTwoIcon } from "@chakra-ui/icons";
 import {
+  ArrowForwardIcon,
+  ViewIcon,
+  ChevronUpIcon,
+  ChevronDownIcon,
+  WarningTwoIcon,
+  CheckCircleIcon,
+} from "@chakra-ui/icons";
+import {
+  Badge,
   Box,
   Button,
   Container,
@@ -14,6 +22,7 @@ import {
   Wrap,
   Link,
   Collapse,
+  Icon,
 } from "@chakra-ui/react";
 import { useQueries } from "@tanstack/react-query";
 import NextLink from "next/link";
@@ -45,6 +54,8 @@ import { exportDataAsXlsx } from "@/common/utils/exportUtils";
 import { formatCivility, formatSiretSplitted } from "@/common/utils/stringUtils";
 import DownloadButton from "@/components/buttons/DownloadButton";
 import CerfaLink from "@/components/Cerfa/CerfaLink";
+import CustomLink from "@/components/Links/Link";
+import { BasicModal } from "@/components/Modals/BasicModal";
 import NotificationTransmissionError from "@/components/Notifications/TransmissionErrors";
 import Ribbons from "@/components/Ribbons/Ribbons";
 import SuggestFeature from "@/components/SuggestFeature/SuggestFeature";
@@ -53,7 +64,9 @@ import { InfoTooltip } from "@/components/Tooltip/InfoTooltip";
 import withAuth from "@/components/withAuth";
 import { useOrganisationOrganisme } from "@/hooks/organismes";
 import useAuth from "@/hooks/useAuth";
+import { InfoCircle } from "@/theme/components/icons";
 import { DashboardWelcome } from "@/theme/components/icons/DashboardWelcome";
+import Eye from "@/theme/components/icons/Eye";
 
 import InfosTransmissionEtParametrageOFA from "../admin/InfosTransmissionEtParametrageOFA";
 import { ExternalLinks } from "../admin/OrganismeDetail";
@@ -288,6 +301,23 @@ const DashboardOrganisme = ({ organisme, modePublique }: Props) => {
     organisme.permissions?.indicateursEffectifs && getIndicateursEffectifsPartielsMessage(auth, organisme);
 
   const isFiable = organisme.fiabilisation_statut === STATUT_FIABILISATION_ORGANISME.FIABLE;
+
+  const missionLocaleMock = {
+    id: 349,
+    nom: "DU FOREZ",
+    siret: "44456065000012",
+    localisation: {
+      geopoint: {},
+      adresse: "Maison des Permanences",
+      cp: "42600",
+      ville: "MONTBRISON",
+    },
+    contact: {
+      email: "contact@mlforez.fr",
+      telephone: "04 77 58 42 03",
+      siteWeb: "https://www.mlforez.fr",
+    },
+  };
 
   return (
     <Box>
@@ -632,6 +662,90 @@ const DashboardOrganisme = ({ organisme, modePublique }: Props) => {
                     )}
                   </>
                 )}
+
+                <HStack alignItems="flex-start">
+                  <Text whiteSpace="nowrap">Mission Locale près de chez vous&nbsp;:</Text>
+                  <Flex gap={2}>
+                    <Text fontWeight="bold">ML {missionLocaleMock.nom} </Text>
+                    <BasicModal
+                      renderTrigger={(onOpen) => (
+                        <Button
+                          variant="link"
+                          fontSize="md"
+                          borderBottom="1px"
+                          borderRadius="0"
+                          p="0"
+                          width="fit-content"
+                          onClick={onOpen}
+                        >
+                          <Eye mr={1} />
+                          Voir le contact
+                        </Button>
+                      )}
+                      title={`Mission Locale ${missionLocaleMock.nom}`}
+                      size="4xl"
+                    >
+                      <Flex gap={3}>
+                        <InfoCircle color="plaininfo" w={4} h={4} mt={1} />
+                        <Text color="plaininfo" size="zeta">
+                          Les Missions Locales peuvent avoir accès au Tableau de bord de l’apprentissage et aux données
+                          nominatives des jeunes sans contrat, en rupture de contrat ou en abandon. Dans le cadre de
+                          leur mission d’accompagnement des jeunes, elles peuvent être amenées à contacter votre CFA.
+                        </Text>
+                      </Flex>
+                      <Flex direction="column" gap={4}>
+                        <Flex
+                          direction="column"
+                          flexGrow={1}
+                          borderLeft="4px solid"
+                          borderColor="bluefrance"
+                          pl={6}
+                          ml={6}
+                          my={3}
+                        >
+                          <Text fontSize="lg">
+                            {missionLocaleMock.localisation.adresse}, {missionLocaleMock.localisation.cp}{" "}
+                            {missionLocaleMock.localisation.ville}
+                          </Text>
+                          <Text>{missionLocaleMock.contact.telephone}</Text>
+                          <Text>{missionLocaleMock.contact.email}</Text>
+                          <CustomLink href={missionLocaleMock.contact.siteWeb} isExternal isUnderlined>
+                            {missionLocaleMock.contact.siteWeb}
+                          </CustomLink>
+                        </Flex>
+                        <Text>
+                          Les contacts ci-dessous ont créé un compte sur le Tableau de bord de l’apprentissage :
+                        </Text>
+
+                        <UnorderedList spacing={3} my={3}>
+                          <ListItem key={1}>
+                            <Flex align="center" gap={2}>
+                              <Text fontWeight="bold">Alexandre Martin</Text>
+                              <Badge variant="purple" borderRadius="full" px={3} py={1} fontSize="sm">
+                                <Flex align="center" gap={1}>
+                                  <Icon as={CheckCircleIcon} mr={1} />
+                                  Compte créé le 30/01/2025
+                                </Flex>
+                              </Badge>
+                            </Flex>
+                            <Text>Conseiller entreprises</Text>
+                            <Text>alexandra.martin@ml-arcachon.fr</Text>
+                            <Text>05 57 42 98 12</Text>
+                          </ListItem>
+                        </UnorderedList>
+                        <CustomLink
+                          href="https://www.unml.info/le-reseau/annuaire/?type=&nom=&region=&affichage=liste&hp_v=&hp_r=1"
+                          isUnderlined
+                          isExternal
+                          color="blueFrance"
+                          w="fit-content"
+                        >
+                          Annuaire complet des Missions Locales
+                        </CustomLink>
+                      </Flex>
+                    </BasicModal>
+                  </Flex>
+                </HStack>
 
                 {organisme.organismesResponsables && organisme.organismesResponsables.length > 0 && (
                   <HStack alignItems="flex-start">
