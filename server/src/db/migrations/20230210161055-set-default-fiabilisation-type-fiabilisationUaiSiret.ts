5c3ee547682c0b2a53e68b978ea591354a072416
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-02-18 11:14:32 +0100

    chore: suppression des migrations anterieures à Novembre 2024 (#4070)

--- server/src/db/migrations/20230210161055-set-default-fiabilisation-type-fiabilisationUaiSiret.ts
+++
@@ -1,9 +0,0 @@
-import { Db, MongoClient } from "mongodb";
-
-export const up = async (db: Db, _client: MongoClient) => {
-  const collection = db.collection("fiabilisationUaiSiret");
-  await collection.updateMany(
-    { siret: { $exists: true }, type: { $exists: false } },
-    { $set: { type: "A_FIABILISER" } }
-  );
-};
--- server/src/db/migrations/20230321103036-update-organismes-fiabilise-fiable.ts
+++
@@ -1,9 +0,0 @@
-import { Db, MongoClient } from "mongodb";
-
-export const up = async (db: Db, _client: MongoClient) => {
-  await db
-    .collection("organismes")
-    .updateMany({ fiabilisation_statut: "FIABILISE" }, { $set: { fiabilisation_statut: "FIABLE" } });
-};
-
-export const down = async () => {};
