23480bf7062e962dda02cbfbbe9654dd7fa6fbfb
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-02-20 17:03:08 +0100

    feat: ajout des nom des regions dans le fichier excel des listes d'organismes des reseaux

--- ui/common/exports.ts
+++ ui/common/exports.ts
@@ -1,4 +1,4 @@
-import { IOrganisation, IUsersMigration } from "shared";
+import { DEPARTEMENTS, IOrganisation, IUsersMigration } from "shared";
 
 import { Organisme } from "./internal/Organisme";
 import { ExportColumn } from "./utils/exportUtils";
@@ -242,7 +242,7 @@ export function convertOrganismeToExport(
     enseigne: organisme.enseigne ?? "",
     nature: organisme.nature,
     departement: organisme.adresse?.departement ?? "",
-    region: organisme.adresse?.region ?? "",
+    region: DEPARTEMENTS.find((dept) => dept.region.code === String(organisme.adresse?.region))?.region.nom ?? "",
     commune: organisme.adresse?.commune ?? "",
     adresse: organisme.adresse?.complete ?? "",
     last_transmission_date: organisme.last_transmission_date ?? "",
