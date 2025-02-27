18ad626ed0261cfb103f7794642b90983ee98bc5
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-02-27 17:11:00 +0100

    fix: mettre à jour que les documents avec effectifs

--- server/src/db/migrations/20250227154518-telechargement-liste-nom-logs-move-effectifs-to-element-list.ts
+++ server/src/db/migrations/20250227154518-telechargement-liste-nom-logs-move-effectifs-to-element-list.ts
@@ -1,7 +1,7 @@
 import { Db } from "mongodb";
 
 export const up = async (db: Db) => {
-  await db.collection("telechargementListeNomLogs").updateMany({}, [
+  await db.collection("telechargementListeNomLogs").updateMany({ elementList: { $exists: false } }, [
     {
       $set: {
         elementList: {
