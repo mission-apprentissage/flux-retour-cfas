eb9d580b12207c1bb3c667a32c66c35d07f2947e
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-03-03 17:54:40 +0100

    feat: implementation du model d'ingestion person v2

--- shared/models/data/v2/person.v2.model.ts
+++ shared/models/data/v2/person.v2.model.ts
@@ -2,12 +2,19 @@ import type { CreateIndexesOptions, IndexSpecification } from "mongodb";
 import { z } from "zod";
 import { zObjectId } from "zod-mongodb-schema";
 
-const indexes: [IndexSpecification, CreateIndexesOptions][] = [];
+const indexes: [IndexSpecification, CreateIndexesOptions][] = [
+  [{ "identifiant.nom": 1, "identifiant.prenom": 1, "identifiant.date_de_naissance": 1 }, { unique: true }],
+];
 
 const collectionName = "personV2";
 
 const zPersonV2 = z.object({
   _id: zObjectId,
+  identifiant: z.object({
+    nom: z.string(),
+    prenom: z.string(),
+    date_de_naissance: z.date(),
+  }),
 });
 
 export type IPersonV2 = z.output<typeof zPersonV2>;
