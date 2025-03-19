9d0577ebc6671af06bf62f107af2e4131b1a254c
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-03-19 17:59:53 +0100

    feat: ajout not found page

--- ui/app/mission-locale/error.tsx
+++ ui/app/mission-locale/error.tsx
@@ -2,73 +2,17 @@
 
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import { Box, Container, Typography } from "@mui/material";
-import * as Sentry from "@sentry/nextjs";
-import { useEffect } from "react";
+import { notFound } from "next/navigation";
 
-import { publicConfig } from "@/config.public";
-import { NotFound } from "@/icons/NotFound";
-
-import { DsfrLink } from "../_components/link/DsfrLink";
-
-function getErrorDescription(error: unknown): string | null {
-  if (!error) {
-    return null;
-  }
-
-  if (publicConfig.env === "local") {
-    if (error instanceof Error) {
-      return error.message;
-    }
-
-    if (typeof error === "string") {
-      return error;
-    }
-  }
-
-  return null;
-}
-
-export default function ErrorComponent({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-    console.error(error);
-  }, [error]);
-
-  const details = getErrorDescription(error);
-
+export default function ErrorComponent() {
   return (
     <Container maxWidth="xl">
-      <Box>
-        <Box
-          padding={8}
-          display="flex"
-          justifyContent="center"
-          flexDirection="column"
-          margin="auto"
-          maxWidth="600px"
-          textAlign="center"
-        >
-          <NotFound />
-
-          <Box mt={4}>
-            <Typography variant="h1" gutterBottom>
-              Une erreur est survenue
-            </Typography>
-            {details && <Typography gutterBottom>{details}</Typography>}
-
-            <Box mt={2}>
-              <Button onClick={() => reset()} type="button">
-                Essayer à nouveau
-              </Button>
-            </Box>
-
-            <Box mt={2}>
-              <DsfrLink href="/" locale="fr">
-                Retourner à la page d&apos;accueil
-              </DsfrLink>
-            </Box>
-          </Box>
-        </Box>
+      <Box textAlign="center" padding={8}>
+        <Typography variant="h1" gutterBottom>
+          Une erreur est survenue
+        </Typography>
+        <Typography gutterBottom>Impossible de traiter votre demande pour le moment.</Typography>
+        <Button onClick={() => notFound()}>Retour</Button>
       </Box>
     </Container>
   );
--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -30,13 +30,12 @@ const contentSecurityPolicy = `
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  outputFileTracingRoot: path.join(path.dirname(fileURLToPath(import.meta.url)), "../"),
   transpilePackages: ["shared"],
   poweredByHeader: false,
   productionBrowserSourceMaps: true,
   experimental: {
-    outputFileTracingRoot: path.join(path.dirname(fileURLToPath(import.meta.url)), "../"),
     typedRoutes: true,
-    instrumentationHook: true,
   },
   output: "standalone",
   sentry: {
@@ -47,6 +46,10 @@ const nextConfig = {
       test: /\.woff2$/,
       type: "asset/resource",
     });
+    // Bson is using top-level await, which is not supported by default in Next.js in client side
+    // Probably related to https://github.com/vercel/next.js/issues/54282
+    config.resolve.alias.bson = path.join(path.dirname(fileURLToPath(import.meta.resolve("bson"))), "bson.cjs");
+
     config.resolve.extensionAlias = {
       ".js": [".ts", ".tsx", ".js", ".jsx"],
       ".mjs": [".mts", ".mjs"],
@@ -54,6 +57,8 @@ const nextConfig = {
     };
     return config;
   },
+
+  // Headers
   async headers() {
     return [
       {
@@ -67,6 +72,8 @@ const nextConfig = {
       },
     ];
   },
+
+  // Redirects
   async redirects() {
     return [
       {
@@ -74,11 +81,6 @@ const nextConfig = {
         destination: "/reinscription",
         permanent: true,
       },
-      // {
-      //   source: "/mission-locale",
-      //   destination: "/",
-      //   permanent: true,
-      // },
       {
         source: "/politique-confidentialite",
         destination: "/politique-de-confidentialite",
