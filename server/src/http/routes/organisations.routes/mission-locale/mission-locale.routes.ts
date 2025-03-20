ad21f21cc7e1e7f8ef83f1e1d50c0b9f80be4699
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-03-20 13:21:59 +0100

    fix: correction sur l'api de mise a jour d'update de l'effectif ml

--- server/src/http/routes/organisations.routes/mission-locale/mission-locale.routes.ts
+++ server/src/http/routes/organisations.routes/mission-locale/mission-locale.routes.ts
@@ -3,11 +3,10 @@ import { ObjectId } from "bson";
 import express from "express";
 import {
   API_TRAITEMENT_TYPE,
-  IEffectif,
+  IMissionLocaleEffectif,
   IOrganisationMissionLocale,
   updateMissionLocaleEffectifApi,
 } from "shared/models";
-import { IEffectifDECA } from "shared/models/data/effectifsDECA.model";
 import { effectifsParMoisFiltersMissionLocaleSchema } from "shared/models/routes/mission-locale/missionLocale.api";
 
 import {
@@ -17,7 +16,7 @@ import {
   setEffectifMissionLocaleData,
 } from "@/common/actions/mission-locale/mission-locale.actions";
 import { createTelechargementListeNomLog } from "@/common/actions/telechargementListeNomLogs.actions";
-import { effectifsDb, effectifsDECADb } from "@/common/model/collections";
+import { missionLocaleEffectifsDb } from "@/common/model/collections";
 import { getAgeFromDate } from "@/common/utils/miscUtils";
 import { validateFullZodObjectSchema } from "@/common/utils/validationUtils";
 import { addSheetToXlscFile } from "@/common/utils/xlsxUtils";
@@ -37,16 +36,14 @@ const updateEffectifMissionLocaleData = async ({ body, params }, { locals }) => 
   const missionLocale = locals.missionLocale as IOrganisationMissionLocale;
   const data = await validateFullZodObjectSchema(body, updateMissionLocaleEffectifApi);
 
-  let effectif: IEffectif | IEffectifDECA | null = await effectifsDb().findOne({ _id: new ObjectId(effectifId) });
-
-  if (!effectif) {
-    effectif = await effectifsDECADb().findOne({ _id: new ObjectId(effectifId) });
-  }
+  const effectif: IMissionLocaleEffectif | null = await missionLocaleEffectifsDb().findOne({
+    "effectif_snapshot._id": new ObjectId(effectifId),
+  });
 
   if (!effectif) {
     throw Boom.notFound("Effectif introuvable");
   }
-  if (effectif.apprenant.adresse?.mission_locale_id?.toString() !== missionLocale.ml_id.toString()) {
+  if (effectif.mission_locale_id.toString() !== missionLocale._id.toString()) {
     throw Boom.forbidden("Accès non autorisé");
   }
   return await setEffectifMissionLocaleData(missionLocale._id, effectifId, data);
