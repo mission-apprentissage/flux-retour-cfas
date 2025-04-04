a884c39575f40854d629005bf6dc767224333a1d
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-04-04 10:14:32 +0200

    feat: mise en place accompagnement API et intégration design

--- ui/app/campagnes/mission-locale/layout.tsx
+++ ui/app/campagnes/mission-locale/layout.tsx
@@ -1,8 +1,10 @@
 import { fr } from "@codegouvfr/react-dsfr";
+import { Stack } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 
-import { Footer } from "../_components/Footer";
-import { HeaderCampagnes } from "../_components/HeaderCampagnes";
-import { Providers } from "../providers";
+import { Footer } from "@/app/_components/Footer";
+import { HeaderCampagnes } from "@/app/_components/HeaderCampagnes";
+import { Providers } from "@/app/providers";
 
 export default async function RootLayout({ children }: { children: JSX.Element }) {
   return (
@@ -18,7 +20,11 @@ export default async function RootLayout({ children }: { children: JSX.Element }
           }),
         }}
       >
-        {children}
+        <Grid container maxWidth="md" margin="auto" sx={{ padding: "0 16px" }}>
+          <Stack spacing={3} sx={{ width: "100%" }}>
+            {children}
+          </Stack>
+        </Grid>
       </div>
       <Footer />
     </Providers>
