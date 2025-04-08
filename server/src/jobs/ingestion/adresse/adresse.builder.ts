8fdda11ee423d9f41b4062e9e94f4b4858276a7d
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-04-08 16:17:38 +0200

    fix: rollback v2 mission locale code for now

--- server/src/jobs/ingestion/adresse/adresse.builder.ts
+++ server/src/jobs/ingestion/adresse/adresse.builder.ts
@@ -32,6 +32,5 @@ export async function buildAdresse(dossier: IBuildAdresseParams): Promise<IEffec
     code_departement: communeInfo.departement.codeInsee,
     code_region: communeInfo.region.codeInsee,
     mission_locale_id: communeInfo.mission_locale?.id ?? null,
-    mission_locale_code: communeInfo.mission_locale?.code ?? null,
   };
 }
