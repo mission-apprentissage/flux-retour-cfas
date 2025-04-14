06fdfd4530b7602f373ed899f57dc2075d9a637f
Paul G. <paul.gaucher@beta.gouv.fr>
2025-04-14 16:41:40 +0200

    fix: correction du mécanisme d'imposture sur les ml (#4178)

--- server/src/http/routes/admin.routes/mission-locale.routes.ts
+++ server/src/http/routes/admin.routes/mission-locale.routes.ts
@@ -1,12 +1,19 @@
+import Boom from "boom";
 import express from "express";
 import { z } from "zod";
 
-import { activateMissionLocale } from "@/common/actions/admin/mission-locale/mission-locale.admin.actions";
+import {
+  activateMissionLocale,
+  getAllMlFromOrganisations,
+} from "@/common/actions/admin/mission-locale/mission-locale.admin.actions";
+import { getMissionsLocales } from "@/common/apis/apiAlternance/apiAlternance";
 import { returnResult } from "@/http/middlewares/helpers";
 import validateRequestMiddleware from "@/http/middlewares/validateRequestMiddleware";
 
 export default () => {
   const router = express.Router();
+
+  router.get("/", returnResult(getAllMls));
 
   router.post(
     "/activate",
@@ -22,4 +29,16 @@ export default () => {
 const activateMLAtDate = ({ body }) => {
   const { date, missionLocaleId } = body;
   return activateMissionLocale(missionLocaleId, date);
+};
+
+const getAllMls = async () => {
+  const externalML = await getMissionsLocales();
+  if (!externalML) {
+    throw Boom.notFound("Aucune mission locale trouvée");
+  }
+  const organisationMl = await getAllMlFromOrganisations();
+
+  return organisationMl
+    .map((orga) => ({ organisation: orga, externalML: externalML.find((ml) => ml.id === orga.ml_id) }))
+    .filter((ml) => ml.externalML);
 };
