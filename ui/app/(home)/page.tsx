af10e338f1fe10ecd9f9dd112f44a39462c0e9eb
Nicolas KREMER <nicolas.kremer@beta.gouv.fr>
2025-05-27 14:34:18 +0200

    feat: ajout de la transcription de la video de la page d'accueil

--- ui/app/(home)/page.tsx
+++ ui/app/(home)/page.tsx
@@ -256,6 +256,7 @@ export default function Page() {
           >
             Ils parlent de nous !
           </Typography>
+
           <figure role="group" className="fr-content-media">
             <iframe
               title="Titre de l'iframe"
@@ -264,6 +265,135 @@ export default function Page() {
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
+            <figcaption className="fr-content-media__caption">
+              Témoignages des Missions Locales / Mission interministérielle pour l&apos;apprentissage
+              <a className="fr-link" href="https://www.youtube.com/watch?v=Al8aSdWTw94">
+                https://www.youtube.com/watch?v=Al8aSdWTw94
+              </a>
+            </figcaption>
+            <div className="fr-transcription" id="transcription-2160">
+              <button
+                className="fr-transcription__btn"
+                aria-expanded="false"
+                aria-controls="fr-transcription-collapse-transcription-2160"
+                data-fr-js-collapse-button="true"
+              >
+                Transcription
+              </button>
+              <div className="fr-collapse" id="fr-transcription-collapse-transcription-2160" data-fr-js-collapse="true">
+                <div className="fr-transcription__footer">
+                  <div className="fr-transcription__actions-group">
+                    <button
+                      className="fr-btn--fullscreen fr-btn"
+                      aria-controls="fr-transcription-modal-transcription-2160"
+                      aria-label="Agrandir la transcription"
+                      data-fr-opened="false"
+                      id="button-2163"
+                      data-fr-js-modal-button="true"
+                    >
+                      Agrandir
+                    </button>
+                  </div>
+                </div>
+                <div
+                  id="fr-transcription-modal-transcription-2160"
+                  className="fr-modal"
+                  aria-labelledby="fr-transcription-modal-transcription-2160-title"
+                  data-fr-js-modal="true"
+                >
+                  <div className="fr-container fr-container--fluid fr-container-md">
+                    <div className="fr-grid-row fr-grid-row--center">
+                      <div className="fr-col-12 fr-col-md-10 fr-col-lg-8">
+                        <div className="fr-modal__body" data-fr-js-modal-body="true">
+                          <div className="fr-modal__header">
+                            <button
+                              className="fr-btn--close fr-btn"
+                              aria-controls="fr-transcription-modal-transcription-2160"
+                              id="button-2164"
+                              title="Fermer"
+                              data-fr-js-modal-button="true"
+                            >
+                              Fermer
+                            </button>
+                          </div>
+                          <div className="fr-modal__content">
+                            <h1 id="fr-transcription-modal-transcription-2160-title" className="fr-modal__title">
+                              Témoignages des Missions Locales
+                            </h1>
+
+                            <div>
+                              <p>
+                                Avant le lancement de notre service numérique,Alexandra Delohen, Chargée de projet,
+                                relations réseau et partenaires à la Mission Locale Technowest Nouvelle-Aquitaine, et
+                                Carole HyrezaResponsable du pôle entreprises et anticipations des mutations économiques
+                                à la Mission LocaleInsertion Formation Emploi du Grand Amiénois nous ont partagé les
+                                difficultés rencontréespour repérer les jeunes en rupture de contrat d’apprentissage.
+                              </p>
+                              <p>
+                                Elle partage le même constat : notre service numérique permet principalement
+                                d’identifier des jeunes en ruptureque la Mission locale ne connaît pas encore.Il y a
+                                beaucoup de jeunes qu&apos;on connaissait,mais on n&apos;avait pas forcément eu
+                                l&apos;informationqui nous disait que ce jeune avait rompu,parce que pour certains, là
+                                le dernier c&apos;était novembreon est aujourd&apos;hui le 29 avril,donc voilà,
+                                c&apos;est cette information làet à peu près 75%sur cette liste de 215, ce sont des
+                                jeunesqui ne connaissaient pas la Mission Locale a priori,puisqu&apos;en fait il
+                                n&apos;était pas dans notre base de données.
+                              </p>
+                              <p>
+                                Mais on a également accès aux jeunes rupturantsqui ne connaissent pas forcément la
+                                Mission Localeou qui n&apos;ont jamais été inscrits à la Mission Locale.Et de ce fait,
+                                ça nous permet de prendre contact avec euxet de leur proposer un accompagnement Mission
+                                Locale pour les aiderdans leur recherche d&apos;un nouveau contrat.
+                              </p>
+                              <p>
+                                Au-delà de ce repérage,Thibaut Clery, Référent Formation et Alternanceà la Mission
+                                Locale du Beauvaisis,mentionne un autre avantage de notre service numérique,celui de
+                                multiplier les interactions avec les CFA.C&apos;était d&apos;abord de peut-être beaucoup
+                                plus interagir,en tout cas facilement avec un certain nombre de CFA.Ça démultiplie les
+                                échanges en tout cas avec les CFA pour dire :« Bah tiens, j&apos;ai ce jeune là qui
+                                apparaît,qu&apos;est-ce que tu en penses ? Où est-ce qu&apos;on en est ? »Lui allumer la
+                                lumière en disant :« N&apos;oublie pas de nous l&apos;orienter au cas où. »Ça permet de
+                                rappeler aussi les bonnes pratiquesqu&apos;on utilise avec un certain nombre de CFA.Donc
+                                ça, c&apos;est vraiment un plus pour nous.
+                              </p>
+                              <p>
+                                À la question :« Recommanderiez-vous notre service public numérique ? »la réponse est
+                                unanime : oui.Plébiscité pour sa facilité d’utilisation et son utilitédans
+                                l’accompagnement des jeunes,notre service a convaincu les Missions Locales qui l’ont
+                                expérimenté.Cette expérience positive les conduit aujourd’huià envisager un engagement à
+                                long termedans le déploiement du service.
+                              </p>
+                              <p>
+                                Je vais être directe avec vous,nous on souhaite continuer en tout cas
+                                l&apos;expérimentationet continuer la démarche avec vous.Même si au début, quand on a
+                                commencé l&apos;expérimentation,on s&apos;est retrouvé avec 150 jeunes à contacter,ça
+                                nous a un peu semblé beaucoup sur le moment.Mais finalement, on a trouvé une
+                                organisation, une articulationqui nous a permis de contacter tous les jeuneset puis
+                                d&apos;obtenir des rendez-vous.
+                              </p>
+                              <p>
+                                Je recommande vivementet j&apos;ai déjà recommandé à mon collèguequi n&apos;est pas très
+                                loin et qui est entré dans l&apos;expérimentation il y a 3 semaines.Donc oui je pense
+                                que c&apos;est un outil qu&apos;il faut généraliser certes,après il y a peut-être des
+                                Missions Localesqui n&apos;auront pas forcément le tempsou les moyens humains de pouvoir
+                                le gérer.Comme je disais, il faut y aller tous les 2-3 jours,ça se met à jour
+                                régulièrement doncpour être réactif au niveau des jeunesil faut quand même qu&apos;on
+                                ait l&apos;information assez tôt,dès sa rupture de contratil faut qu&apos;on aille le
+                                plus vite possible pour pouvoir le raccrocher.
+                              </p>
+                              <p>
+                                Vous êtes un CFA ou une Mission Locale ? Engageons une collaboration durable pour
+                                soutenir l’insertion des apprentis !
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </figure>
           <Grid container rowSpacing={4} sx={{ width: { xs: "100%", md: "66.6667%" }, mb: fr.spacing("12w") }}>
             {QUOTES.map((quote) => (
