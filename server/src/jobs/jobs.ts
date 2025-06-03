4c24912de4929bbc7b574467688f84d1ea9429d9
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-06-03 15:04:36 +0200

    feat: ajout d'informations sur le changement de statut d'un rupturant

--- server/src/jobs/jobs.ts
+++ server/src/jobs/jobs.ts
@@ -33,6 +33,7 @@ import {
   hydrateMissionLocaleAdresse,
   hydrateMissionLocaleOrganisation,
   hydrateMissionLocaleSnapshot,
+  updateMissionLocaleEffectifCurrentStatus,
   updateMissionLocaleSnapshotFromLastStatus,
 } from "./hydrate/mission-locale/hydrate-mission-locale";
 import { hydrateOpenApi } from "./hydrate/open-api/hydrate-open-api";
@@ -374,6 +375,11 @@ export async function setupJobProcessor() {
       },
       "tmp:migrate:effectifs": {
         handler: migrateEffectifs,
+      },
+      "tmp:migrate:mission-locale-current-status": {
+        handler: async () => {
+          return updateMissionLocaleEffectifCurrentStatus();
+        },
       },
     },
   });
