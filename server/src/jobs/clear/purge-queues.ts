ef72268a3cdb7c8e442643c6cf946307fabf15f3
Paul G. <paul.gaucher@beta.gouv.fr>
2025-06-19 11:51:37 +0200

    fix: mise a jour de la logique de rapport de transmission et de purge… (#4284)

--- server/src/jobs/clear/purge-queues.ts
+++ server/src/jobs/clear/purge-queues.ts
@@ -1,3 +1,5 @@
+import { captureException } from "@sentry/node";
+
 import logger from "@/common/logger";
 import { effectifsQueueDb } from "@/common/model/collections";
 
@@ -5,123 +7,65 @@ import { effectifsQueueDb } from "@/common/model/collections";
  * purge de la collection effectifsQueue
  */
 export const purgeQueues = async (NB_DAYS_TO_KEEP = 15) => {
-  const aggregationPipeline = [
-    {
-      $match: {
-        computed_day: {
-          $exists: true,
-          $ne: null,
-        },
-        organisme_id: {
-          $exists: true,
-          $ne: null,
-        },
-        processed_at: {
-          $exists: true,
-          $ne: null,
-        },
-      },
-    },
-    {
-      $group: {
-        _id: {
-          organisme_id: "$organisme_id",
-          computed_day: "$computed_day",
-        },
-        records: {
-          $push: "$$ROOT",
-        },
-      },
-    },
-    {
-      $group: {
-        _id: "$_id.organisme_id",
-        allDates: {
-          $push: {
-            date: "$_id.computed_day",
-            records: "$records",
-          },
-        },
-      },
-    },
-    {
-      $project: {
-        organisme_id: "$_id",
-        sortedDates: {
-          $sortArray: {
-            input: "$allDates",
-            sortBy: {
-              date: -1,
-            },
-          },
-        },
-      },
-    },
-    {
-      $project: {
-        organisme_id: 1,
-        datesToDelete: {
-          $cond: {
-            if: {
-              $gt: [
-                {
-                  $size: "$sortedDates",
-                },
-                NB_DAYS_TO_KEEP,
-              ],
-            },
-            then: {
-              $slice: [
-                "$sortedDates",
-                NB_DAYS_TO_KEEP,
-                {
-                  $subtract: [
-                    {
-                      $size: "$sortedDates",
-                    },
-                    NB_DAYS_TO_KEEP,
-                  ],
-                },
-              ],
-            },
-            else: [],
-          },
-        },
-      },
-    },
-    {
-      $match: {
-        "datesToDelete.0": {
-          $exists: true,
-        },
-      },
-    },
-    {
-      $unwind: "$datesToDelete",
-    },
-    {
-      $unwind: "$datesToDelete.records",
-    },
-    {
-      $replaceRoot: {
-        newRoot: "$datesToDelete.records",
-      },
-    },
-    {
-      $project: {
-        _id: 1,
-      },
-    },
-  ];
+  const organisme = await effectifsQueueDb().distinct("organisme_id");
 
-  const recordsToDelete = await effectifsQueueDb().aggregate(aggregationPipeline).toArray();
-  const idsToDelete = recordsToDelete.map((record) => record._id);
+  if (organisme.length === 0) {
+    logger.info("No organisme found in effectifsQueue, nothing to purge.");
+    return;
+  }
 
-  if (idsToDelete.length > 0) {
-    const deleteResult = await effectifsQueueDb().deleteMany({ _id: { $in: idsToDelete } });
-    logger.info(`Deleted ${deleteResult.deletedCount} records from effectifsQueue`);
-  } else {
-    logger.info("No records to delete from effectifsQueue.");
+  for (const org of organisme) {
+    try {
+      if (!org) {
+        continue;
+      }
+
+      const aggregation = [
+        {
+          $match: {
+            computed_day: {
+              $exists: true,
+              $ne: null,
+            },
+            organisme_id: org,
+            processed_at: {
+              $exists: true,
+              $ne: null,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$computed_day",
+          },
+        },
+        {
+          $sort: {
+            _id: -1,
+          },
+        },
+        {
+          $skip: NB_DAYS_TO_KEEP,
+        },
+      ];
+
+      const daysToDelete = await effectifsQueueDb().aggregate(aggregation).toArray();
+      const daysToDeleteFormatted = daysToDelete.map((day) => day._id);
+
+      if (daysToDeleteFormatted.length === 0) {
+        logger.info(`No days to delete for organisme_id: ${org}`);
+        continue;
+      }
+
+      const deleteResult = await effectifsQueueDb().deleteMany({
+        organisme_id: org,
+        computed_day: { $in: daysToDeleteFormatted },
+      });
+      logger.info(`Deleted ${deleteResult.deletedCount} records for organisme_id: ${org}`);
+    } catch (error) {
+      captureException(error);
+      continue;
+    }
   }
 
   logger.info("End Purging effectifsQueue");
