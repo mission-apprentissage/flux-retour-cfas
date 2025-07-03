18ad205fcc4640b663be319a66b0f35c094fc48b
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2025-07-03 17:23:00 +0200

    feat: extract ARMLContent

--- ui/app/(arml)/arml/ARMLClient.tsx
+++ ui/app/(arml)/arml/ARMLClient.tsx
@@ -3,12 +3,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { IOrganisationARML, IMissionLocaleWithStats } from "shared";
 
-import ARMLIndicateurGlobal from "@/app/_components/arml/ARMLIndicateurGlobal";
+import ARMLContent from "@/app/_components/arml/ARMLContent";
 import { TableSkeleton } from "@/app/_components/suspense/LoadingSkeletons";
 import { SuspenseWrapper } from "@/app/_components/suspense/SuspenseWrapper";
 import { _get } from "@/common/httpClient";
 
-function ARMLContent() {
+export default function ARMLClient() {
   const { data: armlData } = useQuery<{ arml: IOrganisationARML; mlList: Array<IMissionLocaleWithStats> }>(
     ["arml"],
     async () => {
@@ -20,27 +20,11 @@ function ARMLContent() {
     }
   );
 
-  return (
-    <div className="fr-grid-row fr-grid-row--gutters">
-      <div className="fr-col-12">
-        <h3 className="fr-h3" style={{ marginBottom: "1rem", color: "var(--text-title-blue-france)" }}>
-          ARML {armlData!.arml.nom}
-        </h3>
-      </div>
-      <div className="fr-col-12">
-        <h4 className="fr-h4" style={{ marginBottom: "1rem", color: "var(--text-title-blue-france)" }}>
-          Indicateurs Globaux
-        </h4>
-      </div>
-      <ARMLIndicateurGlobal armls={armlData!.mlList} />
-    </div>
-  );
-}
+  if (!armlData) return null;
 
-export default function ARMLClient() {
   return (
     <SuspenseWrapper fallback={<TableSkeleton />}>
-      <ARMLContent />
+      <ARMLContent armlData={armlData} />
     </SuspenseWrapper>
   );
 }
