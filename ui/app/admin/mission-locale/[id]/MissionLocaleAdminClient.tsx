88884a2a0d4326c0530ba4fcfa50b272e7862c4b
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-07-08 10:42:38 +0200

    fix: ajout du bouton de navigation pour l'edition de ficher ml en admin

--- ui/app/admin/mission-locale/[id]/MissionLocaleAdminClient.tsx
+++ ui/app/admin/mission-locale/[id]/MissionLocaleAdminClient.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Button from "@codegouvfr/react-dsfr/Button";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { use } from "react";
 
 import MissionLocaleDetailsContent from "@/app/_components/arml/MissionLocaleDetailsContent";
@@ -10,6 +12,7 @@ import { SuspenseWrapper } from "@/app/_components/suspense/SuspenseWrapper";
 import { _get } from "@/common/httpClient";
 
 export default function MissionLocaleDetailsClient({ params }: { params: Promise<{ id: string }> }) {
+  const router = useRouter();
   const { id } = use(params);
   const { data: missionLocaleData } = useQuery(
     ["mission-locale", id],
@@ -23,7 +26,21 @@ export default function MissionLocaleDetailsClient({ params }: { params: Promise
   if (!missionLocaleData) return null;
   return (
     <SuspenseWrapper fallback={<TableSkeleton />}>
-      <CustomBreadcrumb path={`/admin/mission-locale/[mlId]`} name={missionLocaleData.nom} />
+      <div className="fr-grid-row fr-grid-row--gutters">
+        <div className="fr-col-md-6 fr-col-6" style={{ marginBottom: "2rem" }}>
+          <CustomBreadcrumb path={`/admin/mission-locale/[mlId]`} name={missionLocaleData.nom} />
+        </div>
+        <div className="fr-col-md-6 fr-col-6" style={{ textAlign: "right", marginBottom: "2rem", paddingTop: "2rem" }}>
+          <Button
+            iconId="ri-arrow-right-line"
+            iconPosition="right"
+            onClick={() => router.push(`/admin/mission-locale/${id}/edit`)}
+          >
+            Édition de fiches
+          </Button>
+        </div>
+      </div>
+
       <MissionLocaleDetailsContent ml={missionLocaleData} />
     </SuspenseWrapper>
   );
