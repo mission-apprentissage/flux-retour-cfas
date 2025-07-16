dbe33883c551bec7e48c062aa6001441a79b5d9b
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-07-16 16:51:02 +0200

    fix: mise a jour de l'algo d'affectation voeux/effectif

--- server/src/jobs/hydrate/affelnet/hydrate-voeux-effectifs.ts
+++ server/src/jobs/hydrate/affelnet/hydrate-voeux-effectifs.ts
@@ -3,16 +3,14 @@ import { ObjectId } from "bson";
 import { getAcademieById } from "shared/constants";
 
 import logger from "@/common/logger";
-import { effectifsDb, effectifsDECADb, organismesDb, voeuxAffelnetDb } from "@/common/model/collections";
+import { effectifsDb, effectifsDECADb, voeuxAffelnetDb } from "@/common/model/collections";
 
 export const hydrateVoeuxEffectifsRelations = async () => {
-  const organismes = await organismesDb().find({}).toArray();
   let foundCount = 0;
-  const organismesMap = organismes.reduce((acc, orga) => {
-    return orga.uai ? { ...acc, [orga.uai]: orga } : acc;
-  }, {});
-  const cursor = voeuxAffelnetDb().find({ effectif_id: null });
-  const totalFound = await voeuxAffelnetDb().countDocuments({ effectif_id: null });
+
+  const cursor = voeuxAffelnetDb().find();
+  const totalFound = await voeuxAffelnetDb().countDocuments();
+
   while (await cursor.hasNext()) {
     const voeux = await cursor.next();
     const computedAnneeScolaire = [
@@ -20,16 +18,11 @@ export const hydrateVoeuxEffectifsRelations = async () => {
       `${voeux?.annee_scolaire_rentree}-${Number(voeux?.annee_scolaire_rentree) + 1}`,
     ];
 
-    if (
-      voeux?.raw.nom &&
-      voeux?.raw.prenom_1 &&
-      voeux?.raw.uai_etatblissement_formateur &&
-      organismesMap[voeux?.raw.uai_etatblissement_formateur]
-    ) {
+    if (voeux?.raw.nom && voeux?.raw.prenom_1 && voeux.organisme_formateur_id) {
       const filter = {
         "apprenant.nom": { $regex: `^${voeux?.raw.nom.toLowerCase()}$`, $options: "i" }, // case insensitive search
         "apprenant.prenom": { $regex: `^${voeux?.raw.prenom_1.toLowerCase()}$`, $options: "i" }, // quid des autres prenoms ?
-        organisme_id: new ObjectId(organismesMap[voeux?.raw.uai_etatblissement_formateur]._id),
+        organisme_id: voeux.organisme_formateur_id,
         annee_scolaire: {
           $in: computedAnneeScolaire,
         },
@@ -47,13 +40,11 @@ export const hydrateVoeuxEffectifsRelations = async () => {
 };
 
 export const hydrateVoeuxEffectifsDECARelations = async () => {
-  const organismes = await organismesDb().find({}).toArray();
   let foundCount = 0;
-  const organismesMap = organismes.reduce((acc, orga) => {
-    return orga.uai ? { ...acc, [orga.uai]: orga } : acc;
-  }, {});
-  const cursor = voeuxAffelnetDb().find({ effectif_deca_id: null });
-  const totalFound = await voeuxAffelnetDb().countDocuments({ effectif_deca_id: null });
+
+  const cursor = voeuxAffelnetDb().find();
+  const totalFound = await voeuxAffelnetDb().countDocuments();
+
   while (await cursor.hasNext()) {
     const voeux = await cursor.next();
     const computedAnneeScolaire = [
@@ -61,16 +52,11 @@ export const hydrateVoeuxEffectifsDECARelations = async () => {
       `${voeux?.annee_scolaire_rentree}-${Number(voeux?.annee_scolaire_rentree) + 1}`,
     ];
 
-    if (
-      voeux?.raw.nom &&
-      voeux?.raw.prenom_1 &&
-      voeux?.raw.uai_etatblissement_formateur &&
-      organismesMap[voeux?.raw.uai_etatblissement_formateur]
-    ) {
+    if (voeux?.raw.nom && voeux?.raw.prenom_1 && voeux.organisme_formateur_id) {
       const filter = {
         "apprenant.nom": { $regex: `^${voeux?.raw.nom.toLowerCase()}$`, $options: "i" }, // case insensitive search
         "apprenant.prenom": { $regex: `^${voeux?.raw.prenom_1.toLowerCase()}$`, $options: "i" }, // quid des autres prenoms ?
-        organisme_id: new ObjectId(organismesMap[voeux?.raw.uai_etatblissement_formateur]._id),
+        organisme_id: voeux.organisme_formateur_id,
         annee_scolaire: {
           $in: computedAnneeScolaire,
         },
