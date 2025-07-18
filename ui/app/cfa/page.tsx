a8ef189f1ebde67ec25002a26584c90ff3369111
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2025-07-18 15:10:37 +0200

    feat: add cfa page

--- ui/app/cfa/page.tsx
+++ ui/app/cfa/page.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from "next";
 
+import CfaClient from "./CfaClient";
+
 export const metadata: Metadata = {
   title: "CFA | Tableau de bord de l'apprentissage",
 };
 
 export default function CfaPage() {
-  return <></>;
+  return <CfaClient />;
 }
