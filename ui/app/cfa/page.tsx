74d420942aae91afa3a284cfc83a32320772ad7d
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2025-07-18 11:42:14 +0200

    feat: redirect cfa beta tester

---
+++ ui/app/cfa/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "CFA | Tableau de bord de l'apprentissage",
+};
+
+export default function CfaPage() {
+  return <></>;
+}
