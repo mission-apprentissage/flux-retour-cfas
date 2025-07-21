0e901f60b3ed13b6f23e38740e5107aa3838c97d
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-07-21 11:37:59 +0200

    fix: ajout de l'api de détails d'un effectif pour un cfa

--- server/src/http/routes/organismes.routes/mission-locale.routes.ts
+++ server/src/http/routes/organismes.routes/mission-locale.routes.ts
@@ -1,10 +1,14 @@
 import Boom from "boom";
 import { ObjectId } from "bson";
 import express from "express";
-import { IMissionLocaleEffectif } from "shared/models";
+import { API_EFFECTIF_LISTE, IMissionLocaleEffectif } from "shared/models";
 import { updateMissionLocaleEffectifOrganismeApi } from "shared/models/routes/organismes/mission-locale/missions-locale.api";
+import { z } from "zod";
 
-import { getAllEffectifsParMois } from "@/common/actions/mission-locale/mission-locale.actions";
+import {
+  getAllEffectifsParMois,
+  getEffectifFromMissionLocaleId,
+} from "@/common/actions/mission-locale/mission-locale.actions";
 import { getOrganisationOrganismeByOrganismeId } from "@/common/actions/organisations.actions";
 import { setEffectifMissionLocaleDataFromOrganisme } from "@/common/actions/organismes/mission-locale.actions";
 import { getOrganismeById } from "@/common/actions/organismes/organismes.actions";
@@ -16,6 +20,7 @@ export default () => {
   const router = express.Router();
 
   router.get("/effectifs-per-month", returnResult(getEffectifsParMoisMissionLocale));
+  router.get("/effectif/:id", returnResult(getEffectifMissionLocale));
   router.put("/effectif/:id", returnResult(updateEffectifMissionLocaleData));
 
   return router;
@@ -49,4 +54,24 @@ const updateEffectifMissionLocaleData = async ({ body, params }, { locals }) => 
     throw Boom.notFound("Effectif introuvable");
   }
   return await setEffectifMissionLocaleDataFromOrganisme(organisme._id, effectifId, data);
+};
+
+const getEffectifMissionLocale = async (req, { locals }) => {
+  const { nom_liste } = await validateFullZodObjectSchema(req.query, {
+    nom_liste: z.enum([
+      API_EFFECTIF_LISTE.PRIORITAIRE,
+      API_EFFECTIF_LISTE.INJOIGNABLE,
+      API_EFFECTIF_LISTE.A_TRAITER,
+      API_EFFECTIF_LISTE.TRAITE,
+    ]),
+  });
+
+  const effectifId = req.params.id;
+  const organisme = await getOrganisationOrganismeByOrganismeId(locals.organismeId);
+
+  if (!organisme) {
+    throw Boom.forbidden("No organisme found for the provided ID");
+  }
+
+  return await getEffectifFromMissionLocaleId(organisme, effectifId, nom_liste);
 };
