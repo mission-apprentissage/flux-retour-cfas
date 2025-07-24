f158411d6f43c1dc1d15c8199fa46ed484e63c2f
Paul G. <paul.gaucher@beta.gouv.fr>
2025-07-24 10:42:41 +0200

    fix: correctif sur le changement d'année à venir pour les ml (#4335)

--- shared/utils/anneeScolaire.test.ts
+++ shared/utils/anneeScolaire.test.ts
@@ -1,8 +1,99 @@
 import { it, expect, describe } from "vitest";
 
-import { getAnneeScolaireFromDate, getAnneesScolaireListFromDate, getSIFADate } from "./anneeScolaire";
+import {
+  getAnneeScolaireFromDate,
+  getAnneeScolaireListFromDateRange,
+  getAnneesScolaireListFromDate,
+  getSIFADate,
+} from "./anneeScolaire";
 
-describe("getAnneesScolaireListFromDate()", () => {
+describe("getAnneesScolaireListFromDateRange()", () => {
+  [
+    { start: "2020-09-01", end: "2020-09-30", expected: ["2020-2020", "2020-2021"] },
+    { start: "2025-01-01", end: "2025-07-31", expected: ["2024-2025", "2025-2025"] },
+    { start: "2025-01-01", end: "2025-08-02", expected: ["2024-2025", "2025-2025", "2025-2026"] },
+    {
+      start: "2020-01-10",
+      end: "2025-01-31",
+      expected: [
+        "2019-2020",
+        "2020-2020",
+        "2020-2021",
+        "2021-2021",
+        "2021-2022",
+        "2022-2022",
+        "2022-2023",
+        "2023-2023",
+        "2023-2024",
+        "2024-2024",
+        "2024-2025",
+        "2025-2025",
+      ],
+    },
+    {
+      start: "2020-01-10",
+      end: "2025-08-31",
+      expected: [
+        "2019-2020",
+        "2020-2020",
+        "2020-2021",
+        "2021-2021",
+        "2021-2022",
+        "2022-2022",
+        "2022-2023",
+        "2023-2023",
+        "2023-2024",
+        "2024-2024",
+        "2024-2025",
+        "2025-2025",
+        "2025-2026",
+      ],
+    },
+    {
+      start: "2020-08-10",
+      end: "2025-01-31",
+      expected: [
+        "2020-2020",
+        "2020-2021",
+        "2021-2021",
+        "2021-2022",
+        "2022-2022",
+        "2022-2023",
+        "2023-2023",
+        "2023-2024",
+        "2024-2024",
+        "2024-2025",
+        "2025-2025",
+      ],
+    },
+    {
+      start: "2020-08-10",
+      end: "2025-08-31",
+      expected: [
+        "2020-2020",
+        "2020-2021",
+        "2021-2021",
+        "2021-2022",
+        "2022-2022",
+        "2022-2023",
+        "2023-2023",
+        "2023-2024",
+        "2024-2024",
+        "2024-2025",
+        "2025-2025",
+        "2025-2026",
+      ],
+    },
+  ].forEach(({ start, end, expected }) => {
+    it(`returns [${expected.join(", ")}] for ${start} to ${end}`, () => {
+      expect(
+        getAnneeScolaireListFromDateRange(new Date(`${start}T00:00:00Z`), new Date(`${end}T00:00:00Z`))
+      ).toStrictEqual(expected);
+    });
+  });
+});
+
+describe("getAnneeScolaire()", () => {
   [
     { date: "2020-09-01", expected: ["2020-2020", "2020-2021"] },
     { date: "2021-01-10", expected: ["2021-2021", "2020-2021"] },
