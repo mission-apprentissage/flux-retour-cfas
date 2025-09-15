e324f5a69784ed16b492829182250ccc11cb1227
Paul Gaucher <paul.gaucher@beta.gouv.fr>
2025-09-15 17:02:05 +0200

    fix: suppression des n-uplet sur les organisations

---
+++ server/src/db/migrations/20250915145019-suppressions-des-n-uplet-des-organisation-organisme.ts
@@ -0,0 +1,18 @@
+import logger from "@/common/logger";
+import { organisationsDb, usersMigrationDb } from "@/common/model/collections";
+
+// Suppression des organisations de type ORGANISME_FORMATION n'ayant aucun utilisateur rattaché
+export const up = async () => {
+  let deletionCount = 0;
+  const data = await organisationsDb().find({ type: "ORGANISME_FORMATION" }).toArray();
+
+  for (const orga of data) {
+    const users = await usersMigrationDb().find({ organisation_id: orga._id }).toArray();
+    if (users.length === 0) {
+      deletionCount++;
+      logger.info(`Suppression de l'organisme ${orga._id} car aucun utilisateur n'y est rattaché`);
+      await organisationsDb().deleteOne({ _id: orga._id });
+    }
+  }
+  logger.info(`Nombre d'organisations supprimés : ${deletionCount}`);
+};
