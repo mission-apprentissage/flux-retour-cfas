46d47f83b22f5245adc2301a626cb5a7ee2734fe
Paul G. <paul.gaucher@beta.gouv.fr>
2025-10-08 17:03:53 +0200

    fix: mise a jour de l'affichage de la date de rupture (#4414)

--- server/src/common/actions/mission-locale/mission-locale.actions.ts
+++ server/src/common/actions/mission-locale/mission-locale.actions.ts
@@ -1336,14 +1336,7 @@ export const getEffectifsListByMisisonLocaleId = (
             },
           },
         },
-        contrat_date_rupture: {
-          $getField: {
-            field: "date_rupture",
-            input: {
-              $last: "$effectif_snapshot.contrats",
-            },
-          },
-        },
+        contrat_date_rupture: "$date_rupture",
         contrat_date_fin: {
           $getField: {
             field: "date_fin",
