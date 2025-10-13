1811f2b7137d20fb56904ff359d626050d3022e6
Paul G. <paul.gaucher@beta.gouv.fr>
2025-10-13 10:31:32 +0200

    fix: ajour de l'organisation France Travail (#4419)

---
+++ server/src/db/migrations/20251013081701-create-france-travail.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "bson";
+import { REGIONS } from "shared/constants";
+
+import { organisationsDb } from "@/common/model/collections";
+
+export const up = async () => {
+  const date = new Date();
+  const data = REGIONS.map(({ nom, code }) => {
+    return {
+      _id: new ObjectId(),
+      type: "FRANCE_TRAVAIL" as const,
+      nom,
+      code_region: code,
+      created_at: date,
+    };
+  });
+  await organisationsDb().insertMany(data);
+};
