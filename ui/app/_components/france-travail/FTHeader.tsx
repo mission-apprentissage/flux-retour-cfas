513e19cc393344b5a2951e5feb334875a45b4bc6
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2025-10-22 16:43:45 +0200

    fix: remove DECA source

--- ui/app/_components/france-travail/FTHeader.tsx
+++ ui/app/_components/france-travail/FTHeader.tsx
@@ -17,14 +17,7 @@ export const FTHeader = ({ secteurLabel }: FTHeaderProps) => {
           Nous affichons sur le Tableau de bord, tous les jeunes ayant un statut d&apos;inscrit en CFA mais sans contrat
           en entreprise, en les classant par date d&apos;inscription en formation (du plus récent au plus ancien).{" "}
           <br />
-          Sources : CFA et{" "}
-          <a
-            href="https://efpconnect.emploi.gouv.fr/auth/realms/efp/protocol/cas/login?TARGET=https%3A%2F%2Fdeca.alternance.emploi.gouv.fr%3A443%2Fdeca-app%2F"
-            target="_blank"
-            rel="noopener external"
-          >
-            DECA
-          </a>
+          Source : CFA
         </p>
       )}
     </div>
