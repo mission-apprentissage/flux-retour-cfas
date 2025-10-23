782d53bfc1dec94d6a44bea36eb4d901b926ae9f
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2025-10-23 16:17:04 +0200

    feat: add table component for dossiers traités

--- ui/app/_components/france-travail/FTHeader.tsx
+++ ui/app/_components/france-travail/FTHeader.tsx
@@ -9,6 +9,8 @@ interface FTHeaderProps {
 export const FTHeader = ({ secteurLabel }: FTHeaderProps) => {
   const title = secteurLabel ? `${secteurLabel}` : "Liste des jeunes inscrits en CFA sans contrat | À traiter";
 
+  const isEffectifsTraites = secteurLabel === "Dossiers traités";
+
   return (
     <div className={styles.headerContainer}>
       <h2 className={`fr-h2 ${styles.title}`}>{title}</h2>
@@ -18,6 +20,13 @@ export const FTHeader = ({ secteurLabel }: FTHeaderProps) => {
           en entreprise, en les classant par date d&apos;inscription en formation (du plus récent au plus ancien).{" "}
           <br />
           Source : CFA
+        </p>
+      )}
+      {isEffectifsTraites && (
+        <p className={styles.description}>
+          Retrouvez ici les dossiers de tous les jeunes inscrits en CFA sans contrat à la rentrée qui ont été contactés
+          par les agents France Travail dans votre région. Cette liste présente les dossiers des jeunes par ordre
+          chronologique, en premier le dossier qui a été traité le plus récemment.
         </p>
       )}
     </div>
