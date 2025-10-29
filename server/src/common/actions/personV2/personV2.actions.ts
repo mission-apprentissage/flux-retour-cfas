f0ceacae794f0377300e24c6de11a1dd5f60d8f4
Paul G. <paul.gaucher@beta.gouv.fr>
2025-10-29 14:00:46 +0100

    fix: mise a jour de la migration pour les isc (#4450)

--- server/src/common/actions/personV2/personV2.actions.ts
+++ server/src/common/actions/personV2/personV2.actions.ts
@@ -19,11 +19,24 @@ export async function getPersonV2FromIdentifiant(input: {
   if (!input.nom || !input.prenom || !input.date_de_naissance) {
     return null;
   }
-  const person = await personV2Db().findOne({
-    "identifiant.nom": input.nom,
-    "identifiant.prenom": input.prenom,
-    "identifiant.date_de_naissance": input.date_de_naissance,
+  const normalizedIdentifiant = normalisePersonIdentifiant({
+    nom: input.nom,
+    prenom: input.prenom,
+    date_de_naissance: input.date_de_naissance,
   });
+  const person = await personV2Db().findOneAndUpdate(
+    {
+      "identifiant.nom": normalizedIdentifiant.nom,
+      "identifiant.prenom": normalizedIdentifiant.prenom,
+      "identifiant.date_de_naissance": normalizedIdentifiant.date_de_naissance,
+    },
+    {
+      $setOnInsert: {
+        identifiant: normalizedIdentifiant,
+      },
+    },
+    { returnDocument: "after", upsert: true }
+  );
 
-  return person;
+  return person.value;
 }
