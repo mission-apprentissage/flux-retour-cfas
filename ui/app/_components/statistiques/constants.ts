5f6127644a54dd8bec9ee7197efdbfafabe60e69
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2025-11-12 10:19:26 +0100

    fix: change pourcentage method

--- ui/app/_components/statistiques/constants.ts
+++ ui/app/_components/statistiques/constants.ts
@@ -9,8 +9,9 @@ export const COLORS = {
 
 export function calculatePercentage(current: number, previous: number): string {
   if (previous === 0 && current === 0) return "0%";
-  if (previous === 0) return "+100%";
-  const percentage = ((current - previous) / previous) * 100;
+  if (current === 0 && previous > 0) return "-100%";
+  if (current === 0) return "0%";
+  const percentage = ((current - previous) / current) * 100;
   if (percentage === 0) return "0%";
   const sign = percentage > 0 ? "+" : "";
   return `${sign}${Math.round(percentage)}%`;
