868c98556c4e0d3cccb0f1fbfeed5df21942b610
Mathieu Borrell <mathieu.borrell@beta.gouv.fr>
2026-03-30 11:54:57 +0200

    fix

--- .infra/files/scripts/sync-preprod.sh
+++ .infra/files/scripts/sync-preprod.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 
 sourceUri="{{ MNA_TDB_MONGODB_URI }}"
-targetUri="{{ preprod.MNA_TDB_MONGODB_URI }}"
+targetUri="{{ SYNC_MNA_TDB_MONGODB_URI }}"
 
 dbName="{{ MNA_TDB_MONGODB_DB_NAME }}"
-dbNameTarget="{{ preprod.MNA_TDB_MONGODB_DB_NAME }}"
+dbNameTarget="{{ SYNC_MNA_TDB_MONGODB_DB_NAME }}"
 
 SECONDS=0
 docker run -i --rm mongo:7 mongodump --ssl --uri="${sourceUri}" --db="${dbName}" --archive --authenticationDatabase=${dbName} | docker run -i --rm mongo:8 mongorestore --archive --nsInclude="${dbName}.*" --nsFrom="${dbName}.*" --nsTo="${dbNameTarget}.*" --uri="${targetUri}" --authenticationDatabase=${dbNameTarget} --drop
