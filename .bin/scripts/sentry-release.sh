716a78a93d79bd388659662e7ab34d5facc94144
Mathieu Borrell <mathieu.borrell@beta.gouv.fr>
2026-04-01 14:38:54 +0200

    fix: corrections sur le script sentry-release.sh (#4578)

--- .bin/scripts/sentry-release.sh
+++ .bin/scripts/sentry-release.sh
@@ -10,6 +10,6 @@ shift 1
 
 export SENTRY_AUTH_TOKEN=$(sops --decrypt --extract '["SENTRY_AUTH_TOKEN"]' ${ROOT_DIR}/.infra/env.global.yml)
 
-export SENTRY_DSN=$(sops --decrypt --extract '["LBA_SERVER_SENTRY_DSN"]' ${ROOT_DIR}/.infra/env.global.yml)
+export SENTRY_DSN=$(sops --decrypt --extract '["SERVER_SENTRY_DSN"]' ${ROOT_DIR}/.infra/env.global.yml)
 cd "$ROOT_DIR/server"
 "./sentry-release-server.sh" "mission-apprentissage/flux-retour-cfas" "${COMMIT_ID}" "${VERSION}"
