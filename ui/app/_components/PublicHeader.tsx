8abcc0f2cb33d99183d26c6e6b8f51c919996017
Lucas Détré <lucas.detre.ext@beta.gouv.fr>
2026-04-30 17:45:23 +0200

    feat: modification du PublicHeader

--- ui/app/_components/PublicHeader.tsx
+++ ui/app/_components/PublicHeader.tsx
@@ -1,7 +1,26 @@
+"use client";
+
 import { Header as DsfrHeader } from "@codegouvfr/react-dsfr/Header";
+import { usePathname } from "next/navigation";
 import { CRISP_FAQ } from "shared";
 
+const ROUTES = {
+  ACCUEIL: "/",
+  ACCUEIL_CFA: "/accueil-cfa",
+  ACCUEIL_MISSION_LOCALE: "/accueil-mission-locale",
+  ACCUEIL_TERRITOIRE: "/accueil-territoire",
+  CENTRE_AIDE: CRISP_FAQ,
+};
+
+function isActive(pathname: string, href: string) {
+  if (href === "/") {
+    return pathname === "/";
+  }
+  return pathname?.startsWith(href);
+}
+
 export function PublicHeader() {
+  const pathname = usePathname() ?? "/";
   return (
     <DsfrHeader
       brandTop={<>RÉPUBLIQUE FRANÇAISE</>}
@@ -32,25 +51,44 @@ export function PublicHeader() {
       navigation={[
         {
           text: "Accueil",
-          isActive: true,
+          isActive: isActive(pathname, "/"),
           linkProps: {
             href: "/",
             target: "_self",
           },
         },
         {
-          text: "Aide et ressources",
-          menuLinks: [
-            {
-              linkProps: {
-                href: CRISP_FAQ,
-                target: "_blank",
-                rel: "noopener noreferrer",
-              },
-
-              text: "Centre d’aide",
-            },
-          ],
+          text: "Établissement de formation (CFA)",
+          isActive: isActive(pathname, ROUTES.ACCUEIL_CFA),
+          linkProps: {
+            href: ROUTES.ACCUEIL_CFA,
+            target: "_self",
+          },
+        },
+        {
+          text: "Missions Locales",
+          isActive: isActive(pathname, ROUTES.ACCUEIL_MISSION_LOCALE),
+          linkProps: {
+            href: ROUTES.ACCUEIL_MISSION_LOCALE,
+            target: "_self",
+          },
+        },
+        {
+          text: "Collectivités et acteurs de l'apprentissage",
+          isActive: isActive(pathname, ROUTES.ACCUEIL_TERRITOIRE),
+          linkProps: {
+            href: ROUTES.ACCUEIL_TERRITOIRE,
+            target: "_self",
+          },
+        },
+        {
+          text: "Centre d'aide",
+          isActive: isActive(pathname, ROUTES.CENTRE_AIDE),
+          linkProps: {
+            href: ROUTES.CENTRE_AIDE,
+            target: "_blank",
+            rel: "noopener noreferrer",
+          },
         },
       ]}
     />
