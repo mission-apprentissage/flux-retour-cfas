fa0903b0159dffaa176387e6a1a8209bbb9196ae
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2026-06-11 11:22:24 +0200

    perf: drop redundant departement index on effectifs and effectifsDECA

--- shared/models/data/effectifs.model.ts
+++ shared/models/data/effectifs.model.ts
@@ -75,7 +75,6 @@ const indexes: [IndexSpecification, CreateIndexesOptions][] = [
   [{ updated_at: 1 }, { name: "updated_at" }],
   [{ transmitted_at: 1 }, {}],
   [{ "_computed.organisme.region": 1 }, {}],
-  [{ "_computed.organisme.departement": 1 }, {}],
   [{ "_computed.organisme.departement": 1, "formation.date_entree": 1 }, {}],
   [{ "_computed.organisme.academie": 1 }, {}],
   [{ "_computed.organisme.bassinEmploi": 1 }, {}],
