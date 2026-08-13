d5b8c37040e7fd8cb48d5bde96312ac891d60da4
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2026-08-13 16:14:50 +0200

    chore: remove dead exports across ui, server and shared

--- ui/common/constants/usersConstants.ts
+++ ui/common/constants/usersConstants.ts
@@ -3,9 +3,3 @@ export const USER_STATUS_LABELS = {
   PENDING_ADMIN_VALIDATION: "en attente validation admin",
   CONFIRMED: "accès confirmé",
 };
-
-export const USER_STATUS_STYLE = {
-  PENDING_EMAIL_VALIDATION: "red",
-  PENDING_ADMIN_VALIDATION: "orange",
-  CONFIRMED: "green",
-};
