f5d4e9d2670ae20e0bbcf1095921e4aa6751aaff
Mathieu Borrell <mathieu.borrell@beta.gouv.fr>
2026-08-20 16:26:44 +0200

    fix: exécution de mongodump et mongorestore sur le même conteneur

--- .infra/files/scripts/sync-preprod.sh
+++ .infra/files/scripts/sync-preprod.sh
@@ -9,5 +9,19 @@ dbName="{{ MNA_TDB_MONGODB_DB_NAME }}"
 dbNameTarget="{{ SYNC_MNA_TDB_MONGODB_DB_NAME }}"
 
 SECONDS=0
-docker run -i --rm --log-driver=none mongo:7 mongodump --ssl --uri="${sourceUri}" --db="${dbName}" --archive --authenticationDatabase=${dbName} | docker run -i --rm --log-driver=none mongo:8 mongorestore --archive --nsInclude="${dbName}.*" --nsFrom="${dbName}.*" --nsTo="${dbNameTarget}.*" --uri="${targetUri}" --authenticationDatabase=${dbNameTarget} --drop
+docker run -i --rm --log-driver=none mongo:8 bash -c "
+  mongodump \
+    --ssl \
+    --uri=\"${sourceUri}\" \
+    --db=\"${dbName}\" \
+    --archive \
+    --authenticationDatabase=${dbName} \
+    | mongorestore \
+      --archive \
+      --nsInclude=\"${dbName}.*\" \
+      --nsFrom=\"${dbName}.*\" \
+      --nsTo=\"${dbNameTarget}.*\" \
+      --uri=\"${targetUri}\" \
+      --authenticationDatabase=\"${dbNameTarget}\" \
+      --drop"
 echo "Elapsed Time: $SECONDS seconds"
