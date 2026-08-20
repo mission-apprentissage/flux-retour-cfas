3beef247b4572b4681da31f931ed73104bef6375
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2026-08-20 16:49:39 +0200

    fix: rétablir le chargement des pages Notion en montant la lib en v8

--- ui/pages/politique-de-confidentialite.tsx
+++ ui/pages/politique-de-confidentialite.tsx
@@ -1,18 +1,16 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
-import { NotionAPI } from "notion-client";
 import { ExtendedRecordMap } from "notion-types";
 import { NotionRenderer } from "react-notion-x";
 
 import { _get } from "@/common/httpClient";
-import { sanitizeNotionRecordMap } from "@/common/utils/notionUtils";
+import { getNotionPage } from "@/common/utils/notionUtils";
 import SimplePage from "@/components/Page/SimplePage";
 
 import "react-notion-x/src/styles.css";
 
 export const getStaticProps = (async () => {
-  const notion = new NotionAPI();
-  const recordMap = await notion.getPage("Politique-de-confidentialit-7b1c32f4c2214e0c9523686b18ada6fa");
-  return { props: { data: sanitizeNotionRecordMap(recordMap) }, revalidate: 60 * 30 };
+  const recordMap = await getNotionPage("Politique-de-confidentialit-7b1c32f4c2214e0c9523686b18ada6fa");
+  return { props: { data: recordMap }, revalidate: 60 * 30 };
 }) satisfies GetStaticProps<{
   data: ExtendedRecordMap;
 }>;
