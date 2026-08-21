55ed22e221fc9bb8e83dd66637767ea355d5e878
Yohann Gablowski <yohann.gablowski@beta.gouv.fr>
2026-08-21 15:59:40 +0200

    fix: accès à la fiche organisme réservé aux administrateurs et têtes de réseau

--- ui/app/(organisme)/organismes/layout.tsx
+++ ui/app/(organisme)/organismes/layout.tsx
@@ -1,5 +1,17 @@
+import { redirect } from "next/navigation";
+import { ORGANISATION_TYPE } from "shared";
+
+import { getSession } from "@/app/_utils/session.utils";
+
 import styles from "./organismes-fond.module.css";
 
-export default function OrganismesLayout({ children }: { children: React.ReactNode }) {
+export default async function OrganismesLayout({ children }: { children: React.ReactNode }) {
+  const user = await getSession();
+  const organisationType = user?.organisation?.type;
+
+  if (organisationType !== ORGANISATION_TYPE.ADMINISTRATEUR && organisationType !== ORGANISATION_TYPE.TETE_DE_RESEAU) {
+    redirect("/");
+  }
+
   return <div className={styles.fond}>{children}</div>;
 }
